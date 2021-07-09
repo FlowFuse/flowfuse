015c2e193f813553a10d925cbbb6a169c2537264
Ben Hardill <ben@flowforge.com>
2021-07-09 14:41:10 +0100

    Fix spaces

--- forge/db/models/Project.js
+++ forge/db/models/Project.js
@@ -5,15 +5,15 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = {
-	name: 'Project',
-	schema: {
-		id: { type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4},
-		name: { type: DataTypes.STRING, allowNull: false},
-		type: { type: DataTypes.STRING, allowNull: false},
-		url: { type: DataTypes.STRING, allowNull: false}
-	},
-	associations: function(M) {
-		this.belongsToMany(M['Team'], { through: M['ProjectTeam']})
-		this.hasMany(M['ProjectTeam'])
-	}
+    name: 'Project',
+    schema: {
+        id: { type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4},
+        name: { type: DataTypes.STRING, allowNull: false},
+        type: { type: DataTypes.STRING, allowNull: false},
+        url: { type: DataTypes.STRING, allowNull: false}
+    },
+    associations: function(M) {
+        this.belongsToMany(M['Team'], { through: M['ProjectTeam']})
+        this.hasMany(M['ProjectTeam'])
+    }
 }
--- forge/routes/api/project.js
+++ forge/routes/api/project.js
@@ -8,52 +8,52 @@
  */
  module.exports = async function(app) {
 
- 	/**
- 	 * Get the details of a givevn project
- 	 * @name /api/v1/project
- 	 * @static
- 	 * @memberof forge.routes.api.project
- 	 */
- 	app.get('/:id', async (request, reply) => {
- 		let project = {
- 				id: "45ec7fd2-3e80-4ba5-ad19-e756e354c928",
- 				name: "instance1",
- 				url: "https://instance1.example.com"
- 		}
- 		reply.send(project)
- 	})
+    /**
+     * Get the details of a givevn project
+     * @name /api/v1/project
+     * @static
+     * @memberof forge.routes.api.project
+     */
+    app.get('/:id', async (request, reply) => {
+        let project = {
+                id: "45ec7fd2-3e80-4ba5-ad19-e756e354c928",
+                name: "instance1",
+                url: "https://instance1.example.com"
+        }
+        reply.send(project)
+    })
 
- 	/**
- 	 * Create an new project
- 	 * @name /api/v1/project
- 	 * 
- 	 */
- 	app.post('/', {
- 		schema: {
- 			body: {
- 				type: 'object',
- 				required: ['name','options', 'type'],
- 				properties: {
- 					name: { type: 'string'},
- 					type: { type: 'string'},
- 					options: { type: 'object'}
- 				}
- 			}
- 		}
- 	}, async (request, reply) => {
- 		reply.send({
- 			name: request.body.name, 
- 			namespace: request.body.options.snamespace,
- 			url: "https://" + request.body.name + ".example.com"
- 		})
- 	})
+    /**
+     * Create an new project
+     * @name /api/v1/project
+     * 
+     */
+    app.post('/', {
+        schema: {
+            body: {
+                type: 'object',
+                required: ['name','options', 'type'],
+                properties: {
+                    name: { type: 'string'},
+                    type: { type: 'string'},
+                    options: { type: 'object'}
+                }
+            }
+        }
+    }, async (request, reply) => {
+        reply.send({
+            name: request.body.name, 
+            namespace: request.body.options.snamespace,
+            url: "https://" + request.body.name + ".example.com"
+        })
+    })
 
- 	/**
- 	 * Delete an project
- 	 * @name /api/v1/project
- 	 * @memberof foreg.routes.api.project 
- 	 */
- 	app.delete('/:id', async (request, reply) => {
- 		reply.send({ status: "okay"})
- 	})
+    /**
+     * Delete an project
+     * @name /api/v1/project
+     * @memberof foreg.routes.api.project 
+     */
+    app.delete('/:id', async (request, reply) => {
+        reply.send({ status: "okay"})
+    })
  }
