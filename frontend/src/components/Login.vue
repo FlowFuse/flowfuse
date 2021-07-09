0dae85382c3b8a4fed4ac1914eae50809235e8a5
Nick O'Leary <nick.oleary@gmail.com>
2021-07-09 13:37:32 +0100

    Add login failed feedback

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -9,18 +9,8 @@
                     </h2>
                 </div>
                 <form class="px-4 sm:px-6 lg:px-8 mt-8 space-y-6">
-                    <input type="hidden" name="remember" value="true" />
-                    <div class="rounded-md shadow-sm -space-y-px">
-                        <div>
-                            <label for="username" class="sr-only">Username</label>
-                            <input id="username" v-model="input.username" name="username" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none  focus:ring-blue-700 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Username" />
-                        </div>
-                        <div>
-                            <label for="password" class="sr-only">Password</label>
-                            <input id="password" v-model="input.password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none  focus:ring-blue-700 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" @keyup.enter.native="login"/>
-                        </div>
-                    </div>
-
+                    <FormRow :error="errors.username" v-model="input.username">Username</FormRow>
+                    <FormRow type="password" :error="errors.password" v-model="input.password">Password</FormRow>
                     <div class="flex flex-col justify-between">
                         <div class="flex items-center">
                             <input id="remember_me" name="remember_me" v-model="input.remember" type="checkbox" class="h-4 w-4 text-indigo-600  focus:ring-blue-700 border-gray-300 rounded" />
@@ -59,28 +49,52 @@
 <script>
 import { mapState } from 'vuex'
 import Logo from "@/components/Logo"
+import FormRow from '@/components/FormRow'
 import { LockClosedIcon } from '@heroicons/vue/outline'
 
 export default {
     name: "Login",
-    computed: mapState('account',['pending']),
+    computed:mapState('account',['pending', 'loginError']),
     data() {
         return {
             input: {
                 username: "",
                 password: "",
                 remember: false
+            },
+            errors: {
+                username: null,
+                password: null
             }
         }
     },
     methods: {
         login() {
-            this.$store.dispatch('account/login',this.input);
+            let valid = true;
+            this.errors.username = "";
+            this.errors.password = "";
+            if (this.input.username === "") {
+                valid = false;
+                this.errors.username = "Required field"
+            }
+            if (this.input.password === "") {
+                valid = false;
+                this.errors.password = "Required field"
+            }
+            if (valid) {
+                this.$store.dispatch('account/login',this.input);
+            }
+        }
+    },
+    watch: {
+        loginError(newError, oldError) {
+            this.errors.username = "Login failed"
         }
     },
     components: {
         Logo,
         LockClosedIcon,
+        FormRow
     }
 }
 </script>
