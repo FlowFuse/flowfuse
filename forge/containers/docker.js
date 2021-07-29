106f732c33b05bb3fc2bcbd1108fa3447cb1953c
Ben Hardill <ben@flowforge.com>
2021-07-29 12:05:11 +0100

    Moving closer to something useful

--- forge/containers/docker.js
+++ forge/containers/docker.js
@@ -31,14 +31,47 @@ module.exports = {
         try {
             let container = await this._docker.createContainer(contOptions);
             await container.start();
-            return {status: "started"};
+            return {
+                name: name, 
+                status: "started", 
+                url: "",
+                meta: container
+            };
         } catch (err) {
             return {error: err}
         }
     },
     remove: async (name) => {
         console.log("removing ", name)
-        let container = await this._docker.getContainer(name);
-        await container.remove()
+        try {
+            let container = await this._docker.getContainer(name);
+            await container.stop()
+            await container.remove()
+            return {status: "removed"}
+        } catch (err) {
+            return {error: err}
+        }
+    },
+    details: async (name) => {
+        try {
+            let container = await this._docker.getContainer(name);
+            return container
+        } catch (err) {
+            return {error: err}
+        }
+    },
+    list: async (filter) => {
+        let containers = await this._docker.listContainers({all: true})
+        //console.log(containers)
+        return containers.map(c => { return c.Names[0].substring(1)})
+    },
+    start: async (name) => {
+
+    },
+    stop: async (name) => {
+
+    },
+    restart: async (name) => {
+
     }
 }
--- forge/routes/api/project.js
+++ forge/routes/api/project.js
@@ -41,29 +41,29 @@
                 properties: {
                     name: { type: 'string' },
                     type: { type: 'string' },
-                    url: { type: 'string' },
                     team: { type: 'number'},
                     options: { type: 'object'}
                 }
             }
         }
     }, async (request, reply) => {
-        let project = await app.db.models.Project.create({
-            name: request.body.name,
-            type: request.body.type,
-            url: request.body.url
+        app.containers.create(request.body.name, request.body.options)
+        .then(container => {
+            return app.db.models.Project.create({
+                name: request.body.name,
+                type: request.body.type,
+                url: container.url
+            })
         })
-        if (project) {
+        .then(async (project) => {
             let team = await app.db.models.Team.findOne({where:{id: request.body.team}})
             project.setTeam(team);
-
-            //this needs to setup instance DB stuff somewhere
-            let status = await app.containers.create(request.body.name)
-
             reply.send(project)
-        } else {
+        })
+        .catch(err => {
+            //need some better rollback logic here
             reply.status(500).send({error: "Something went wrong"})
-        }
+        })
     })
 
     /**
@@ -72,14 +72,21 @@
      * @memberof foreg.routes.api.project 
      */
     app.delete('/:id', async (request, reply) => {
-        let project = await app.db.models.Project.byId(request.params.id);
-        if (project) {
-            await project.destroy();
-            await app.containers.remove(project.name);
-            reply.send({ status: "okay"});
-        } else {
-            reply.status(404).send({error: "Project not found"})
-        }
+        app.containers.remove(project.name)
+        .then(async status => {
+            let project = await app.db.models.Project.byId(request.params.id);
+            if (project) {
+                project.destroy();
+                reply.send({ status: "okay"});
+            } else {
+                reply.status(404).send({error: "Project not found"})
+            }
+        })
+        .catch(err => {
+            reply.status(500).send(err)
+        })
+ 
+        
         
     })
  }
