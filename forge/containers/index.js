73e7a868672f95694b7cab2d17c2cca2b57efca1
Ben Hardill <ben@flowforge.com>
2021-12-02 15:48:40 +0000

    Add Docker driver

--- forge/containers/index.js
+++ forge/containers/index.js
@@ -45,7 +45,7 @@ module.exports = fp(async function(app, _opts, next){
             domain: process.env.DOMAIN ||"example.com",
             //this list needs loading from an external source
             containers:{
-                basic: "docker-pi.local:5000/flowforge/node-red"
+                basic: "flowforge/node-red"
             }
         });
         app.decorate('containers', driver);
