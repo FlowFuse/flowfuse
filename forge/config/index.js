db9c6e2fffabef5b19df4d817bc56b507051027d
Ben Hardill <ben@flowforge.com>
2022-01-27 10:32:46 +0000

    Move the NodeJS check to very start of application

--- forge/config/index.js
+++ forge/config/index.js
@@ -18,7 +18,7 @@ const fs = require("fs");
 const path = require("path");
 const fp = require("fastify-plugin");
 const YAML = require("yaml");
-const semver = require('semver')
+
 // const FastifySecrets = require('fastify-secrets-env')
 
 module.exports = fp(async function(app, _opts, next) {
@@ -45,12 +45,7 @@ module.exports = fp(async function(app, _opts, next) {
         app.log.info(`FlowForge v${version}`)
     }
 
-    if (!semver.satisfies(process.version,">=16.0.0")) {
-        app.log.warn(`FlowForge requires at least NodeJS v16, ${process.version} found`)
-        process.exit(1)
-    } else {
-        app.log.info(`FlowForge running with NodeJS ${process.version}`)
-    }
+    app.log.info(`FlowForge running with NodeJS ${process.version}`)
 
     app.log.info(`FlowForge Data Directory: ${process.env.FLOWFORGE_HOME}`)
 
