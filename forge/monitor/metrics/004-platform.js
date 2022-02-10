d3543bf7cf43b8d4c5129b2e0d1529b5c3c233f0
Nick O'Leary <nick.oleary@gmail.com>
2022-02-10 13:58:01 +0000

    Add platform.config.driver to ping payload

--- forge/monitor/metrics/004-platform.js
+++ forge/monitor/metrics/004-platform.js
@@ -2,6 +2,7 @@ module.exports = async (app) => {
     return {
         'platform.counts.users': await app.db.models.User.count(),
         'platform.counts.teams': await app.db.models.Team.count(),
-        'platform.counts.projects': await app.db.models.Project.count()
+        'platform.counts.projects': await app.db.models.Project.count(),
+        'platform.config.driver': app.config.driver.type
     }
 }
