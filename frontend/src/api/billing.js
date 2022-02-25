40cc4043b30763e72e830596d7f2a24f0a1efadd
Joe Pavitt <joepavitt@joes-mbp.home>
2022-02-25 17:09:42 +0000

    First pass of Billing API service

---
+++ frontend/src/api/billing.js
@@ -0,0 +1,52 @@
+import client from './client'
+
+// Custom Portal - Redirect
+const customerPortal = async (teamId) => {
+    return client.get('/ee/billing/customer-portal' + teamId).then(res => {
+        return res.data
+    })
+}
+
+// Add new Subscription (Team)
+const createSubscription = async (teamId) => {
+    return client.get('/ee/billing/teams' + teamId).then(res => {
+        return res.data
+    })
+}
+
+// Get information on billing/subscription for a given team
+const getSubscriptionInfo = async (teamId) => {
+    return client.get('/ee/billing/teams/' + teamId).then(res => {
+        return res.data
+    })
+}
+
+// Close a subscription
+const closeSubscription = async (teamId) => {
+    return client.delete('/ee/billing/teams/' + teamId).then(res => {
+        return res.data
+    })
+}
+
+// Add a new project to a subscription
+const addToSubscription = async (teamId, project) => {
+    return client.post('/ee/billing/teams/' + teamId + '/projects', project).then(res => {
+        return res.data
+    })
+}
+
+// Remove a project from a subscription
+const removeFromSubscription = async (teamId, projectId) => {
+    return client.post('/ee/billing/team-update/' + teamId + '/projects/' + projectId).then(res => {
+        return res.data
+    })
+}
+
+export default {
+    customerPortal,
+    createSubscription,
+    getSubscriptionInfo,
+    closeSubscription,
+    addToSubscription,
+    removeFromSubscription
+}
