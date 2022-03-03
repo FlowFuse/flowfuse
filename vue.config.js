d2cc0959107f3084bad517af87a8c9158598b231
Joe Pavitt <joepavitt@Joes-MacBook-Pro.local>
2022-03-03 10:09:23 +0000

    GitHub Pages Vue Build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
+const path = require('path')
+
 // vue.config.js
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/flowforge-ui-components/' : '/',
     pluginOptions: {
         lintStyleOnBuild: true,
         stylelint: {}
-    }
+    },
+    pages: {
+        index: {
+            entry: 'docs/main.js'
+        }
+    },
+    outputDir: path.resolve(__dirname, 'dist/docs')
 }
