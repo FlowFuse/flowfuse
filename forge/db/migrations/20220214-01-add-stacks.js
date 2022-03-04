fd7388d74ea5848420ee06569440649416efed99
Nick O'Leary <nick.oleary@gmail.com>
2022-03-04 16:53:00 +0000

    Fix stack migration

--- forge/db/migrations/20220214-01-add-stacks.js
+++ forge/db/migrations/20220214-01-add-stacks.js
@@ -17,13 +17,19 @@ module.exports = {
             createdAt: { type: DataTypes.DATE },
             updatedAt: { type: DataTypes.DATE }
         })
-        await context.addColumn('Projects', 'stackId', {
+        await context.addColumn('Projects', 'ProjectStackId', {
             type: DataTypes.INTEGER,
             references: { model: 'ProjectStacks', key: 'id' },
             onDelete: 'set null',
             onUpdate: 'cascade'
         })
-        await context.removeColumn('Projects', 'type')
+        // We cannot safely remove columns when using sqlite as it triggers
+        // 'on delete cascade' events that wipes the ProjectSettings table
+        // https://stackoverflow.com/a/70486686/2117239
+        // Possible workaround is to wrap this with a modification to the
+        // other tables to remove the `on delete cascade` constraint - and
+        // then restore it afterwards.
+        // await context.removeColumn('Projects', 'type')
     },
     down: async (context) => {
     }
