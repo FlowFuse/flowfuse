bd7f41c67cf1452f742d3c6bc131e71c19dc1c6e
Ben Hardill <ben@flowforge.com>
2022-03-08 16:32:50 +0000

    Return new Stripe setup session if no subscription

--- forge/ee/routes/billing/index.js
+++ forge/ee/routes/billing/index.js
@@ -109,7 +109,8 @@ module.exports = async function (app) {
         const team = request.team
         const sub = await app.db.models.Subscription.byTeam(team.id)
         if (!sub) {
-            response.code(404).type('text/html').send('Not Found')
+            const session = await app.billing.createSubscriptionSession(team, request.session.User)
+            response.code(404).type('application/json').send({billingURL: session.url})
             return
         }
 
