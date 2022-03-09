0d01fc601cc3b01cc4a6c790c34ae85954507d11
Joe Pavitt <joepavitt@joes-mbp.home>
2022-03-09 13:58:51 +0000

    "Thank You" message w/placeholder upon completion of finance details

--- frontend/src/pages/team/Overview.vue
+++ frontend/src/pages/team/Overview.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="block md:flex">
-        <div class="flex-grow p-2">
+        <div v-if="!showingMessage" class="flex-grow p-2">
             <FormHeading>
                 <router-link to="./projects">Projects</router-link>
                 <template v-if="createProjectEnabled" v-slot:tools>
@@ -16,6 +16,12 @@
                 </div>
             </template>
         </div>
+        <div v-else class="flex-grow p-2">
+            <p>
+                Thank you for signing up to FlowForge. You are now able to create projects and use the platform.
+            </p>
+            <router-link to="./projects/create" class="forge-button pl-1 pr-2 mt-2"><PlusSmIcon class="w-4" /><span class="text-xs">Create Project</span></router-link>
+        </div>
         <div class="md:w-48 p-2 md:ml-8">
             <FormHeading>
                 <router-link to="./members">Members</router-link>
@@ -30,13 +36,11 @@
 import { Roles } from '@core/lib/roles'
 
 import teamApi from '@/api/team'
-import billingApi from '@/api/billing'
 
 import FormHeading from '@/components/FormHeading'
 import MemberSummaryList from './components/MemberSummaryList'
 import ProjectSummaryList from './components/ProjectSummaryList'
-import { PlusSmIcon, UsersIcon, ChevronRightIcon } from '@heroicons/vue/outline'
-import CreateProjectButton from '@/components/CreateProjectButton'
+import { PlusSmIcon } from '@heroicons/vue/outline'
 
 export default {
     name: 'TeamOverview',
@@ -46,13 +50,19 @@ export default {
             userCount: 0,
             users: null,
             projectCount: 0,
-            projects: null
+            projects: null,
+            show: {
+                thankyou: false
+            }
 
         }
     },
     computed: {
         createProjectEnabled: function () {
             return this.teamMembership.role === Roles.Owner
+        },
+        showingMessage: function () {
+            return this.show.thankyou
         }
     },
     watch: {
@@ -60,6 +70,7 @@ export default {
     },
     mounted () {
         this.fetchData()
+        this.checkBillingSession()
     },
     methods: {
         fetchData: async function (newVal, oldVal) {
@@ -73,6 +84,10 @@ export default {
                 this.userCount = members.count
                 this.users = members.members
             }
+        },
+        // has the user navigated here directly from Stripe, having just completed payment details
+        checkBillingSession () {
+            this.show.thankyou = 'billing_session' in this.$route.query
         }
     },
     components: {
