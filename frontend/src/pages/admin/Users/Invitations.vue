6ee584589d052f3ac60518f609d21e77e5aa60f8
Joe Pavitt <joepavitt@joes-mbp.home>
2022-03-15 11:49:19 +0000

    Link button to the teamApi removeteamInvitation method

--- frontend/src/pages/admin/Users/Invitations.vue
+++ frontend/src/pages/admin/Users/Invitations.vue
@@ -12,21 +12,22 @@
 
 <script>
 import adminApi from '@/api/admin'
-import { markRaw } from "vue"
+import teamApi from '@/api/team'
+import { markRaw } from 'vue'
 import FormHeading from '@/components/FormHeading'
 import ItemTable from '@/components/tables/ItemTable'
 import InviteUserCell from '@/components/tables/cells/InviteUserCell'
 import { TrashIcon } from '@heroicons/vue/outline'
-import { useRoute, useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router'
 
 const MemberInviteRemoveButton = {
-    template: `<button type="button" class="forge-button-inline px-2 py-2" @click="removeInvite"><TrashIcon class="w-5" /></button>`,
-    props: ['id','team','onremove'],
+    template: '<button type="button" class="forge-button-inline px-2 py-2" @click="removeInvite"><TrashIcon class="w-5" /></button>',
+    props: ['id', 'team', 'onremove'],
     components: {
         TrashIcon
     },
     methods: {
-        async removeInvite() {
+        async removeInvite () {
             await this.onremove(this.team.id, this.id)
         }
     }
@@ -34,32 +35,32 @@ const MemberInviteRemoveButton = {
 
 export default {
     name: 'UserInviteTable',
-    data() {
+    data () {
         return {
             invitations: [],
             inviteColumns: [
-                {name: 'User', component: { is: markRaw(InviteUserCell) }, property: 'invitee'},
-                {name: 'Team', property:'teamName'},
-                {name: 'Invited By',property:'invitor', component: { is: markRaw(InviteUserCell) }},
-                {name: 'Expires In', property: 'expires'},
-                {name: '', class: ['w-16'], component: {is: markRaw(MemberInviteRemoveButton) } }
+                { name: 'User', component: { is: markRaw(InviteUserCell) }, property: 'invitee' },
+                { name: 'Team', property: 'teamName' },
+                { name: 'Invited By', property: 'invitor', component: { is: markRaw(InviteUserCell) } },
+                { name: 'Expires In', property: 'expires' },
+                { name: '', class: ['w-16'], component: { is: markRaw(MemberInviteRemoveButton) } }
             ]
         }
     },
-    mounted() {
+    mounted () {
         this.fetchData()
     },
     methods: {
-        async removeInvite(teamId, inviteId) {
-            // await adminApi.removeTeamInvitation(teamId, inviteId)
-            // await this.fetchData();
+        async removeInvite (teamId, inviteId) {
+            await teamApi.removeTeamInvitation(teamId, inviteId)
+            await this.fetchData()
         },
         async fetchData () {
             const invitations = await adminApi.getInvitations()
             this.invitations = invitations.invitations.map(invite => {
-                invite.teamName = invite.team.name;
-                invite.onremove = (teamId, inviteId) => { this.removeInvite(teamId,inviteId) }
-                return invite;
+                invite.teamName = invite.team.name
+                invite.onremove = (teamId, inviteId) => { this.removeInvite(teamId, inviteId) }
+                return invite
             })
         }
     },
