f390889f36dd95bf53f9d022aec98be35eb1873c
Joe Pavitt <joepavitt@joes-mbp.home>
2022-03-18 16:23:28 +0000

    Fix issue with team_slug missing in Team API

--- frontend/src/api/team.js
+++ frontend/src/api/team.js
@@ -8,7 +8,7 @@ import { RoleNames } from '@core/lib/roles'
 const getTeams = () => {
     return client.get('/api/v1/user/teams').then(res => {
         res.data.teams = res.data.teams.map(r => {
-            r.link = { name: 'Team', params: { id: slugify(r.name) } }
+            r.link = { name: 'Team', params: { team_slug: r.slug } }
             r.roleName = RoleNames[r.role]
             return r
         })
