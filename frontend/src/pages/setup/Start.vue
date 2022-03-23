ef702e8882d8fa3eeb2c36ca841928eb670b80fb
Joe Pavitt <joepavitt@joes-mbp.home>
2022-03-23 10:05:40 +0000

    Include forge-ui-components in Setup app

--- frontend/src/pages/setup/Start.vue
+++ frontend/src/pages/setup/Start.vue
@@ -2,7 +2,7 @@
     <form class="px-4 sm:px-6 lg:px-8 mt-8 space-y-4">
         <p class="text-gray-700 text-lg mt-10 text-center">Welcome to your shiny new FlowForge platform.</p>
         <p class="text-gray-700 text-center">Let's get it setup for you to start using.</p>
-        <div class="space-x-2 text-center">
+        <div class="flex justify-center">
             <ff-button @click="done()" class="mt-6">Start setup</ff-button>
         </div>
     </form>
