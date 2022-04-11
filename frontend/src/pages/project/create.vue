211dc031447d61a451c9236ab9155dd231af512a
Ben Hardill <ben@flowforge.com>
2022-04-11 10:58:51 +0100

    Update frontend/src/pages/project/create.vue

--- frontend/src/pages/project/create.vue
+++ frontend/src/pages/project/create.vue
@@ -64,7 +64,7 @@ export default {
                 team: '',
                 stack: '',
                 template: '',
-                billingConfirmation: false,
+                billingConfirmation: false
                 // description: "",
             },
             errors: {
