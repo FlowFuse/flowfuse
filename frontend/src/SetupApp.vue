e36e4510ad693ebab8083e0c10b75ea33ba214c8
Joe Pavitt <joepavitt@joes-mbp.home>
2022-04-22 11:31:29 +0100

    Rebrand the "Setup" workflow

--- frontend/src/SetupApp.vue
+++ frontend/src/SetupApp.vue
@@ -1,19 +1,8 @@
 <template>
-    <div class="min-h-screen flex flex-col bg-gray-300 ">
-        <div class="flex-grow mx-auto flex bg-gray-50 pt-4 pb-12">
-            <div class="sm:max-w-xl w-screen space-y-2">
-                <div class="max-w-xs mx-auto w-full mb-4">
-                    <Logo/>
-                    <h2 class="mt-2 text-center text-3xl font-bold text-gray-900">
-                        <span>FLOW</span><span class="font-light">FORGE</span>
-                    </h2>
-                </div>
-                <component :is="views[step]" :state="state" @next="next"></component>
-            </div>
-        </div>
-        <div class="w-full bg-gray-800 flex-grow-0">
-            <PageFooter />
-        </div>
+    <div class="min-h-screen flex flex-col">
+        <ff-layout-box class="ff-setup">
+            <component :is="views[step]" :state="state" @next="next"></component>
+        </ff-layout-box>
     </div>
 </template>
 
@@ -27,6 +16,8 @@ import Options from '@/pages/setup/Options'
 import CreateAdminUser from '@/pages/setup/CreateAdminUser'
 import License from '@/pages/setup/License'
 import Final from '@/pages/setup/Final'
+
+import FFLayoutBox from '@/layouts/Box'
 
 // To add more views in the setup dialogs, add them to this list.
 // Just make sure 'Final' is the last one.
@@ -58,7 +49,12 @@ export default {
         PageFooter,
         Logo,
         CreateAdminUser,
-        License
+        License,
+        'ff-layout-box': FFLayoutBox
     }
 }
 </script>
+
+<style lang="scss">
+@import "./stylesheets/common.scss";
+</style>
--- frontend/src/pages/setup/Start.vue
+++ frontend/src/pages/setup/Start.vue
@@ -1,7 +1,7 @@
 <template>
-    <form class="px-4 sm:px-6 lg:px-8 mt-8 space-y-4">
-        <p class="text-gray-700 text-lg mt-10 text-center">Welcome to your shiny new FlowForge platform.</p>
-        <p class="text-gray-700 text-center">Let's get it setup for you to start using.</p>
+    <form class="">
+        <p class="text-center">Welcome to your shiny new FlowForge platform.</p>
+        <p class="text-center">Let's get it setup for you to start using.</p>
         <div class="flex justify-center">
             <ff-button @click="done()" class="mt-6">Start setup</ff-button>
         </div>
