8b2a1d3db55180e4c394b94b2dcec099baaf2f1f
Joe Pavitt <joepavitt@flowforge.com>
2022-05-10 11:38:37 +0100

    Update Dialogs: Admin > Template (save)

--- frontend/src/pages/admin/Template/dialogs/AdminTemplateSaveDialog.vue
+++ frontend/src/pages/admin/Template/dialogs/AdminTemplateSaveDialog.vue
@@ -1,43 +1,18 @@
 <template>
-    <TransitionRoot appear :show="isOpen" as="template">
-        <Dialog as="div" @close="close">
-            <div class="fixed inset-0 z-10 overflow-y-auto">
-                <div class="min-h-screen px-4 text-center">
-                    <DialogOverlay class="fixed inset-0 bg-black opacity-50" />
-                    <span class="inline-block h-screen align-middle" aria-hidden="true">
-                        &#8203;
-                    </span>
-                    <TransitionChild
-                        as="template"
-                        enter="duration-300 ease-out"
-                        enter-from="opacity-0 scale-95"
-                        enter-to="opacity-100 scale-100"
-                        leave="duration-200 ease-in"
-                        leave-from="opacity-100 scale-100"
-                        leave-to="opacity-0 scale-95"
-                    >
-                        <div class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-lg rounded">
-                            <DialogTitle as="h3" class="text-lg font-medium leading-6 text-red-700">Save template</DialogTitle>
-                            <form class="space-y-6">
-                                <div class="mt-2 space-y-2">
-                                    <p class="text-sm text-gray-500">
-                                        Are you sure you want to save this template?
-                                    </p>
-                                    <p class="text-sm text-gray-500">
-                                        Any projects using this template will need to be manually restarted to pick up any changes.
-                                    </p>
-                                </div>
-                                <div class="mt-4 flex flex-row justify-end">
-                                    <ff-button kind="secondary" @click="close">Cancel</ff-button>
-                                    <ff-button class="ml-4" @click="confirm">Save Template</ff-button>
-                                </div>
-                            </form>
-                        </div>
-                    </TransitionChild>
+    <ff-dialog :open="isOpen" header="Delete Stack" @close="close">
+        <template v-slot:default>
+            <form class="space-y-6">
+                <div class="space-y-2">
+                    <p>Are you sure you want to save this template?</p>
+                    <p>Any projects using this template will need to be manually restarted to pick up any changes.</p>
                 </div>
-            </div>
-        </Dialog>
-    </TransitionRoot>
+            </form>
+        </template>
+        <template v-slot:actions>
+            <ff-button kind="secondary" @click="close">Cancel</ff-button>
+            <ff-button kind="primary" @click="confirm">Save Template</ff-button>
+        </template>
+    </ff-dialog>
 </template>
 
 <script>
