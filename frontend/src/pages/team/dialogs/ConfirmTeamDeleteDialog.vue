f6700e83d288fbc379661564167a22984ecc4ab7
Joe Pavitt <joepavitt@flowforge.com>
2022-05-10 12:20:31 +0100

    Update Dialogs: Team > Users & Team > Settings > Danger

--- frontend/src/pages/team/dialogs/ConfirmTeamDeleteDialog.vue
+++ frontend/src/pages/team/dialogs/ConfirmTeamDeleteDialog.vue
@@ -1,67 +1,33 @@
 <template>
-    <TransitionRoot appear :show="isOpen" as="template">
-        <Dialog as="div" @close="close">
-            <div class="fixed inset-0 z-10 overflow-y-auto">
-                <div class="min-h-screen px-4 text-center">
-                    <DialogOverlay class="fixed inset-0 bg-black opacity-50" />
-                    <span class="inline-block h-screen align-middle" aria-hidden="true">
-                        &#8203;
-                    </span>
-                    <TransitionChild
-                        as="template"
-                        enter="duration-300 ease-out"
-                        enter-from="opacity-0 scale-95"
-                        enter-to="opacity-100 scale-100"
-                        leave="duration-200 ease-in"
-                        leave-from="opacity-100 scale-100"
-                        leave-to="opacity-0 scale-95"
-                    >
-                        <div class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-lg rounded">
-                            <DialogTitle as="h3" class="text-lg font-medium leading-6 text-red-700">Delete team</DialogTitle>
-                            <form class="space-y-6">
-                                <div class="mt-2 space-y-2">
-                                    <p class="text-sm text-gray-500">
-                                        Are you sure you want to delete this team? Once deleted, there is no going back.
-                                    </p>
-                                    <p class="text-sm text-gray-500">
-                                        Enter the team name <code class="block">{{team.name}}</code> to continue.
-                                    </p>
-                                </div>
-                                <FormRow v-model="input.teamName" id="projectName">Name</FormRow>
-                                <div class="mt-4 flex flex-row justify-end">
-                                    <ff-button kind="secondary" @click="close()">Cancel</ff-button>
-                                    <ff-button kind="danger" :disabled="!formValid" class="ml-4" @click="confirm()">Delete</ff-button>
-                                </div>
-                            </form>
-                        </div>
-                    </TransitionChild>
+    <ff-dialog :open="isOpen" header="Delete Team" @close="close">
+        <template v-slot:default>
+            <form class="space-y-6" v-if="team">
+                <div class="space-y-6">
+                    <p>
+                        Are you sure you want to delete this team? Once deleted, there is no going back.
+                    </p>
+                    <p>
+                        Enter the team name <code class="block">{{team.name}}</code> to continue.
+                    </p>
                 </div>
-            </div>
-        </Dialog>
-    </TransitionRoot>
+                <FormRow v-model="input.teamName" id="projectName">Name</FormRow>
+            </form>
+        </template>
+        <template v-slot:actions>
+            <ff-button kind="secondary" @click="close()">Cancel</ff-button>
+            <ff-button kind="danger" :disabled="!formValid" class="ml-4" @click="confirm()">Delete</ff-button>
+        </template>
+    </ff-dialog>
 </template>
 
 <script>
 import { ref } from 'vue'
-import {
-    TransitionRoot,
-    TransitionChild,
-    Dialog,
-    DialogOverlay,
-    DialogTitle
-} from '@headlessui/vue'
 
 import FormRow from '@/components/FormRow'
 
 export default {
     name: 'ConfirmProjectDeleteDialog',
-
     components: {
-        TransitionRoot,
-        TransitionChild,
-        Dialog,
-        DialogOverlay,
-        DialogTitle,
         FormRow
     },
     data () {
@@ -75,7 +41,7 @@ export default {
     },
     watch: {
         'input.teamName': function () {
-            this.formValid = this.team.name === this.input.teamName
+            this.formValid = this.team?.name === this.input.teamName
         }
     },
     methods: {
--- frontend/src/pages/team/dialogs/InviteMemberDialog.vue
+++ frontend/src/pages/team/dialogs/InviteMemberDialog.vue
@@ -1,53 +1,31 @@
 <template>
-    <TransitionRoot appear :show="isOpen" as="template">
-        <Dialog as="div" @close="close">
-            <div class="fixed inset-0 z-10 overflow-y-auto">
-                <div class="min-h-screen px-4 text-center">
-                    <DialogOverlay class="fixed inset-0 bg-black opacity-50" />
-                    <span class="inline-block h-screen align-middle" aria-hidden="true">
-                        &#8203;
-                    </span>
-                    <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
-                        <div class="inline-block w-full max-w-lg p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-lg rounded">
-                            <DialogTitle as="h3" class="text-lg font-medium leading-6">Add team members</DialogTitle>
-                            <form class="space-y-6" @submit.enter.prevent="">
-                                <div class="mt-2 space-y-2">
-                                    <template v-if="!responseErrors">
-                                        <p class="text-sm text-gray-500">
-                                            Invite a user to join the team.
-                                        </p>
-                                        <FormRow id="userInfo" v-model="input.userInfo" :error="errors.userInfo" :placeholder="'username'+(externalEnabled?' or email':'')"></FormRow>
-                                    </template>
-                                    <template v-else>
-                                        <ul>
-                                            <li class="text-sm" v-for="(value, name) in responseErrors" :key="name">
-                                                <span class="font-medium">{{name}}</span>: <span>{{value}}</span>
-                                            </li>
-                                        </ul>
-                                    </template>
-                                </div>
-                                <div class="mt-4 flex flex-row justify-end">
-                                    <ff-button kind="secondary" @click="close">Cancel</ff-button>
-                                    <ff-button :disabled="responseErrors || !input.userInfo.trim() || errors.userInfo" class="ml-4" @click="confirm">Invite</ff-button>
-                                </div>
-                            </form>
-                        </div>
-                    </TransitionChild>
+    <ff-dialog :open="isOpen" header="Invite Team Member" @close="close">
+        <template v-slot:default>
+            <form class="space-y-6" @submit.enter.prevent="">
+                <div class="space-y-2">
+                    <template v-if="!responseErrors">
+                        <p>Invite a user to join the team.</p>
+                        <FormRow id="userInfo" v-model="input.userInfo" :error="errors.userInfo" :placeholder="'username'+(externalEnabled?' or email':'')"></FormRow>
+                    </template>
+                    <template v-else>
+                        <ul>
+                            <li class="text-sm" v-for="(value, name) in responseErrors" :key="name">
+                                <span class="font-medium">{{name}}</span>: <span>{{value}}</span>
+                            </li>
+                        </ul>
+                    </template>
                 </div>
-            </div>
-        </Dialog>
-    </TransitionRoot>
+            </form>
+        </template>
+        <template v-slot:actions>
+            <ff-button kind="secondary" @click="close">Cancel</ff-button>
+            <ff-button :disabled="responseErrors || !input.userInfo.trim() || errors.userInfo" class="ml-4" @click="confirm">Invite</ff-button>
+        </template>
+    </ff-dialog>
 </template>
 
 <script>
 import { ref } from 'vue'
-import {
-    TransitionRoot,
-    TransitionChild,
-    Dialog,
-    DialogOverlay,
-    DialogTitle
-} from '@headlessui/vue'
 import { mapState } from 'vuex'
 import FormRow from '@/components/FormRow'
 import teamApi from '@/api/team'
@@ -55,11 +33,6 @@ import teamApi from '@/api/team'
 export default {
     name: 'InviteMemberDialog',
     components: {
-        TransitionRoot,
-        TransitionChild,
-        Dialog,
-        DialogOverlay,
-        DialogTitle,
         FormRow
     },
     props: ['team'],
