eee42ecb7bfdd9c66ab8d766dc539c3d1248afda
Joe Pavitt <joepavitt@flowforge.com>
2022-05-10 12:05:38 +0100

    Update Dialogs: Project > Settings

--- frontend/src/pages/project/Settings/dialogs/ConfirmProjectDeleteDialog.vue
+++ frontend/src/pages/project/Settings/dialogs/ConfirmProjectDeleteDialog.vue
@@ -1,68 +1,34 @@
 <template>
-    <TransitionRoot appear :show="isOpen" as="template">
-        <Dialog as="div" @close="close">
-            <div class="fixed inset-0 z-10 overflow-y-auto">
-                <div class="min-h-screen px-4 text-center">
-                    <DialogOverlay class="fixed inset-0 bg-black opacity-50" />
-                    <span class="inline-block h-screen align-middle" aria-hidden="true">
-                        &#8203;
-                    </span>
-                    <TransitionChild
-                        as="template"
-                        enter="duration-300 ease-out"
-                        enter-from="opacity-0 scale-95"
-                        enter-to="opacity-100 scale-100"
-                        leave="duration-200 ease-in"
-                        leave-from="opacity-100 scale-100"
-                        leave-to="opacity-0 scale-95"
-                    >
-                        <div class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-lg rounded">
-                            <DialogTitle as="h3" class="text-lg font-medium leading-6 text-red-700">Delete project</DialogTitle>
-                            <form class="space-y-6">
-                                <div class="mt-2 space-y-2">
-                                    <p class="text-sm text-gray-500">
-                                        Are you sure you want to delete this project? Once deleted, there is no going back.
-                                    </p>
-                                    <p class="text-sm text-gray-500">
-                                        Enter the project name to continue.
-                                        <code class="block">{{ project.name }}</code>
-                                    </p>
-                                </div>
-                                <FormRow v-model="input.projectName" id="projectName">Name</FormRow>
-                                <div class="mt-4 flex flex-row justify-end">
-                                    <ff-button kind="secondary" @click="close()">Cancel</ff-button>
-                                    <ff-button kind="danger" :disabled="!formValid" class="ml-4" @click="confirm()">Delete</ff-button>
-                                </div>
-                            </form>
-                        </div>
-                    </TransitionChild>
+    <ff-dialog header="Delete Project" :open="isOpen">
+        <template v-slot:default>
+            <form class="space-y-6">
+                <div class="mt-2 space-y-2">
+                    <p>
+                        Are you sure you want to delete this project? Once deleted, there is no going back.
+                    </p>
+                    <p>
+                        Enter the project name to continue.
+                        <code class="block">{{ project?.name }}</code>
+                    </p>
                 </div>
-            </div>
-        </Dialog>
-    </TransitionRoot>
+                <FormRow v-model="input.projectName" id="projectName">Name</FormRow>
+            </form>
+        </template>
+        <template v-slot:actions>
+            <ff-button kind="secondary" @click="close()">Cancel</ff-button>
+            <ff-button kind="danger" :disabled="!formValid" class="ml-4" @click="confirm()">Delete</ff-button>
+        </template>
+    </ff-dialog>
 </template>
 
 <script>
 import { ref } from 'vue'
-import {
-    TransitionRoot,
-    TransitionChild,
-    Dialog,
-    DialogOverlay,
-    DialogTitle
-} from '@headlessui/vue'
 
 import FormRow from '@/components/FormRow'
 
 export default {
     name: 'ConfirmProjectDeleteDialog',
-
     components: {
-        TransitionRoot,
-        TransitionChild,
-        Dialog,
-        DialogOverlay,
-        DialogTitle,
         FormRow
     },
     data () {
--- frontend/src/pages/project/Settings/dialogs/ExportProjectDialog.vue
+++ frontend/src/pages/project/Settings/dialogs/ExportProjectDialog.vue
@@ -1,52 +1,24 @@
 <template>
-    <TransitionRoot appear :show="isOpen" as="template">
-        <Dialog as="div" @close="close">
-            <div class="fixed inset-0 z-10 overflow-y-auto">
-                <div class="min-h-screen px-4 text-center">
-                    <DialogOverlay class="fixed inset-0 bg-black opacity-50" />
-                    <span class="inline-block h-screen align-middle" aria-hidden="true">
-                        &#8203;
-                    </span>
-                    <TransitionChild
-                        as="template"
-                        enter="duration-300 ease-out"
-                        enter-from="opacity-0 scale-95"
-                        enter-to="opacity-100 scale-100"
-                        leave="duration-200 ease-in"
-                        leave-from="opacity-100 scale-100"
-                        leave-to="opacity-0 scale-95"
-                    >
-                        <div class="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-lg rounded">
-                            <DialogTitle as="h3" class="text-lg font-medium leading-6 text-red-700">Export project</DialogTitle>
-                            <form class="space-y-6">
-                                <div class="mt-2 space-y-2">
-                                    <p class="text-sm text-gray-500">
-                                        Select project components to export:
-                                    </p>
-                                </div>
-                                <ExportProjectComponents id="exportSettings" v-model="parts" showSecret="true"/>
-                                <div class="mt-4 flex flex-row justify-end">
-                                    <ff-button kind="secondary" @click="close()">Cancel</ff-button>
-                                    <ff-button class="ml-4" @click="confirm()">Export project</ff-button>
-                                </div>
-                            </form>
-                        </div>
-                    </TransitionChild>
+    <ff-dialog header="Export Project" :open="isOpen">
+        <template v-slot:default>
+            <form class="space-y-6">
+                <div class="mt-2 space-y-2">
+                    <p class="text-sm text-gray-500">
+                        Select project components to export:
+                    </p>
                 </div>
-            </div>
-        </Dialog>
-    </TransitionRoot>
+                <ExportProjectComponents id="exportSettings" v-model="parts" showSecret="true"/>
+            </form>
+        </template>
+        <template v-slot:actions>
+            <ff-button kind="secondary" @click="close()">Cancel</ff-button>
+            <ff-button class="ml-4" @click="confirm()">Export project</ff-button>
+        </template>
+    </ff-dialog>
 </template>
 
 <script>
 import { ref } from 'vue'
-import {
-    TransitionRoot,
-    TransitionChild,
-    Dialog,
-    DialogOverlay,
-    DialogTitle
-} from '@headlessui/vue'
 
 // import FormRow from '@/components/FormRow'
 import ExportProjectComponents from '../../components/ExportProjectComponents'
@@ -54,11 +26,6 @@ import ExportProjectComponents from '../../components/ExportProjectComponents'
 export default {
     name: 'ExportProjectDialog',
     components: {
-        TransitionRoot,
-        TransitionChild,
-        Dialog,
-        DialogOverlay,
-        DialogTitle,
         ExportProjectComponents
     },
     data () {
