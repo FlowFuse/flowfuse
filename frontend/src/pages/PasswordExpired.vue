0ed5d5ff9669506180df1dfab7a7cdabc42669e9
Joe Pavitt <joepavitt@flowforge.com>
2022-05-11 09:41:49 +0100

    Branding updates & fix update expired password routing after success

--- frontend/src/pages/PasswordExpired.vue
+++ frontend/src/pages/PasswordExpired.vue
@@ -1,23 +1,16 @@
 <template>
-    <div class="mx-auto min-h-screen flex items-center justify-center bg-gray-50 py-12">
-        <div class="sm:w-72 w-xs w-screen space-y-2">
-            <div class="max-w-xs mx-auto w-full mb-4">
-                <FlowForgeLogo/>
-                <h2 class="mt-2 text-center text-3xl font-bold text-gray-900">
-                    <span>FLOW</span><span class="font-light">FORGE</span>
-                </h2>
-            </div>
-            <form class="px-4 sm:px-6 lg:px-8 mt-8 space-y-6">
-                <UpdateExpiredPassword />
-            </form>
-        </div>
-    </div>
+    <ff-layout-box>
+        <form class="px-4 sm:px-6 lg:px-8 mt-8 space-y-6">
+            <UpdateExpiredPassword />
+        </form>
+    </ff-layout-box>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import FlowForgeLogo from '@/components/Logo'
 import UpdateExpiredPassword from '@/components/auth/UpdateExpiredPassword'
+
+import FFLayoutBox from '@/layouts/Box'
 
 export default {
     name: 'PasswordExpired',
@@ -25,7 +18,7 @@ export default {
     },
     computed: mapState('account', ['user']),
     components: {
-        FlowForgeLogo,
+        'ff-layout-box': FFLayoutBox,
         UpdateExpiredPassword
     }
 }
