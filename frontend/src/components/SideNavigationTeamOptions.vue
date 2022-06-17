e38453470436b91debb35157b69861d4d4a70ffd
Joe Pavitt <joepavitt@flowforge.com>
2022-06-17 12:01:13 +0100

    Disable team selection when in nested view

--- frontend/src/components/SideNavigationTeamOptions.vue
+++ frontend/src/components/SideNavigationTeamOptions.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="team" class="ff-side-navigation" :class="{'minimised': nested}">
         <div class="ff-side-navigation--primary">
-            <ff-team-selection></ff-team-selection>
+            <ff-team-selection :list-enabled="!nested" @click="teamSelectClick()"></ff-team-selection>
             <!-- Team Options: General -->
             <ul class="ff-side-navigation--options">
                 <router-link v-for="route in routes.general" :key="route.label"
@@ -148,6 +148,16 @@ export default {
                 name: 'CreateTeam'
             })
             this.switchTeam()
+        },
+        teamSelectClick () {
+            if (this.nested) {
+                this.$router.push({
+                    name: 'Team',
+                    params: {
+                        team_slug: this.team.slug
+                    }
+                })
+            }
         }
     }
 }
