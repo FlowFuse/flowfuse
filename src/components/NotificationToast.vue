82cd5b0ae3724103db293ff5b2e1830b36e17266
Joe Pavitt <joepavitt@flowforge.com>
2022-06-17 17:49:26 +0100

    Add ff-notification-pill and ff-notification-toast

---
+++ src/components/NotificationToast.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="ff-notification-toast" :class="'ff-notification-toast--' + type">
+        <div class="ff-notification-toast--message">
+            <div>
+                <slot name="message">
+                    {{ message }}
+                </slot>
+            </div>
+            <XIcon v-if="showClose" class="ff-icon ff-notification-toast--close"/>
+        </div>
+        <div v-if="showActions" class="ff-notification-toast--actions">
+            <slot name="actions"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { XIcon } from '@heroicons/vue/solid'
+
+export default {
+    name: 'ff-notification-toast',
+    emits: ['close'],
+    props: {
+        message: {
+            default: null,
+            type: String
+        },
+        type: {
+            default: 'info',
+            type: String
+        },
+        showClose: {
+            default: true,
+            type: Boolean
+        }
+    },
+    components: {
+        XIcon
+    },
+    computed: {
+        showActions: function () {
+            return this.$slots.actions
+        }
+    }
+}
+</script>
