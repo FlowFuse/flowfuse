2f1961686ff5da1d79cc478c6bcf1607b2c665e6
Nick O'Leary <nick.oleary@gmail.com>
2022-06-24 13:37:33 +0100

    Ensure ff-dialog doesn't extend off screen

--- components/DialogBox.vue
+++ components/DialogBox.vue
@@ -2,14 +2,16 @@
     <div class="ff-dialog-container" :class="'ff-dialog-container--' + (open ? 'open' : 'closed')">
         <div class="ff-dialog-box">
             <div class="ff-dialog-header">{{ header }}</div>
-            <div class="ff-dialog-content">
-                <slot></slot>
-            </div>
-            <div class="ff-dialog-actions">
-                <slot name="actions">
-                    <ff-button @click="$emit('cancel')" kind="secondary">Cancel</ff-button>
-                    <ff-button @click="$emit('confirm')">Confirm</ff-button>
-                </slot>
+            <div class="ff-dialog-body">
+                <div class="ff-dialog-content">
+                    <slot></slot>
+                </div>
+                <div class="ff-dialog-actions">
+                    <slot name="actions">
+                        <ff-button @click="$emit('cancel')" kind="secondary">Cancel</ff-button>
+                        <ff-button @click="$emit('confirm')">Confirm</ff-button>
+                    </slot>
+                </div>
             </div>
         </div>
         <div class="ff-dialog-backdrop">
