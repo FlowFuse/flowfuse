d50b5347bc1c5ed9e81de21fe788b9dad7ae78e6
Ben Hardill <b.hardill@gmail.com>
2022-07-26 15:41:34 +0100

    Fix build-docs error
    Last PR looked like it missed a bit

--- components/form/TileSelectionOption.vue
+++ components/form/TileSelectionOption.vue
@@ -64,8 +64,7 @@ export default {
     },
     components: {
         CheckCircleIcon,
-        PencilAltIcon,
-        MarkdownViewer
+        PencilAltIcon
     },
     data () {
         return {
