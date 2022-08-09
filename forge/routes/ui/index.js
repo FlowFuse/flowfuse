b21bc9f4212efb27410f8281ba691d18dc5b9219
Joe Pavitt <joepavitt@flowforge.com>
2022-08-09 10:56:52 +0100

    Broker posthog options (capture_pageview) from flowforge.yml

--- forge/routes/ui/index.js
+++ forge/routes/ui/index.js
@@ -26,7 +26,6 @@ module.exports = async function (app) {
      * feConfig - the 'frontend' portion of our flowforge.yml
      */
     async function injnectAnalytics (feConfig) {
-        console.log(feConfig)
         if (!cachedIndex) {
             const filepath = path.join(frontendAssetsDir, 'index.html')
             const data = await fsp.readFile(filepath, 'utf8')
@@ -42,10 +41,16 @@ module.exports = async function (app) {
 
             if (feConfig.posthog?.apikey) {
                 const apikey = feConfig.posthog.apikey
-                console.log(apikey)
+                const options = {
+                    api_host: 'https://app.posthog.com'
+                }
+                if ('capture_pageview' in feConfig.posthog) {
+                    options.capture_pageview = feConfig.posthog.capture_pageview
+                }
+                // TODO: object to string in the injection script
                 injection += `<script>
                 !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-                posthog.init('phc_U1yxTYD5mcd3yyQ8HGQQtwBjPfIeQrcuasvhm7RH367', { api_host: 'https://app.posthog.com', capture_pageview: false })
+                posthog.init('${apikey}', ${JSON.stringify(options)})
             </script>`
             }
 
@@ -74,7 +79,6 @@ module.exports = async function (app) {
         }
         // check if we need to inject plausible
         if (app.config.telemetry.frontend) {
-            console.log('found frontend config settings')
             const injectedContent = await injnectAnalytics(app.config.telemetry.frontend) // injectPlausible(app.config.telemetry.frontend.plausible.domain, app.config.telemetry.frontend.plausible.extension)
             reply.type('text/html').send(injectedContent)
         } else {
@@ -102,7 +106,6 @@ module.exports = async function (app) {
         // }
         // check if we need to inject plausible
         if (app.config.telemetry.frontend) {
-            console.log('found frontend config settings')
             const injectedContent = await injnectAnalytics(app.config.telemetry.frontend) // injectPlausible(app.config.telemetry.frontend.plausible.domain, app.config.telemetry.frontend.plausible.extension)
             reply.type('text/html').send(injectedContent)
         } else {
