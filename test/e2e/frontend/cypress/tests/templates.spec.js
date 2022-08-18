4fab07cc697251cc46397a6b9fc258feca09f40e
Steve-Mcl <sdmclaughlin@gmail.com>
2022-08-18 18:17:51 +0100

    add test can not delete template if in use

--- test/e2e/frontend/cypress/tests/templates.spec.js
+++ test/e2e/frontend/cypress/tests/templates.spec.js
@@ -42,4 +42,27 @@ describe('FlowForge - Templates', () => {
         // check it has been deleted
         cy.get('[data-el="templates"] tbody').find('tr').should('have.length', 1)
     })
+
+    it('can not delete a template from the table if template is in use', () => {
+        cy.intercept('DELETE', '/api/*/templates/*').as('deleteTemplate')
+        // add a template that we will then delete
+        cy.request('POST', '/api/v1/templates', { name: 'New Template', settings: { disableEditor: false, httpAdminRoot: '', codeEditor: 'monaco', theme: 'forge-light', page: { title: 'FlowForge', favicon: '' }, header: { title: 'FlowForge', url: '' }, timeZone: 'UTC', palette: { allowInstall: true, nodesExcludes: '', denyList: [] }, modules: { allowInstall: true, denyList: [] }, env: [] }, policy: { disableEditor: false, httpAdminRoot: false, codeEditor: false, theme: false, page: { title: false, favicon: false }, header: { title: false, url: false }, timeZone: false, palette: { allowInstall: false, nodesExcludes: false, denyList: false }, modules: { allowInstall: false, denyList: false } } })
+
+        cy.visit('/admin/templates')
+
+        // wait until two Stacks API calls complete
+        cy.intercept('GET', '/api/*/templates?*').as('getTemplates')
+        cy.wait('@getTemplates')
+
+        // check we have two templates
+        cy.get('[data-el="templates"] tbody').find('tr').should('have.length', 2)
+
+        cy.get('[data-el="templates"] tbody').find('.ff-kebab-menu').eq(0).click()
+        cy.get('[data-el="templates"] tbody .ff-kebab-menu .ff-kebab-options').find('.ff-list-item').eq(1).click()
+
+        cy.get('.ff-dialog-box').should('be.visible')
+
+        // check that the "Delete" button is disabled
+        cy.get('.ff-dialog-box button.ff-btn.ff-btn--danger').contains('Delete').should('be.disabled')
+    })
 })
