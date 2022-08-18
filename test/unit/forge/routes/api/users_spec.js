bf5b121637f4bf05c359225b0d0e53e857dbc85c
Steve-Mcl <sdmclaughlin@gmail.com>
2022-08-18 11:20:58 +0100

    Permit admin to manually verify users email fixes #692

--- test/unit/forge/routes/api/users_spec.js
+++ test/unit/forge/routes/api/users_spec.js
@@ -14,6 +14,7 @@ describe('Users API', async function () {
         // bob : admin, team owner
         // chris (team owner)
         // dave <-- the only user who can be cleanly deleted
+        // elvis <-- this user doesn't have email_verified
 
         // ATeam ( alice  (owner), bob (owner), chris)
         // BTeam ( bob (owner), chris, dave)
@@ -21,29 +22,36 @@ describe('Users API', async function () {
 
         // Alice create in setup()
         TestObjects.alice = await app.db.models.User.byUsername('alice')
-        TestObjects.bob = await app.db.models.User.create({ username: 'bob', name: 'Bob Solo', email: 'bob@example.com', email_verified: true, password: 'bbPassword', admin: true })
+        TestObjects.bob = await app.db.models.User.create({ username: 'bob', name: 'Bob Solo', email: 'bob@example.com', email_verified: true, password: 'bbPassword' })
         TestObjects.chris = await app.db.models.User.create({ username: 'chris', name: 'Chris Kenobi', email: 'chris@example.com', email_verified: true, password: 'ccPassword' })
         TestObjects.dave = await app.db.models.User.create({ username: 'dave', name: 'Dave Vader', email: 'dave@example.com', email_verified: true, password: 'ddPassword' })
+        TestObjects.elvis = await app.db.models.User.create({ username: 'elvis', name: 'Elvis Dooku', email: 'elvis@example.com', email_verified: false, password: 'eePassword' })
 
         // ATeam create in setup()
         TestObjects.ATeam = await app.db.models.Team.byName('ATeam')
         TestObjects.BTeam = await app.db.models.Team.create({ name: 'BTeam' })
         TestObjects.CTeam = await app.db.models.Team.create({ name: 'CTeam' })
+        TestObjects.DTeam = await app.db.models.Team.create({ name: 'DTeam' })
 
         // Alice set as ATeam owner in setup()
         await TestObjects.ATeam.addUser(TestObjects.bob, { through: { role: Roles.Owner } })
+        await TestObjects.ATeam.addUser(TestObjects.elvis, { through: { role: Roles.Member } })
         await TestObjects.ATeam.addUser(TestObjects.chris, { through: { role: Roles.Member } })
         await TestObjects.BTeam.addUser(TestObjects.bob, { through: { role: Roles.Owner } })
         await TestObjects.BTeam.addUser(TestObjects.chris, { through: { role: Roles.Member } })
         await TestObjects.BTeam.addUser(TestObjects.dave, { through: { role: Roles.Member } })
         await TestObjects.CTeam.addUser(TestObjects.chris, { through: { role: Roles.Owner } })
         await TestObjects.CTeam.addUser(TestObjects.dave, { through: { role: Roles.Member } })
+        await TestObjects.DTeam.addUser(TestObjects.dave, { through: { role: Roles.Owner } })
+        await TestObjects.DTeam.addUser(TestObjects.chris, { through: { role: Roles.Member } })
+        await TestObjects.DTeam.addUser(TestObjects.elvis, { through: { role: Roles.Member } })
 
         TestObjects.tokens = {}
         await login('alice', 'aaPassword')
         await login('bob', 'bbPassword')
         await login('chris', 'ccPassword')
         await login('dave', 'ddPassword')
+        await login('elvis', 'eePassword')
     })
 
     async function login (username, password) {
@@ -121,6 +129,87 @@ describe('Users API', async function () {
                 result.should.have.property('error')
             })
         })
+        describe('Modify a user', async function () {
+            // PUT /api/v1/users/:userId  (sharedUser.updateUser)
+            it('admin can manually verify email', async function () {
+                const response = await app.inject({
+                    method: 'PUT',
+                    url: `/api/v1/users/${TestObjects.elvis.hashid}`,
+                    payload: {
+                        email_verified: true
+                    },
+                    cookies: { sid: TestObjects.tokens.alice }
+                })
+                response.statusCode.should.equal(200)
+                const result = response.json()
+                result.should.have.property('email_verified', true)
+                result.should.not.have.property('error')
+            })
+            it('team owner can not manually verify email', async function () {
+                const response = await app.inject({
+                    method: 'PUT',
+                    url: `/api/v1/users/${TestObjects.elvis.hashid}`,
+                    payload: {
+                        email_verified: true
+                    },
+                    cookies: { sid: TestObjects.tokens.dave }
+                })
+                response.statusCode.should.equal(401)
+                const result = response.json()
+                result.should.have.property('error')
+            })
+            it('member can not manually verify own email', async function () {
+                const response = await app.inject({
+                    method: 'PUT',
+                    url: `/api/v1/users/${TestObjects.elvis.hashid}`,
+                    payload: {
+                        email_verified: true
+                    },
+                    cookies: { sid: TestObjects.tokens.elvis }
+                })
+                response.statusCode.should.equal(401)
+                const result = response.json()
+                result.should.have.property('error')
+            })
+            it('team member can not manually verify a users email', async function () {
+                const response = await app.inject({
+                    method: 'PUT',
+                    url: `/api/v1/users/${TestObjects.elvis.hashid}`,
+                    payload: {
+                        email_verified: true
+                    },
+                    cookies: { sid: TestObjects.tokens.chris }
+                })
+                response.statusCode.should.equal(401)
+                const result = response.json()
+                result.should.have.property('error')
+            })
+            it('other team owner can not manually verify a users email', async function () {
+                const response = await app.inject({
+                    method: 'PUT',
+                    url: `/api/v1/users/${TestObjects.elvis.hashid}`,
+                    payload: {
+                        email_verified: true
+                    },
+                    cookies: { sid: TestObjects.tokens.bob }
+                })
+                response.statusCode.should.equal(401)
+                const result = response.json()
+                result.should.have.property('error')
+            })
+            it('anonymous can not manually verify a users email', async function () {
+                const response = await app.inject({
+                    method: 'PUT',
+                    url: `/api/v1/users/${TestObjects.elvis.hashid}`,
+                    payload: {
+                        email_verified: true
+                    }
+                })
+                response.statusCode.should.equal(401)
+                const result = response.json()
+                result.should.have.property('error')
+            })
+        })
     })
 
     describe('Delete a user', async function () {
@@ -181,72 +270,83 @@ describe('Users API', async function () {
                 cookies: { sid: TestObjects.tokens.dave }
             })
             response.statusCode.should.equal(200)
-            await app.inject({
+            const deleteResult = await app.inject({
                 method: 'DELETE',
-                url: `/api/v1/users/${TestObjects.dave.hashid}`,
+                url: `/api/v1/users/${TestObjects.elvis.hashid}`,
                 cookies: { sid: TestObjects.tokens.alice }
             })
+            deleteResult.statusCode.should.equal(200)
             const postDeleteResponse = await app.inject({
                 method: 'GET',
                 url: '/api/v1/user',
-                cookies: { sid: TestObjects.tokens.dave }
+                cookies: { sid: TestObjects.tokens.elvis }
             })
             postDeleteResponse.statusCode.should.equal(401)
         })
 
         it('Deleting a user removes pending invites for them', async function () {
-            // Alice invites Dave to TeamA
-            // Delete Dave
+            // Alice invites Elvis to TeamB
+            // Delete Elvis
             await app.inject({
                 method: 'POST',
-                url: `/api/v1/teams/${TestObjects.ATeam.hashid}/invitations`,
+                url: `/api/v1/teams/${TestObjects.BTeam.hashid}/invitations`,
                 cookies: { sid: TestObjects.tokens.alice },
                 payload: {
-                    user: 'dave'
+                    user: 'elvis'
                 }
             })
             const inviteListA = (await app.inject({
                 method: 'GET',
-                url: `/api/v1/teams/${TestObjects.ATeam.hashid}/invitations`,
+                url: `/api/v1/teams/${TestObjects.BTeam.hashid}/invitations`,
                 cookies: { sid: TestObjects.tokens.alice }
             })).json()
             inviteListA.should.have.property('count', 1)
-
-            await app.inject({
+            const deleteResult = await app.inject({
                 method: 'DELETE',
-                url: `/api/v1/users/${TestObjects.dave.hashid}`,
+                url: `/api/v1/users/${TestObjects.elvis.hashid}`,
                 cookies: { sid: TestObjects.tokens.alice }
             })
+            deleteResult.statusCode.should.equal(200)
             const inviteListB = (await app.inject({
                 method: 'GET',
-                url: `/api/v1/teams/${TestObjects.ATeam.hashid}/invitations`,
+                url: `/api/v1/teams/${TestObjects.BTeam.hashid}/invitations`,
                 cookies: { sid: TestObjects.tokens.alice }
             })).json()
             inviteListB.should.have.property('count', 0)
         })
 
         it('Deleting a user removes them from all teams they are in', async function () {
-            // Dave is in BTeam and CTeam
-            // - delete Dave - check the member lists
+            // elvis is in ATeam and DTeam
+            // - delete elvis - check the member lists
 
-            const countTeamMembers = async (teamId) => {
+            const getTeamMembers = async (teamId) => {
                 return (await app.inject({
                     method: 'GET',
                     url: `/api/v1/teams/${teamId}/members`,
                     cookies: { sid: TestObjects.tokens.alice }
-                })).json().count
+                })).json()
             }
-            ;(await countTeamMembers(TestObjects.BTeam.hashid)).should.equal(3)
-            ;(await countTeamMembers(TestObjects.CTeam.hashid)).should.equal(2)
+            const membersBeforeA = await getTeamMembers(TestObjects.ATeam.hashid)
+            const membersBeforeD = await getTeamMembers(TestObjects.DTeam.hashid)
+            membersBeforeA.should.have.property('count', 4)
+            membersBeforeD.should.have.property('count', 3)
+            // ensure elvis was actually added
+            membersBeforeA.members.filter(e => e.username === 'elvis').should.have.property('length', 1)
+            membersBeforeD.members.filter(e => e.username === 'elvis').should.have.property('length', 1)
 
             await app.inject({
                 method: 'DELETE',
-                url: `/api/v1/users/${TestObjects.dave.hashid}`,
+                url: `/api/v1/users/${TestObjects.elvis.hashid}`,
                 cookies: { sid: TestObjects.tokens.alice }
             })
 
-            ;(await countTeamMembers(TestObjects.BTeam.hashid)).should.equal(2)
-            ;(await countTeamMembers(TestObjects.CTeam.hashid)).should.equal(1)
+            const membersAfterA = await getTeamMembers(TestObjects.ATeam.hashid)
+            const membersAfterD = await getTeamMembers(TestObjects.DTeam.hashid)
+            membersAfterA.should.have.property('count', 3)
+            membersAfterD.should.have.property('count', 2)
+            // ensure elvis was actually removed
+            membersAfterA.members.filter(e => e.username === 'elvis').should.have.property('length', 0)
+            membersAfterA.members.filter(e => e.username === 'elvis').should.have.property('length', 0)
         })
     })
 })
