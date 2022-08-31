90f77693e763a935eaf1a7f62e42269b70551d27
Steve-Mcl <sdmclaughlin@gmail.com>
2022-08-31 12:12:00 +0100

    make new col an index and split migration to 2 steps

---
+++ forge/db/migrations/20220826-02-update-project-safeName.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+/**
+ * Update safeName column in Projects table
+ */
+const { DataTypes, QueryInterface } = require('sequelize')
+
+module.exports = {
+    /**
+     * upgrade database
+     * @param {QueryInterface} context Sequelize.QueryInterface
+     */
+    up: async (context) => {
+        await context.sequelize.query('UPDATE Projects SET "safeName" = LOWER("name") WHERE "safeName" IS NULL;')
+        await context.addIndex('Projects', { name: 'projects_safe_name_unique', fields: ['safeName'], unique: true })
+    },
+    down: async (context) => {
+    }
+}
