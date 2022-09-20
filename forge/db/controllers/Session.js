de6c1307517e7f1a947f9be17d8bab40c92448ff
Nick O'Leary <nick.oleary@gmail.com>
2022-09-20 17:19:11 +0100

    Update idle grace period back to 31 hours

--- forge/db/controllers/Session.js
+++ forge/db/controllers/Session.js
@@ -7,7 +7,7 @@ const DEFAULT_WEB_SESSION_EXPIRY = HOUR * 24 * 7 // One week expiry
 const DEFAULT_WEB_SESSION_IDLE_TIMEOUT = HOUR * 32 // 32 hours
 // We only update the idle time if there is activity within this period prior
 // to idle timeout. That avoids the need to update idle timeout on every single request
-const DEFAULT_WEB_SESSION_IDLE_GRACE = HOUR * 6 // 6 hours
+const DEFAULT_WEB_SESSION_IDLE_GRACE = HOUR * 31 // 31 hours
 
 const DEFAULT_TOKEN_SESSION_EXPIRY = 1000 * 60 * 30 // 30 mins session - with refresh token support
 
