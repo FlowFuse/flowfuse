f91af2731a094da0a06c36e2a536e0ba1d49181c
Nick O'Leary <nick.oleary@gmail.com>
2022-09-22 18:56:18 +0100

    Add 'leave team' option on User Settings/Teams view

--- frontend/src/components/tables/cells/TeamCell.vue
+++ frontend/src/components/tables/cells/TeamCell.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex align-center">
+    <div class="flex align-center cursor-pointer" @click='goToTeam'>
         <div class="flex flex-col justify-center"><img class="rounded-md mr-3 w-6 inline" :src="avatar"/></div>
         <div class="inline-flex flex-col">
             <div>{{ name }}</div>
@@ -10,6 +10,13 @@
 <script>
 export default {
     name: 'TeamCell',
-    props: ['avatar', 'name', 'id']
+    props: ['avatar', 'name', 'id', 'slug'],
+    methods: {
+        goToTeam () {
+            if (this.slug) {
+                this.$router.push({ name: 'Team', params: { team_slug: this.slug } })
+            }
+        }
+    }
 }
 </script>
