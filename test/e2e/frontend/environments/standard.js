b551eafaac9873ad60232b93411dd62e6577ab8e
Pez Cuckow <email@pezcuckow.com>
2022-10-12 14:05:39 +0200

    E2E Tests: Create a second team with a project

--- test/e2e/frontend/environments/standard.js
+++ test/e2e/frontend/environments/standard.js
@@ -26,9 +26,9 @@ module.exports = async function (settings = {}, config = {}) {
     const forge = await Forge({ config })
 
     await forge.settings.set('setup:initialised', true)
-    // full platform & team admin
+    // full platform & team1 admin
     const userAlice = await forge.db.models.User.create({ admin: true, username: 'alice', name: 'Alice Skywalker', email: 'alice@example.com', email_verified: true, password: 'aaPassword' })
-    // team admin, not platform admin
+    // team1 & team2 admin, not platform admin
     const userBob = await forge.db.models.User.create({ admin: false, username: 'bob', name: 'Bob Solo', email: 'bob@example.com', email_verified: true, password: 'bbPassword' })
     // no admin rights
     // eslint-disable-next-line no-unused-vars
@@ -43,6 +43,9 @@ module.exports = async function (settings = {}, config = {}) {
     await team1.addUser(userAlice, { through: { role: Roles.Owner } })
     await team1.addUser(userBob, { through: { role: Roles.Owner } })
     // await team1.addUser(userCharlie, { through: { role: Roles.Member } })
+
+    const team2 = await forge.db.models.Team.create({ name: 'BTeam', TeamTypeId: defaultTeamType.id })
+    await team2.addUser(userBob, { through: { role: Roles.Owner } })
 
     const templateProperties = {
         name: 'template1',
@@ -73,10 +76,36 @@ module.exports = async function (settings = {}, config = {}) {
     }
     const stack = await forge.db.models.ProjectStack.create(stackProperties)
     await stack.setProjectType(projectType)
+
     const project1 = await forge.db.models.Project.create({ name: 'project1', type: '', url: '' })
     await team1.addProject(project1)
     await project1.setProjectStack(stack)
     await project1.setProjectTemplate(template)
+    await project1.setProjectType(projectType)
+    await project1.reload({
+        include: [
+            { model: forge.db.models.Team },
+            { model: forge.db.models.ProjectType },
+            { model: forge.db.models.ProjectStack },
+            { model: forge.db.models.ProjectTemplate }
+        ]
+    })
+    await forge.containers.start(project1) // ensure project is initialized
+
+    const project2 = await forge.db.models.Project.create({ name: 'project2', type: '', url: '' })
+    await team2.addProject(project2)
+    await project2.setProjectStack(stack)
+    await project2.setProjectTemplate(template)
+    await project2.setProjectType(projectType)
+    await project2.reload({
+        include: [
+            { model: forge.db.models.Team },
+            { model: forge.db.models.ProjectType },
+            { model: forge.db.models.ProjectStack },
+            { model: forge.db.models.ProjectTemplate }
+        ]
+    })
+    await forge.containers.start(project2) // ensure project is initialized
 
     forge.project = project1
     forge.template = template
