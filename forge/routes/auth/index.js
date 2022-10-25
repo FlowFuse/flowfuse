031ffb288db38d0e38a3959dd1b7e5b09b2a78c2
Steve-Mcl <sdmclaughlin@gmail.com>
2022-10-25 11:30:38 +0100

    use verified user ID if no session user

--- forge/routes/auth/index.js
+++ forge/routes/auth/index.js
@@ -378,7 +378,7 @@ module.exports = fp(async function (app, opts, done) {
                     slug: verifiedUser.username,
                     TeamTypeId: (await app.db.models.TeamType.byName('starter')).id
                 }, verifiedUser)
-                await userLog(request.session?.User?.id, 'verify.auto-create-team', {
+                await userLog(request.session?.User?.id || verifiedUser.id, 'verify.auto-create-team', {
                     team: {
                         name: `Team ${verifiedUser.name}`,
                         type: 'starter'
@@ -406,7 +406,7 @@ module.exports = fp(async function (app, opts, done) {
                     email: verifiedUser.email,
                     admin: !!verifiedUser.isAdmin
                 }
-            })
+            }, verifiedUser?.id)
             reply.send({ status: 'okay' })
         } catch (err) {
             app.log.error(`/account/verify/token error - ${err.toString()}`)
