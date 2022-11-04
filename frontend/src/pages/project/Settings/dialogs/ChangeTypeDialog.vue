8a2e9e8327e0decde4dee71faae713f0b6614e7b
Joe Pavitt <joepavitt@flowforge.com>
2022-11-04 13:23:33 +0000

    Update Project's ChangeTypeDialog to use ff-tile-selection

--- frontend/src/pages/project/Settings/dialogs/ChangeTypeDialog.vue
+++ frontend/src/pages/project/Settings/dialogs/ChangeTypeDialog.vue
@@ -5,17 +5,13 @@
                 <p>
                     Select the type for your project:
                 </p>
-                <ul class="flex flex-wrap gap-1 items-stretch">
-                    <li v-for="(projType, index) in projectTypes" :key="index">
-                        <ProjectTypeSummary :projectType="projType">
-                            <template v-slot:header>
-                                <div class="absolute">
-                                    <input type="radio" name="project-type" :value="projType.id" v-model="input.projectType">
-                                </div>
-                            </template>
-                        </ProjectTypeSummary>
-                    </li>
-                </ul>
+                <ff-tile-selection v-model="input.projectType">
+                    <ff-tile-selection-option v-for="(projType, index) in projectTypes" :key="index"
+                                              :label="projType.name" :description="projType.description"
+                                              :price="projType.properties?.billingDescription?.split('/')[0]"
+                                              :price-interval="projType.properties?.billingDescription?.split('/')[1]"
+                                              :value="projType.id"/>
+                </ff-tile-selection>
             </form>
         </template>
     </ff-dialog>
@@ -24,18 +20,14 @@
 <script>
 
 import projectTypesApi from '@/api/projectTypes'
-import ProjectTypeSummary from '../../../team/components/ProjectTypeSummary'
 
 export default {
     name: 'ChangeTypeDialog',
     emits: ['confirm'],
-    components: {
-        ProjectTypeSummary
-    },
     data () {
         return {
             input: {
-                projectType: ''
+                projectType: null
             },
             projectTypes: [],
             project: null
@@ -58,7 +50,7 @@ export default {
             async show (project) {
                 this.$refs.dialog.show()
                 this.project = project
-                this.input.projectType = this.project.projectType
+                this.input.projectType = this.project.projectType.id
                 const projectTypes = await projectTypesApi.getProjectTypes()
                 this.projectTypes = projectTypes.types
             }
