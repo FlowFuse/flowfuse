f9312970469622f48448827da1f9d2f49bcbf45f
Joe Pavitt <joepavitt@flowforge.com>
2022-11-17 17:15:43 +0000

    Remove app injection into audit-logging/team

--- forge/lib/audit-logging/team.js
+++ forge/lib/audit-logging/team.js
@@ -105,9 +105,9 @@ const log = async (event, actionedBy, teamId, body) => {
     const trigger = triggerObject(actionedBy)
     if (typeof trigger?.id === 'number' && trigger?.id <= 0) {
         body.trigger = trigger // store trigger in body since it's not a real user
-        await app.db.controllers.AuditLog.teamLog(app, teamId, null, event, body)
+        await app.db.controllers.AuditLog.teamLog(teamId, null, event, body)
     } else {
-        await app.db.controllers.AuditLog.teamLog(app, teamId, trigger.id, event, body)
+        await app.db.controllers.AuditLog.teamLog(teamId, trigger.id, event, body)
     }
 }
 
