bdf685fd959c5ab8fb50f7dfbe08c3a626c71ae2
Steve-Mcl <sdmclaughlin@gmail.com>
2022-11-19 17:01:17 +0000

    Notify user of Password Change fixes #814

---
+++ forge/postoffice/templates/PasswordChanged.js
@@ -0,0 +1,15 @@
+module.exports = {
+    subject: 'Password Change Notification for FlowForge',
+    text:
+`Hello
+
+Your password has been changed on the FlowForge platform.
+
+If you did not request this, please contact your system administrator.
+`,
+    html:
+`<p>Hello</p>
+<p>Your password has been changed on the FlowForge platform.</p>
+<p>If you did not request this, please contact your system administrator.</p>
+`
+}
