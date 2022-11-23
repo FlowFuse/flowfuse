ae3ec133f9d4f86c479d8f3303a2dd676120b509
Joe Pavitt <joepavitt@flowforge.com>
2022-11-23 14:03:27 +0000

    Add ff-help-tooltip

--- directives/Tooltip.js
+++ directives/Tooltip.js
@@ -1,6 +1,7 @@
 const directive = {
     name: 'ff-tooltip',
     mounted: (el, binding) => {
+        console.log(el, binding)
         if (el && binding) {
             el.classList.add('ff-tooltip-container')
             let posClass = 'ff-tooltip-right'
