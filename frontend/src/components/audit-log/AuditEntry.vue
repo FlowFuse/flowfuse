e61b514671ce40a44df1c5c0d24dc809b974e63c
Joe Pavitt <joepavitt@flowforge.com>
2022-11-30 17:04:31 +0000

    Add new Audit Log components

---
+++ frontend/src/components/audit-log/AuditEntry.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="ff-audit-entry ml-8 flex py-3 border-b text-base">
+        <!-- Time -->
+        <div class="w-20 text-gray-500">{{ entry.time }}</div>
+        <!-- Icon -->
+        <div class="w-12 flex justify-center">
+            <AuditEntryIcon :event="entry.event"></AuditEntryIcon>
+        </div>
+        <!-- Event -->
+        <div class="flex-grow flex flex-col">
+            <AuditEntryVerbose :entry="entry"></AuditEntryVerbose>
+        </div>
+        <!-- User/Trigger -->
+        <div v-if="entry.trigger.name !== 'unknown'" class="ff-audit-entry-trigger">
+            <div>{{ entry.trigger.name }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import AuditEntryIcon from './AuditEntryIcon.vue'
+import AuditEntryVerbose from './AuditEntryVerbose.vue'
+
+export default {
+    name: 'AuditLogEntry',
+    props: {
+        entry: {
+            required: true,
+            type: Object
+        }
+    },
+    components: {
+        AuditEntryIcon,
+        AuditEntryVerbose
+    }
+}
+</script>
---
+++ frontend/src/components/audit-log/AuditLog.vue
@@ -0,0 +1,118 @@
+<template>
+    <ff-loading v-if="loading" message="Loading Activity..." />
+    <template v-else-if="logEntries.length > 0">
+        <ul class="mx-auto">
+            <li v-for="item in logEntries" :key="item.id">
+                <div v-if="item.date" class="font-medium mt-2 mb-1">{{item.date}}</div>
+                <AuditEntry :entry="item"></AuditEntry>
+            </li>
+            <li v-if="showLoadMore !== false && nextCursor" class="px-8 py-4">
+                <a v-if="!loading" @click.stop="loadMore" class="forge-button-inline">Load more...</a>
+                <div class="text-gray-500" v-else>Loading...</div>
+            </li>
+        </ul>
+    </template>
+</template>
+
+<script>
+/* eslint-disable no-template-curly-in-string */
+
+import AuditEntry from './AuditEntry.vue'
+
+const eventDescriptions = {
+    'project.created': 'Project created',
+    'project.duplicated': 'Project duplicated',
+    'project.deleted': 'Project deleted',
+    'project.stopped': 'Project stopped',
+    'project.started': 'Project started',
+    'auth.login': '${user} logged in',
+    'auth.login.revoke': '${user} logged out',
+    'flows.set': 'Flows updated',
+
+    // Project Log
+    'user.invited': 'User invited',
+    'user.uninvited': 'User uninvited',
+    'user.invite.accept': '${user} accepted invitation',
+    'user.invite.reject': '${user} declined invitation'
+
+}
+
+const eventIcons = {
+    'project.started': 'play',
+    'project.stopped': 'stop',
+    'project.created': 'create',
+    'project.duplicated': 'create',
+    'project.deleted': 'create',
+    'auth.login': 'user',
+    'auth.login.revoke': 'logout',
+    'flows.set': 'pencil'
+}
+
+export default {
+    name: 'AuditLog',
+    props: ['entity', 'loadItems', 'showLoadMore'],
+    watch: {
+        entity: 'fetchData'
+    },
+    data () {
+        return {
+            nextCursor: null,
+            logEntries: [],
+            loading: false,
+            initialLoad: true
+        }
+    },
+    mounted () {
+        this.initialLoad = true
+        this.fetchData()
+    },
+    methods: {
+        loadMore: async function () {
+            this.loading = true
+            const result = await this.loadItems(this.entity.id, this.nextCursor)
+            this.nextCursor = result.meta.next_cursor
+            this.logEntries = this.formatResults(this.logEntries.concat(result.log))
+            this.loading = false
+        },
+        formatResults: function (log) {
+            let lastDate = null
+            return log.map(entry => {
+                if (!entry.time) {
+                    const date = new Date(entry.createdAt)
+                    const thisDate = date.toDateString()
+                    if (thisDate !== lastDate) {
+                        entry.date = date.toDateString()
+                        lastDate = thisDate
+                    }
+                    entry.time = date.toLocaleTimeString()
+                    entry.icon = eventIcons[entry.event] || null
+                    entry.title = eventDescriptions[entry.event] || entry.event
+                    entry.title = entry.title.replace(/\${user}/g, entry.username)
+                } else if (entry.date) {
+                    lastDate = entry.date
+                }
+                return entry
+            })
+        },
+        fetchData: async function (newVal) {
+            if (this.initialLoad) {
+                this.loading = true
+            }
+            if (this.entity && this.entity.id) {
+                const result = await this.loadItems(this.entity.id)
+                this.logEntries = this.formatResults(result.log)
+                this.nextCursor = result.meta.next_cursor
+            }
+            this.initialLoad = false
+            this.loading = false
+        }
+    },
+    components: {
+        AuditEntry
+    }
+}
+</script>
+
+<style lang="scss">
+@import "@/stylesheets/components/audit-log.scss";
+</style>
