ed6dab7bced0da420984c2252fdade4fe9242d3b
Pez Cuckow <email@pezcuckow.com>
2022-12-05 19:41:17 +0200

    Add explaination and coverage of isFQDN

--- forge/lib/validate.js
+++ forge/lib/validate.js
@@ -1,6 +1,10 @@
 module.exports = {
     isFQDN (string) {
-        const regex = /(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,63}\.?$)/
+        // Maximum of 253 characters
+        // Made up of alphanumeric+hyphen segments up 1-63 characters long that does not start/end in hyphens
+        // Ends in a TLD of 2-63 alphanumeric characters
+        // Optional trailing .
+        const regex = /(?=^.{4,253}\.?$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}\.?$)/
 
         return regex.test(string)
     }
