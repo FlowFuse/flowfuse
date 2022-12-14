5c4d2097d309b79193943bf091a0e0426c1dd66a
Joe Pavitt <joepavitt@flowforge.com>
2022-12-14 14:26:08 +0000

    Render updates in natural language list

---
+++ frontend/src/components/audit-log/AuditEntryUpdates.vue
@@ -0,0 +1,17 @@
+<template>
+    <template v-for="(update, $index) in updates" :key="update.key">
+        '{{ update.key }}' has been changed from '{{ update.old }}' to '{{ update.new }}'<template v-if="$index < updates.length - 1">, </template>
+    </template>
+</template>
+
+<script>
+export default {
+    name: 'AuditEntryUpdates',
+    props: {
+        updates: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
