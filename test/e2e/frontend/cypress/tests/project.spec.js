1050b21b811bbe6f38c36153e504d42a3dad31f6
Pez Cuckow <email@pezcuckow.com>
2023-01-13 17:30:41 +0100

    Tests creating projects when billing is enabled

--- test/e2e/frontend/cypress/tests/project.spec.js
+++ test/e2e/frontend/cypress/tests/project.spec.js
@@ -85,6 +85,78 @@ describe('FlowForge - Projects', () => {
     })
 })
 
+describe('FlowForge - Projects - With Billing', () => {
+    beforeEach(() => {
+        cy.enableBilling()
+    })
+
+    it('can create a project that will charge the user', () => {
+        cy.login('alice', 'aaPassword')
+        cy.home()
+
+        cy.request('GET', 'api/v1/teams').then((response) => {
+            const team = response.body.teams[0]
+
+            cy.visit(`/team/${team.slug}/projects/create`)
+
+            cy.get('[data-el="charges-table"]').should('not.exist')
+
+            cy.contains('type1').click()
+
+            cy.get('[data-el="charges-table"]').should('exist')
+
+            cy.get('[data-el="selected-project-type-name"]').contains('type1')
+            cy.get('[data-el="selected-project-type-cost"]').contains('$15.00')
+            cy.get('[data-el="selected-project-type-interval"]').contains('/mo')
+
+            cy.get('[data-el="form-row-description"]').contains('$15.00 now').contains('$15.00/month')
+
+            cy.get('[data-action="create-project"]').should('be.disabled')
+
+            cy.get('[id="billing-confirmation"][data-el="form-row-title"]').click()
+
+            cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+        })
+    })
+
+    it('considers any credit balance the team may have when creating a project', () => {
+        cy.applyBillingCreditToTeam(1001)
+        cy.login('alice', 'aaPassword')
+        cy.home()
+
+        cy.request('GET', 'api/v1/teams').then((response) => {
+            const team = response.body.teams[0]
+
+            cy.visit(`/team/${team.slug}/projects/create`)
+
+            cy.wait('@getTeamBilling')
+
+            cy.get('[data-el="charges-table"]').should('not.exist')
+
+            cy.contains('type1').click()
+
+            cy.get('[data-el="charges-table"]').should('exist')
+
+            cy.get('[data-el="credit-balance-banner"]').should('exist').contains('$10.01')
+
+            cy.get('[data-el="selected-project-type-name"]').contains('type1')
+            cy.get('[data-el="selected-project-type-cost"]').contains('$15.00')
+            cy.get('[data-el="selected-project-type-interval"]').contains('/mo')
+
+            cy.get('[data-el="credit-balance-row"]').should('exist')
+            cy.get('[data-el="credit-balance-amount"]').contains('$10.01')
+
+            cy.get('[data-el="form-row-description"]').contains('$4.99 now').contains('$15.00/month')
+
+            cy.get('[data-action="create-project"]').should('be.disabled')
+
+            cy.get('[id="billing-confirmation"][data-el="form-row-title"]').click()
+
+            cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+        })
+    })
+})
+
 describe('FlowForge stores audit logs for a project', () => {
     beforeEach(() => {
         cy.login('alice', 'aaPassword')
