b6465df27f70c38b03bd3b68723c7babe500a176
Steve-Mcl <sdmclaughlin@gmail.com>
2023-02-09 09:24:44 +0000

    add guards to snapshot lookup (fixes tests)

--- forge/db/views/AccessToken.js
+++ forge/db/views/AccessToken.js
@@ -15,15 +15,17 @@ module.exports = {
             const [key, value] = e.split(':', 2) // split on first colon
             tokenSummary[key] = value
         })
+        // look up the snapshot name if a snapshot id is present
         if (tokenSummary.project) {
             const project = await app.db.models.Project.byId(tokenSummary.project)
-
-            const deviceSettings = await project.getSetting('deviceSettings') || {
+            const deviceSettings = await project?.getSetting('deviceSettings') || {
                 targetSnapshot: null
             }
-
-            const snapshot = await app.db.models.ProjectSnapshot.byId(deviceSettings.targetSnapshot)
-            tokenSummary.targetSnapshot = snapshot.name
+            if (project && deviceSettings?.targetSnapshot) {
+                const snapshot = await app.db.models.ProjectSnapshot.byId(deviceSettings.targetSnapshot)
+                console.log(snapshot)
+                tokenSummary.targetSnapshot = snapshot?.name
+            }
         }
         return tokenSummary
     }
