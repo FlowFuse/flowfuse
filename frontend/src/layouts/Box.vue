16ac71fab1202e42390c7094f427646e637c9aa6
Joe Pavitt <joepavitt@flowforge.com>
2023-02-10 17:03:15 +0000

    mobile friendly styling

--- frontend/src/layouts/Box.vue
+++ frontend/src/layouts/Box.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="ff-layout--box">
-        <div class="ff-layout--box--wrapper" :class="{'grid-cols-2 max-w-6xl': !!this.$slots['splash-content'], 'max-w-2xl': !this.$slots['splash-content']}">
-            <div v-if="!!this.$slots['splash-content']" class="ff-layout--box--left">
+        <div class="ff-layout--box--wrapper" :class="{'md:grid-cols-2 max-w-6xl': !!this.$slots['splash-content'], 'max-w-2xl': !this.$slots['splash-content']}">
+            <div v-if="!!this.$slots['splash-content']" class="ff-layout--box--left hidden md:flex">
                 <div class="ff-layout--box--content">
                     <slot name="splash-content"></slot>
                 </div>
