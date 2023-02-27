e67cf400b5b6faf97e400048bfa0507fa56304d2
Steve-Mcl <sdmclaughlin@gmail.com>
2023-02-27 19:10:00 +0000

    add banner to pages (user/license state dependant)

---
+++ frontend/src/components/banners/LicenseBanner.vue
@@ -0,0 +1,41 @@
+<template>
+    <div
+        v-if="showBanner"
+        class="ff-banner ff-banner-warning"
+        data-el="banner-license-banner"
+    >
+        <span>
+            <ExclamationCircleIcon class="ff-icon mr-2" />
+            <span v-if="license.expiring">Your FlowForge license will expire in {{ license.daysRemaining }} day{{ license.daysRemaining > 1 ? 's': '' }}.</span>
+            <span v-else-if="license.grace">Your FlowForge license has expired and will stop working in  {{ license.graceDaysRemaining }} day{{ license.graceDaysRemaining > 1 ? 's': '' }}.</span>
+            <span v-else-if="license.expired">Your FlowForge license has expired.</span>
+        </span>
+
+        <template>
+            <ChevronRightIcon class="ff-icon align-self-right" />
+        </template>
+    </div>
+</template>
+
+<script>
+import { ChevronRightIcon, ExclamationCircleIcon } from '@heroicons/vue/outline'
+
+import { mapState } from 'vuex'
+
+export default {
+    name: 'LicenseBanner',
+    components: {
+        ExclamationCircleIcon,
+        ChevronRightIcon
+    },
+    computed: {
+        ...mapState('account', ['settings', 'user']),
+        license () {
+            return this.settings.license || {}
+        },
+        showBanner () {
+            return (this.license.expiring && this.user.admin) || this.license.expired || this.license.grace
+        }
+    }
+}
+</script>
