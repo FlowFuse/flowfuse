2b15d564dfb9beb29d53d800c8e4e1ad0e9d8543
Pez Cuckow <email@pezcuckow.com>
2023-03-03 17:47:11 +0100

    Gut the project settings

---
+++ frontend/src/pages/project/Settings.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="flex flex-col sm:flex-row">
+        <SectionSideMenu :options="sideNavigation" />
+        <div class="flex-grow">
+            <FormHeading class="mb-6">Project Details</FormHeading>
+            <div class="space-y-6">
+                <FormRow id="projectId" v-model="input.projectId" type="uneditable" inputClass="font-mono">
+                    Project ID
+                </FormRow>
+
+                <FormRow id="projectName" v-model="input.projectName" type="uneditable">
+                    Name
+                </FormRow>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import FormHeading from '@/components/FormHeading'
+import FormRow from '@/components/FormRow'
+import SectionSideMenu from '@/components/SectionSideMenu'
+import permissionsMixin from '@/mixins/Permissions'
+
+export default {
+    name: 'ProjectSettings',
+    components: {
+        SectionSideMenu,
+        FormHeading,
+        FormRow
+    },
+    mixins: [permissionsMixin],
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    },
+    data () {
+        return {
+            sideNavigation: [{ name: 'General', path: './general' }],
+            input: {
+                projectName: this.project.name,
+                projectId: this.project.id
+            }
+        }
+    },
+    watch: {
+        project () {
+            this.input.projectName = this.project.name
+            this.input.projectId = this.project.id
+        }
+    }
+}
+</script>
--- frontend/src/pages/project/routes.js
+++ frontend/src/pages/project/routes.js
@@ -6,18 +6,9 @@
  */
 import Project from '@/pages/project/index.vue'
 import ProjectOverview from '@/pages/project/Overview.vue'
-import ProjectSettings from '@/pages/project/Settings/index.vue'
-import ProjectSettingsGeneral from '@/pages/project/Settings/General.vue'
-import ProjectSettingsDanger from '@/pages/project/Settings/Danger.vue'
-import ProjectSettingsDevOps from '@/pages/project/Settings/DevOps.vue'
-import ProjectSettingsEditor from '@/pages/project/Settings/Editor.vue'
-import ProjectSettingsSecurity from '@/pages/project/Settings/Security.vue'
-import ProjectSettingsPalette from '@/pages/project/Settings/Palette.vue'
-import ProjectSettingsEnvVar from '@/pages/project/Settings/Environment.vue'
-// import ProjectDebug from '@/pages/project/Debug.vue'
-import ProjectLogs from '@/pages/project/Logs.vue'
 import ProjectActivity from '@/pages/project/Activity.vue'
-import ChangeProjectTypePage from './Settings/ChangeProjectType.vue'
+import ProjectLogs from '@/pages/project/Logs.vue'
+import ProjectSettings from '@/pages/project/Settings.vue'
 
 export default [
     {
@@ -37,27 +28,7 @@ export default [
                 component: ProjectSettings,
                 meta: {
                     title: 'Project - Settings'
-                },
-                redirect: to => {
-                    return `/project/${to.params.id}/settings/general`
-                },
-                children: [
-                    { path: 'general', component: ProjectSettingsGeneral },
-                    { path: 'environment', component: ProjectSettingsEnvVar },
-                    { path: 'devops', component: ProjectSettingsDevOps },
-                    { path: 'editor', component: ProjectSettingsEditor },
-                    { path: 'security', component: ProjectSettingsSecurity },
-                    { path: 'palette', component: ProjectSettingsPalette },
-                    { path: 'danger', component: ProjectSettingsDanger },
-                    {
-                        name: 'ChangeProjectType',
-                        path: 'danger',
-                        component: ChangeProjectTypePage,
-                        meta: {
-                            title: 'Project - Change Type'
-                        }
-                    }
-                ]
+                }
             },
             {
                 path: 'logs',
