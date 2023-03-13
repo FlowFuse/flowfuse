c1ac54300ceb70e3ab0869351d72b945026ac70f
Pez Cuckow <email@pezcuckow.com>
2023-03-13 13:03:47 +0100

    Drop unused dropdown menu functionality

--- frontend/src/components/DropdownMenu.vue
+++ frontend/src/components/DropdownMenu.vue
@@ -24,20 +24,6 @@
                         <template v-else-if="item.disabled">
                             <div :class="[active ? 'bg-gray-200' : '', item.selected? 'bg-gray-100':'', 'block px-4 py-2 text-sm',...(item.class||[]),'opacity-20']">{{ item.name }}</div>
                         </template>
-                        <template v-else-if="item.external">
-                            <a :href="item.link" target="_blank" :class="[active ? 'bg-gray-200' : '', item.selected? 'bg-gray-100':'', 'block px-4 py-2 text-sm text-gray-700',...(item.class||[])]">
-                                <component v-if="item.icon" class="w-4 inline" :is="item.icon"></component>
-                                <img v-if="item.imgUrl" :src="item.imgUrl" class="h-4 v-4 inline rounded mr-1"/>
-                                {{ item.name }}
-                            </a>
-                        </template>
-                        <template v-else-if="item.link || item.path">
-                            <router-link :to="item.link || item" :class="[active ? 'bg-gray-200' : '', item.selected? 'bg-gray-100':'', 'block px-4 py-2 text-sm text-gray-700',...(item.class||[])]">
-                                <component v-if="item.icon" class="w-4 inline" :is="item.icon"></component>
-                                <img v-if="item.imgUrl" :src="item.imgUrl" class="h-4 v-4 inline rounded mr-1"/>
-                                {{ item.name }}
-                            </router-link>
-                        </template>
                         <template v-else>
                             <a @click="item.action" :class="[active ? 'bg-gray-200' : '', item.selected? 'bg-gray-100':'', 'block px-4 py-2 text-sm text-gray-700',...(item.class||[])]" :data-action="`menu-${item.name.toLowerCase()}`">
                                 <component v-if="item.icon" class="w-4 inline" :is="item.icon"></component>
@@ -45,7 +31,6 @@
                                 {{ item.name }}
                             </a>
                         </template>
-
                     </MenuItem>
                 </div>
             </MenuItems>
@@ -56,6 +41,7 @@
 <script>
 /**
  * This component is deprecated and should not be used
+ * ff-dropdown from forge-ui-components is the intended replacement
  */
 import { ref } from 'vue'
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
