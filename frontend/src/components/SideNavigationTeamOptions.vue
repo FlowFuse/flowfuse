bdb836fdb04edb82ea4cf6341c630637d02e9735
Joe Pavitt <joepavitt@flowforge.com>
2023-03-31 16:19:05 +0100

    Remove some duplicated code from merge

--- frontend/src/components/SideNavigationTeamOptions.vue
+++ frontend/src/components/SideNavigationTeamOptions.vue
@@ -67,11 +67,6 @@ export default {
                 tag: 'team-instances',
                 icon: ProjectsIcon
             }, {
-                label: 'Instances',
-                to: '/instances',
-                tag: 'team-instances',
-                icon: ProjectsIcon
-            }, {
                 label: 'Devices',
                 to: '/devices',
                 tag: 'team-devices',
@@ -136,13 +131,6 @@ export default {
             if (route.to === '/applications') {
                 // highlight it if we are currently viewing a single project
                 if (this.$route.path.indexOf('/application') === 0) {
-                    return true
-                }
-            }
-            // the high-level route link to "/instances"
-            if (route.to === '/instances') {
-                // highlight it if we are currently viewing a single instance
-                if (this.$route.path.indexOf('/instance') === 0) {
                     return true
                 }
             }
