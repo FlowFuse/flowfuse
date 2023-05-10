b7f34ce7ac09d7f5b86d69ac41dad46a4f7862fe
Nick O'Leary <nick.oleary@gmail.com>
2023-05-10 11:26:37 +0100

    Add deviceEditor feature flag in place of license check

--- forge/ee/lib/deviceEditor/index.js
+++ forge/ee/lib/deviceEditor/index.js
@@ -1,6 +1,8 @@
 const { DeviceTunnelManager } = require('./DeviceTunnelManager')
 
 module.exports.init = function (app) {
+    app.config.features.register('deviceEditor', true, true)
+
     // Add the tunnelManager to app.comms.devices
     app.comms.devices.tunnelManager = new DeviceTunnelManager(app)
 }
