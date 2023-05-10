0e308ce2f423c60e58f3ab9ad1a07c4d8bcb3c8d
Pez Cuckow <email@pezcuckow.com>
2023-05-10 15:07:47 +0900

    Explicitly create the Pipeline, Pipeline Stage tables

--- forge/db/migrations/20230504-01-add-pipelines.js
+++ forge/db/migrations/20230504-01-add-pipelines.js
@@ -5,11 +5,61 @@ const { DataTypes } = require('sequelize')
 
 module.exports = {
     up: async (context) => {
-        await context.addColumn('Projects', 'PipelineStageId', {
-            type: DataTypes.INTEGER,
-            references: { model: 'PipelineStages', key: 'id' },
-            onDelete: 'set null',
-            onUpdate: 'cascade'
+        await context.createTable('Pipelines', {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            name: { type: DataTypes.STRING, allowNull: false },
+
+            createdAt: { type: DataTypes.DATE },
+            updatedAt: { type: DataTypes.DATE },
+
+            ApplicationId: {
+                type: DataTypes.INTEGER,
+                references: { model: 'Applications', key: 'id' },
+                onDelete: 'cascade',
+                onUpdate: 'cascade'
+            }
+        })
+
+        await context.createTable('PipelineStages', {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            name: { type: DataTypes.STRING, allowNull: false },
+            target: { type: DataTypes.INTEGER, allowNull: true },
+
+            PipelineId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: { model: 'Pipelines', key: 'id' },
+                onDelete: 'cascade',
+                onUpdate: 'cascade'
+            },
+
+            createdAt: { type: DataTypes.DATE },
+            updatedAt: { type: DataTypes.DATE }
+        })
+
+        await context.createTable('PipelineStageInstances', {
+            InstanceId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                references: { model: 'Projects', key: 'id' },
+                onDelete: 'cascade',
+                onUpdate: 'cascade'
+            },
+            PipelineStageId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: { model: 'PipelineStages', key: 'id' },
+                onDelete: 'cascade',
+                onUpdate: 'cascade'
+            }
         })
     },
     down: async (context) => {
