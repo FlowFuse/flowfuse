6440f1c698a0e791b715f997e38363c036ebb0d7
Joe Pavitt <joepavitt@flowforge.com>
2023-06-02 11:12:41 +0100

    Enable updating of tooltip values after "mounted"

--- directives/Tooltip.js
+++ directives/Tooltip.js
@@ -1,19 +1,41 @@
+function renderTooltip (el, binding, vnode) {
+    el.classList.add('ff-tooltip-container')
+
+    let posClass = 'ff-tooltip-right'
+    if (binding.arg) {
+        posClass = 'ff-tooltip-' + binding.arg
+    }
+
+    const span = document.createElement('span')
+    span.className = `ff-tooltip ${posClass}`
+    span.innerHTML = binding.value
+
+    el.appendChild(span)
+}
+
 const directive = {
     name: 'ff-tooltip',
     mounted: (el, binding) => {
-        if (el && binding) {
-            el.classList.add('ff-tooltip-container')
-
-            let posClass = 'ff-tooltip-right'
-            if (binding.arg) {
-                posClass = 'ff-tooltip-' + binding.arg
+        if (el && binding && binding.value) {
+            renderTooltip(el, binding)
+        }
+    },
+    updated (el, binding) {
+        if (binding.value) {
+            const tooltips = el.getElementsByClassName('ff-tooltip')
+            if (tooltips.length) {
+                // update existing tooltip
+                tooltips[0].innerHTML = binding.value
+            } else {
+                // render a new tooltip
+                renderTooltip(el, binding)
             }
-
-            const span = document.createElement('span')
-            span.className = `ff-tooltip ${posClass}`
-            span.innerHTML = binding.value
-
-            el.appendChild(span)
+        } else {
+            // remove all tooltips
+            const tooltips = el.getElementsByClassName('ff-tooltip')
+            for (let i = 0; i < tooltips.length; i++) {
+                tooltips[i].remove()
+            }
         }
     }
 }
