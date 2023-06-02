180925af28013ba8206b12e507aeccac2965023a
Ben Hardill <ben@flowforge.com>
2023-06-02 16:32:26 +0100

    Fix test for kubernetes driver
    The test was looking for `k8s` when it should have been `kubernetes`

--- forge/ee/lib/ha/index.js
+++ forge/ee/lib/ha/index.js
@@ -1,7 +1,7 @@
 const { KEY_HA } = require('../../../db/models/ProjectSettings')
 
 module.exports.init = function (app) {
-    if (app.config.driver.type === 'k8s' || app.config.driver.type === 'stub') {
+    if (app.config.driver.type === 'kubernetes' || app.config.driver.type === 'stub') {
         // Register ha flag as a private flag - no requirement to expose it in public settings
         app.config.features.register('ha', true)
 
