99bda3014ba157e3470563ac99094b7d47996185
Pez Cuckow <email@pezcuckow.com>
2023-06-22 14:45:58 +0200

    Support for running E2E tests against EE version of FlowForge

---
+++ config/cypress-ee.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+
+const shared = require('./cypress-shared.config.js')
+
+module.exports = defineConfig({
+    ...shared,
+    e2e: {
+        ...shared.e2e,
+        ...{
+            baseUrl: 'http://localhost:3001',
+            specPattern: 'test/e2e/frontend/cypress/tests-ee'
+        }
+    }
+})
