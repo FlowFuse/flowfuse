8921fcc0b5da574b50cf5252771d30e94ed374e6
Joe Pavitt <joepavitt@flowforge.com>
2023-06-27 10:42:25 +0100

    Change banner b/g to blue and include "upgrade" link

--- frontend/src/components/banners/FeatureUnavailable.vue
+++ frontend/src/components/banners/FeatureUnavailable.vue
@@ -4,7 +4,9 @@
         data-el="page-banner-feature-unavailable"
     >
         <SparklesIcon class="ff-icon mr-2" style="stroke-width: 1px;" />
-        This is a FlowForge Premium feature. Please upgrade your instance of Flowforge in order to use it.
+        <div>
+            This is a FlowForge Premium feature. Please <a class="ff-link" href="https://flowforge.com/docs/upgrade/open-source-to-premium/" target="_blank" rel="noopener noreferrer">upgrade</a> your instance of Flowforge in order to use it.
+        </div>
         <SparklesIcon class="ff-icon ml-2" style="stroke-width: 1px;" />
     </div>
 </template>
