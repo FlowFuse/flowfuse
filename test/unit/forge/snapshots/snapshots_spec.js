bbe2e7b076f8b0b988ce3188503f81ce30ce2548
Pez Cuckow <email@pezcuckow.com>
2023-07-21 18:00:58 +0200

    Full coverage of the snapshot service

--- test/unit/forge/snapshots/snapshots_spec.js
+++ test/unit/forge/snapshots/snapshots_spec.js
@@ -16,12 +16,140 @@ describe('Snapshots Service', function () {
     })
 
     describe('createSnapshot', function () {
-        it('Creates a snapshot of the passed instance')
-        it('Sets the snapshot as the target if setAsTarget is true')
+        it('Creates a snapshot of the passed instance', async function () {
+            const instance = await APP.db.models.Project.create({ name: 'instance-1', type: '', url: '' })
+
+            await TEAM.addProject(instance)
+
+            // Flows
+            const flows = JSON.stringify([{ id: '456', type: 'newNode' }, { id: '123', type: 'node' }])
+            await APP.db.controllers.StorageFlows.updateOrCreateForProject(instance, flows)
+
+            // Credentials
+            const credentials = { 456: { a: 'b' } }
+            await APP.db.controllers.StorageCredentials.updateOrCreateForProject(instance, credentials)
+
+            // Settings
+            await instance.updateSettings({
+                settings: {
+                    // as array
+                    env: [
+                        { name: 'REMOVED_KEY', value: 'old-value-1' }, // should not get changed
+                        { name: 'EXISTING_KEY', value: 'old-value-2' } // should not get changed
+                    ],
+                    modules: {
+                        foo: '1.2.3'
+                    }
+                }
+            })
+
+            const snapshot = await snapshots.createSnapshot(APP, instance, USER, {
+                name: 'Test Snapshot',
+                description: 'Snapshot description',
+                setAsTarget: false // no need to deploy to devices of the source
+            })
+
+            // Details
+            snapshot.name.should.equal('Test Snapshot')
+            snapshot.description.should.equal('Snapshot description')
+
+            // Flows
+            snapshot.flows.flows[0].should.match({ id: '456', type: 'newNode' })
+            snapshot.flows.flows[1].should.match({ id: '123', type: 'node' })
+            snapshot.flows.flows.length.should.equal(2)
+
+            // Credentials
+            snapshot.flows.credentials.should.match({ 456: { a: 'b' } })
+
+            // Settings
+            snapshot.settings.env.REMOVED_KEY.should.equal('old-value-1')
+            snapshot.settings.env.EXISTING_KEY.should.equal('old-value-2')
+        })
+
+        it('Sets the snapshot as the target if setAsTarget is true', async function () {
+            const instance = await APP.db.models.Project.create({ name: 'instance-1', type: '', url: '' })
+
+            await TEAM.addProject(instance)
+
+            instance.reload()
+
+            const deviceOne = await APP.db.models.Device.create({
+                name: 'device-1',
+                type: 'type-1',
+                credentialSecret: '123',
+                ProjectId: instance.id
+            })
+
+            const deviceTwo = await APP.db.models.Device.create({
+                name: 'device-2',
+                type: 'type-3',
+                credentialSecret: '123',
+                ProjectId: instance.id
+            })
+
+            const snapshot = await snapshots.createSnapshot(APP, instance, USER, {
+                name: 'Test Snapshot',
+                description: 'Snapshot description',
+                setAsTarget: true
+            })
+
+            const deviceSettings = await instance.getSetting('deviceSettings')
+            deviceSettings.targetSnapshot.should.equal(snapshot.id)
+
+            await deviceOne.reload()
+            deviceOne.targetSnapshotId.should.equal(snapshot.id)
+
+            await deviceTwo.reload()
+            deviceTwo.targetSnapshotId.should.equal(snapshot.id)
+        })
     })
 
     describe('copySnapshot', function () {
-        it('Creates a copy of the passed snapshot')
+        it('Creates a copy of the passed snapshot', async function () {
+            const instance = await APP.db.models.Project.create({ name: 'instance-two', type: '', url: '' })
+
+            const snapshotProps = {
+                name: 'Test Snapshot',
+                description: 'Description',
+                settings: {
+                    settings: { settingOne: 'test' },
+                    env: {
+                        ENV_ONE: 'env-1'
+                    },
+                    modules: {
+                        MODULE_ONE: '1.2.3'
+                    }
+                },
+                flows: {
+                    flows: [{ id: '1', type: 'node-1' }],
+                    credentials: { CRED_ONE: { a: 'b' } }
+                },
+                ProjectId: instance.id,
+                UserId: USER.id
+            }
+
+            const originalSnapshot = await APP.db.models.ProjectSnapshot.create(snapshotProps)
+
+            const newSnapshot = await snapshots.copySnapshot(APP, originalSnapshot, instance, { importSnapshot: true, setAsTarget: false })
+
+            originalSnapshot.id.should.not.equal(newSnapshot.id)
+
+            newSnapshot.name.should.equal('Test Snapshot')
+            newSnapshot.description.should.equal('Description')
+
+            // Flows
+            newSnapshot.flows.flows[0].should.match({ id: '1', type: 'node-1' })
+            newSnapshot.flows.flows.length.should.equal(1)
+
+            // Credentials
+            newSnapshot.flows.credentials.should.match({ CRED_ONE: { a: 'b' } })
+
+            // Settings
+            newSnapshot.settings.settings.settingOne.should.equal('test')
+            newSnapshot.settings.env.ENV_ONE.should.equal('env-1')
+            newSnapshot.settings.modules.MODULE_ONE.should.equal('1.2.3')
+        })
+
         it('Imports the snapshot if importSnapshot copying across the environment variables', async function () {
             const instance = await APP.db.models.Project.create({ name: 'instance-1', type: '', url: '' })
 
@@ -66,6 +194,42 @@ describe('Snapshots Service', function () {
             instanceSettings.env[2].value.should.equal('new-value-3')
         })
 
-        it('Sets the snapshot as the target if setAsTarget is true')
+        it('Sets the snapshot as the target if setAsTarget is true', async function () {
+            const toInstance = await APP.db.models.Project.create({ name: 'instance-1', type: '', url: '' })
+
+            await TEAM.addProject(toInstance)
+            toInstance.reload()
+
+            const deviceOne = await APP.db.models.Device.create({
+                name: 'device-1',
+                type: 'type-1',
+                credentialSecret: '123',
+                ProjectId: toInstance.id
+            })
+
+            const deviceTwo = await APP.db.models.Device.create({
+                name: 'device-2',
+                type: 'type-3',
+                credentialSecret: '123',
+                ProjectId: toInstance.id
+            })
+
+            const sourceSnapshot = await snapshots.createSnapshot(APP, toInstance, USER, {
+                name: 'Test Snapshot',
+                description: 'Snapshot description',
+                setAsTarget: true
+            })
+
+            const newSnapshot = await snapshots.copySnapshot(APP, sourceSnapshot, toInstance, { importSnapshot: true, setAsTarget: true })
+
+            const deviceSettings = await toInstance.getSetting('deviceSettings')
+            deviceSettings.targetSnapshot.should.equal(newSnapshot.id)
+
+            await deviceOne.reload()
+            deviceOne.targetSnapshotId.should.equal(newSnapshot.id)
+
+            await deviceTwo.reload()
+            deviceTwo.targetSnapshotId.should.equal(newSnapshot.id)
+        })
     })
 })
