1bf695e5b689203ad1eeb04e5bb537c87bb66807
Joe Pavitt <joepavitt@flowforge.com>
2023-07-27 14:00:27 +0100

    Add E2E test for debit checks

--- test/e2e/frontend/cypress/tests-ee/applications/applications.spec.js
+++ test/e2e/frontend/cypress/tests-ee/applications/applications.spec.js
@@ -58,6 +58,7 @@ describe('FlowForge - Applications - With Billing', () => {
 
             cy.get('[data-el="charges-table"]').should('exist')
 
+            cy.get('[data-el="credit-balance-banner"]').should('exist').contains('credit')
             cy.get('[data-el="credit-balance-banner"]').should('exist').contains('$10.01')
 
             cy.get('[data-el="selected-instance-type-name"]').contains('type1')
@@ -68,6 +69,43 @@ describe('FlowForge - Applications - With Billing', () => {
             cy.get('[data-el="credit-balance-amount"]').contains('$10.01')
 
             cy.get('[data-el="payable-now-summary"]').contains('$4.99')
+
+            cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+        })
+    })
+
+    it('considers any debit balance the team may have when creating a project', () => {
+        cy.applyBillingCreditToTeam(-1001)
+        cy.login('alice', 'aaPassword')
+        cy.home()
+
+        cy.request('GET', 'api/v1/teams').then((response) => {
+            const team = response.body.teams[0]
+
+            cy.visit(`/team/${team.slug}/applications/create`)
+
+            cy.wait('@getTeamBySlug')
+            cy.wait('@getTeamBilling')
+
+            cy.get('[data-el="charges-table"]').should('not.exist')
+
+            cy.get('[data-form="application-name"] input').clear()
+            cy.get('[data-form="application-name"] input').type(`new-application-${Math.random().toString(36).substring(2, 7)}`)
+            cy.contains('type1').click()
+
+            cy.get('[data-el="charges-table"]').should('exist')
+
+            cy.get('[data-el="credit-balance-banner"]').should('exist').contains('owe')
+            cy.get('[data-el="credit-balance-banner"]').should('exist').contains('$10.01')
+
+            cy.get('[data-el="selected-instance-type-name"]').contains('type1')
+            cy.get('[data-el="selected-instance-type-cost"]').contains('$15.00')
+            cy.get('[data-el="selected-instance-type-interval"]').contains('/mo')
+
+            cy.get('[data-el="credit-balance-row"]').should('exist')
+            cy.get('[data-el="credit-balance-amount"]').contains('$10.01')
+
+            cy.get('[data-el="payable-now-summary"]').contains('$25.01')
 
             cy.get('[data-action="create-project"]').should('not.be.disabled').click()
         })
