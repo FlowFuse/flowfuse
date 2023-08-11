8a7f5282de16f5e01bb06f80a8aef759521848ab
Ben Hardill <ben@flowforge.com>
2023-08-11 09:55:32 +0100

    Update frontend/src/pages/team/Library.vue
    Co-authored-by: Joe Pavitt <99246719+joepavitt@users.noreply.github.com>

--- frontend/src/pages/team/Library.vue
+++ frontend/src/pages/team/Library.vue
@@ -11,7 +11,7 @@
                 <template #helptext>
                     <p>In Node-RED you can export and import flows and functions, and save them to your Team Library.</p>
                     <p>The contents of your Team Library are available across any of your application instances in FlowForge.</p>
-                    <p>You can read more about <a href="https://nodered.org/docs/user-guide/editor/workspace/import-export" target="_blank">Import & Exporting Flows</a> in the Node-RED documentation</p>
+                    <p>You can read more about <a href="https://nodered.org/docs/user-guide/editor/workspace/import-export" target="_blank">Import &amp; Exporting Flows</a> in the Node-RED documentation</p>
                 </template>
                 <template #tools>
                     <ff-button v-if="contents" @click="copyToClipboard()">Copy to Clipboard</ff-button>
