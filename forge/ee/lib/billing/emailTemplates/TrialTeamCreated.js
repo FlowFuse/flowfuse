0e07711d45d4b5736d2715abee840fbe5ebe9948
Nick O'Leary <nick.oleary@gmail.com>
2023-08-31 21:13:02 +0100

    Fix sending welcome email at start of new trial type

--- forge/ee/lib/billing/emailTemplates/TrialTeamCreated.js
+++ forge/ee/lib/billing/emailTemplates/TrialTeamCreated.js
@@ -16,6 +16,7 @@ To get started, log in to FlowFuse and begin creating Node-RED flows in your fir
 
 You can also invite other users to join your team to collaborate on your applications.
 
+{{#if trialProjectTypeName}}
 Your {{{trialDuration}}} day trial allows you to create a {{{trialProjectTypeName}}} instance
 for free. Once the trial ends, you will need to add your credit card details to
 keep it running. But don't worry - we'll remind you when the trial is
@@ -24,7 +25,13 @@ nearing its end.
 If you want to do more with your team during the trial, you can add your credit
 card details at any time and create more instances.  Again, we'll email to remind
 you what is happening with the trial.
+{{else}}
+Your {{{trialDuration}}} day trial gives you full access to your new Starter team,
+where you can have up to two Node-RED instances and two devices.
 
+Once the trial ends, you will need to add your credit card details to keep it running.
+But don't worry - we'll remind you when the trial is nearing its end.
+{{/if}}
 We hope you enjoy the FlowFuse experience.
 
 Cheers!
@@ -41,14 +48,22 @@ Your friendly FlowFuse Team
 
 <p>You can also invite other users to join your team to collaborate on your applications.</p>
 
+{{#if trialProjectTypeName}}
 <p>Your {{{trialDuration}}} day trial allows you to create a {{{trialProjectTypeName}}} instance
 for free. Once the trial ends, you will need to add your credit card details to
-keep the it running. But don't worry - we'll remind you when the trial is
+keep it running. But don't worry - we'll remind you when the trial is
 nearing its end.</p>
 
 <p>If you want to do more with your team during the trial, you can add your credit
 card details at any time and create more instances.  Again, we'll email to remind
 you what is happening with the trial.</p>
+{{else}}
+<p>Your {{{trialDuration}}} day trial gives you full access to your new Starter team,
+where you can have up to two Node-RED instances and two devices.</p>
+
+<p>Once the trial ends, you will need to add your credit card details to keep it running.
+But don't worry - we'll remind you when the trial is nearing its end.</p>
+{{/if}}
 
 <p>We hope you enjoy the FlowFuse experience.</p>
 
