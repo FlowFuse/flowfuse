4176e9fd1c04712d8616f659d79dd37760bc0dfd
Nick O'Leary <nick.oleary@gmail.com>
2023-10-05 14:40:48 +0100

    Remove not null constraint on ProjectSnapshot.ProjectId column

---
+++ forge/db/migrations/20231005-01-update-projectSnapshot-constraint.js
@@ -0,0 +1,27 @@
+/**
+ * Remove not null on ProjectSnapshots.ProjectId
+ */
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+    up: async (context) => {
+        const dialect = context.sequelize.options.dialect
+        if (dialect === 'sqlite') {
+            // Disable foreign_keys so that when sequelize does its sqlite-specific
+            // copy/drop workaround for renaming columns, we don't cause cascade
+            // delete on the Teams table
+            await context.sequelize.query('PRAGMA foreign_keys = 0;')
+        }
+
+        await context.changeColumn('ProjectSnapshots', 'ProjectId', {
+            type: DataTypes.UUID,
+            allowNull: true
+        })
+
+        if (dialect === 'sqlite') {
+            await context.sequelize.query('PRAGMA foreign_keys = 1;')
+        }
+    },
+    down: async (context) => {
+    }
+}
