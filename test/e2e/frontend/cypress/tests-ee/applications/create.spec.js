4365d7f671144daea0da8265f7e4c4ad08fb772c
Pez Cuckow <email@pezcuckow.com>
2023-10-13 12:31:19 +0200

    Update Application > Create blueprint blueprint coverage

--- test/e2e/frontend/cypress/tests-ee/applications/create.spec.js
+++ test/e2e/frontend/cypress/tests-ee/applications/create.spec.js
@@ -1,116 +1,118 @@
-describe('FlowForge - Application - With Billing', () => {
-    beforeEach(() => {
-        cy.enableBilling()
-    })
-
+describe('FlowForge - Application', () => {
     describe('Creating an application with an instance', () => {
-        it('will charge the user', () => {
-            cy.intercept('POST', '/api/*/applications').as('createApplication')
-            cy.intercept('POST', '/api/*/projects').as('createInstance')
-            cy.intercept('GET', '/api/*/stacks*').as('loadStacks')
-
-            cy.login('alice', 'aaPassword')
-            cy.home()
-
-            cy.request('GET', 'api/v1/teams').then((response) => {
-                const team = response.body.teams[0]
-
-                cy.visit(`/team/${team.slug}/applications/create`)
-
-                cy.get('[data-action="create-project"]').should('be.disabled')
-
-                cy.get('[data-el="charges-table"]').should('not.exist')
-
-                cy.get('[data-form="application-name"] input').clear()
-                cy.get('[data-form="application-name"] input').type(`new-application-${Math.random().toString(36).substring(2, 7)}`)
-                cy.contains('type1').click()
-
-                cy.wait('@loadStacks')
-                cy.get('[data-el="charges-table"]').should('exist')
-
-                cy.get('[data-el="credit-balance-banner"]').should('not.exist')
-
-                cy.get('[data-el="selected-instance-type-name"]').contains('type1')
-                cy.get('[data-el="selected-instance-type-cost"]').contains('$15.00')
-                cy.get('[data-el="selected-instance-type-interval"]').contains('/mo')
-
-                cy.get('[data-el="payable-now-summary"]').contains('$15.00 now').contains('$15.00 /month')
-
-                cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+        describe('with billing', () => {
+            beforeEach(() => {
+                cy.enableBilling()
             })
-        })
 
-        it('considers any credit balance the team may have', () => {
-            cy.applyBillingCreditToTeam(1001)
-            cy.login('alice', 'aaPassword')
-            cy.home()
+            it('will charge the user', () => {
+                cy.intercept('POST', '/api/*/applications').as('createApplication')
+                cy.intercept('POST', '/api/*/projects').as('createInstance')
+                cy.intercept('GET', '/api/*/stacks*').as('loadStacks')
 
-            cy.request('GET', 'api/v1/teams').then((response) => {
-                const team = response.body.teams[0]
+                cy.login('alice', 'aaPassword')
+                cy.home()
 
-                cy.visit(`/team/${team.slug}/applications/create`)
+                cy.request('GET', 'api/v1/teams').then((response) => {
+                    const team = response.body.teams[0]
 
-                cy.wait('@getTeamBySlug')
-                cy.wait('@getTeamBilling')
+                    cy.visit(`/team/${team.slug}/applications/create`)
 
-                cy.get('[data-el="charges-table"]').should('not.exist')
+                    cy.get('[data-action="create-project"]').should('be.disabled')
 
-                cy.get('[data-form="application-name"] input').clear()
-                cy.get('[data-form="application-name"] input').type(`new-application-${Math.random().toString(36).substring(2, 7)}`)
-                cy.contains('type1').click()
+                    cy.get('[data-el="charges-table"]').should('not.exist')
 
-                cy.get('[data-el="charges-table"]').should('exist')
+                    cy.get('[data-form="application-name"] input').clear()
+                    cy.get('[data-form="application-name"] input').type(`new-application-${Math.random().toString(36).substring(2, 7)}`)
+                    cy.get('[data-form="project-type"]').contains('type1').click()
 
-                cy.get('[data-el="credit-balance-banner"]').should('exist').contains('credit')
-                cy.get('[data-el="credit-balance-banner"]').should('exist').contains('$10.01')
+                    cy.wait('@loadStacks')
+                    cy.get('[data-el="charges-table"]').should('exist')
 
-                cy.get('[data-el="selected-instance-type-name"]').contains('type1')
-                cy.get('[data-el="selected-instance-type-cost"]').contains('$15.00')
-                cy.get('[data-el="selected-instance-type-interval"]').contains('/mo')
+                    cy.get('[data-el="credit-balance-banner"]').should('not.exist')
 
-                cy.get('[data-el="credit-balance-row"]').should('exist')
-                cy.get('[data-el="credit-balance-amount"]').contains('$10.01')
+                    cy.get('[data-el="selected-instance-type-name"]').contains('type1')
+                    cy.get('[data-el="selected-instance-type-cost"]').contains('$15.00')
+                    cy.get('[data-el="selected-instance-type-interval"]').contains('/mo')
 
-                cy.get('[data-el="payable-now-summary"]').contains('$4.99')
+                    cy.get('[data-el="payable-now-summary"]').contains('$15.00 now').contains('$15.00 /month')
 
-                cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+                    cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+                })
             })
-        })
 
-        it('considers any debit balance the team may have', () => {
-            cy.applyBillingCreditToTeam(-1001)
-            cy.login('alice', 'aaPassword')
-            cy.home()
+            it('considers any credit balance the team may have', () => {
+                cy.applyBillingCreditToTeam(1001)
+                cy.login('alice', 'aaPassword')
+                cy.home()
 
-            cy.request('GET', 'api/v1/teams').then((response) => {
-                const team = response.body.teams[0]
+                cy.request('GET', 'api/v1/teams').then((response) => {
+                    const team = response.body.teams[0]
 
-                cy.visit(`/team/${team.slug}/applications/create`)
+                    cy.visit(`/team/${team.slug}/applications/create`)
 
-                cy.wait('@getTeamBySlug')
-                cy.wait('@getTeamBilling')
+                    cy.wait('@getTeamBySlug')
+                    cy.wait('@getTeamBilling')
 
-                cy.get('[data-el="charges-table"]').should('not.exist')
+                    cy.get('[data-el="charges-table"]').should('not.exist')
 
-                cy.get('[data-form="application-name"] input').clear()
-                cy.get('[data-form="application-name"] input').type(`new-application-${Math.random().toString(36).substring(2, 7)}`)
-                cy.contains('type1').click()
+                    cy.get('[data-form="application-name"] input').clear()
+                    cy.get('[data-form="application-name"] input').type(`new-application-${Math.random().toString(36).substring(2, 7)}`)
+                    cy.contains('type1').click()
 
-                cy.get('[data-el="charges-table"]').should('exist')
+                    cy.get('[data-el="charges-table"]').should('exist')
 
-                cy.get('[data-el="credit-balance-banner"]').should('exist').contains('owe')
-                cy.get('[data-el="credit-balance-banner"]').should('exist').contains('$10.01')
+                    cy.get('[data-el="credit-balance-banner"]').should('exist').contains('credit')
+                    cy.get('[data-el="credit-balance-banner"]').should('exist').contains('$10.01')
 
-                cy.get('[data-el="selected-instance-type-name"]').contains('type1')
-                cy.get('[data-el="selected-instance-type-cost"]').contains('$15.00')
-                cy.get('[data-el="selected-instance-type-interval"]').contains('/mo')
+                    cy.get('[data-el="selected-instance-type-name"]').contains('type1')
+                    cy.get('[data-el="selected-instance-type-cost"]').contains('$15.00')
+                    cy.get('[data-el="selected-instance-type-interval"]').contains('/mo')
 
-                cy.get('[data-el="credit-balance-row"]').should('exist')
-                cy.get('[data-el="credit-balance-amount"]').contains('$10.01')
+                    cy.get('[data-el="credit-balance-row"]').should('exist')
+                    cy.get('[data-el="credit-balance-amount"]').contains('$10.01')
 
-                cy.get('[data-el="payable-now-summary"]').contains('$25.01')
+                    cy.get('[data-el="payable-now-summary"]').contains('$4.99')
 
-                cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+                    cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+                })
+            })
+
+            it('considers any debit balance the team may have', () => {
+                cy.applyBillingCreditToTeam(-1001)
+                cy.login('alice', 'aaPassword')
+                cy.home()
+
+                cy.request('GET', 'api/v1/teams').then((response) => {
+                    const team = response.body.teams[0]
+
+                    cy.visit(`/team/${team.slug}/applications/create`)
+
+                    cy.wait('@getTeamBySlug')
+                    cy.wait('@getTeamBilling')
+
+                    cy.get('[data-el="charges-table"]').should('not.exist')
+
+                    cy.get('[data-form="application-name"] input').clear()
+                    cy.get('[data-form="application-name"] input').type(`new-application-${Math.random().toString(36).substring(2, 7)}`)
+                    cy.contains('type1').click()
+
+                    cy.get('[data-el="charges-table"]').should('exist')
+
+                    cy.get('[data-el="credit-balance-banner"]').should('exist').contains('owe')
+                    cy.get('[data-el="credit-balance-banner"]').should('exist').contains('$10.01')
+
+                    cy.get('[data-el="selected-instance-type-name"]').contains('type1')
+                    cy.get('[data-el="selected-instance-type-cost"]').contains('$15.00')
+                    cy.get('[data-el="selected-instance-type-interval"]').contains('/mo')
+
+                    cy.get('[data-el="credit-balance-row"]').should('exist')
+                    cy.get('[data-el="credit-balance-amount"]').contains('$10.01')
+
+                    cy.get('[data-el="payable-now-summary"]').contains('$25.01')
+
+                    cy.get('[data-action="create-project"]').should('not.be.disabled').click()
+                })
             })
         })
 
@@ -209,13 +211,9 @@ describe('FlowForge - Application - With Billing', () => {
                 cy.wait('@getTeamBySlug')
                 cy.wait('@loadBlueprints')
 
-                const applicationName = `new-application-${Math.random().toString(36).substring(2, 7)}`
-                const applicationDescription = `application-description-${Math.random().toString(36).substring(2, 7)}`
-
-                cy.get('[data-form="application-name"] input').type(applicationName)
-                cy.get('[data-form="application-description"]').type(applicationDescription)
-
-                cy.contains('type1').click()
+                cy.get('[data-form="application-name"] input').type(`new-application-${Math.random().toString(36).substring(2, 7)}`)
+                cy.get('[data-form="application-description"]').type(`application-description-${Math.random().toString(36).substring(2, 7)}`)
+                cy.get('[data-form="project-type"]').contains('type1').click()
 
                 cy.get('[data-form="tile-selection-option-label"]').contains('New Blueprint').click()
 
