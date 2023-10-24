52017d28c6cb8bff417cf79761698062f59c76b6
Ben Hardill <ben@flowforge.com>
2023-10-24 16:34:30 +0100

    Add wildcard Content Type parser
    Fixes #2999
    
    Add a catch all content parser for things sent to the editor

--- forge/ee/routes/deviceEditor/index.js
+++ forge/ee/routes/deviceEditor/index.js
@@ -16,6 +16,18 @@ module.exports = async function (app) {
         'device:editor': { description: 'Access the Device Editor', role: Roles.Member }
     })
 
+
+    /**
+     * Add wildcard content parser for these routes
+     */
+    app.addContentTypeParser('*', (req, payload, done) => {
+        var data = ''
+        payload.on('data', chunk => { data += chunk })
+        payload.on('end', () => {
+            done(null, data)
+        })
+    })
+
     app.addHook('preHandler', app.verifySession)
     app.addHook('preHandler', async (request, reply) => {
         if (request.params.deviceId !== undefined) {
