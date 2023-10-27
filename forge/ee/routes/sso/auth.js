ab12b1c1d6148294ff6d4e1d04def9fc48db7d3d
John Bauer <john.e.b.good@gmail.com>
2023-10-27 03:17:26 -0500

    Added auto user creation scuffly

--- forge/ee/routes/sso/auth.js
+++ forge/ee/routes/sso/auth.js
@@ -9,8 +9,12 @@ module.exports = fp(async function (app, opts, done) {
             // state. We don't need to use that, but we need to ensure we have
             // the api in place otherwise passport will complain.
             request.session = {
-                get: key => { return null },
-                set: (key, value) => { return null }
+                get: (key) => {
+                    return null
+                },
+                set: (key, value) => {
+                    return null
+                }
             }
         }
     })
@@ -33,115 +37,174 @@ module.exports = fp(async function (app, opts, done) {
         reply.redirect('/')
     })
 
-    fastifyPassport.use(new MultiSamlStrategy({
-        passReqToCallback: true, // makes req available in callback,
-        wantAssertionsSigned: false, // TODO: allow this to be set per provider
-        async getSamlOptions (request, done) {
-            if (request.body?.RelayState) {
-                // This is an in-flight request. We previously stored the SAML provider
-                // id in RelayState - which should get returned to us here.
-                // Use that to get back the right SAML options
-                const state = JSON.parse(request.body.RelayState)
-                const opts = await app.sso.getProviderOptions(state.provider)
-                if (opts) {
-                    providerOptions = opts
-                    done(null, opts)
-                } else {
-                    done(new Error(`Unknown SAML provider: ${state.provider}`))
+    fastifyPassport.use(
+        new MultiSamlStrategy(
+            {
+                passReqToCallback: true, // makes req available in callback,
+                wantAssertionsSigned: false, // TODO: allow this to be set per provider
+                async getSamlOptions (request, done) {
+                    if (request.body?.RelayState) {
+                        // This is an in-flight request. We previously stored the SAML provider
+                        // id in RelayState - which should get returned to us here.
+                        // Use that to get back the right SAML options
+                        const state = JSON.parse(request.body.RelayState)
+                        const opts = await app.sso.getProviderOptions(state.provider)
+                        if (opts) {
+                            providerOptions = opts
+                            done(null, opts)
+                        } else {
+                            done(new Error(`Unknown SAML provider: ${state.provider}`))
+                        }
+                        return
+                    }
+                    if (request.query.u) {
+                        // This is an initial request to start a SAML flow. The user
+                        // email is provided as a query parameter 'u'
+                        const providerId = await app.sso.getProviderForEmail(request.query.u)
+                        const opts = await app.sso.getProviderOptions(providerId)
+                        if (opts) {
+                            request.query.RelayState = JSON.stringify({
+                                provider: providerId,
+                                redirectTo: decodeURIComponent(request.query.r || '/')
+                            })
+                            done(null, opts)
+                            return
+                        } else {
+                            done(new Error(`No matching SAML provider for email ${request.query.u}`))
+                            return
+                        }
+                    }
+                    done(new Error('Missing u query parameter'))
                 }
-                return
+            },
+            async (req, profile, done) => {
+                if (profile.nameID) {
+                    const user = await app.db.models.User.byUsernameOrEmail(profile.nameID)
+                    const userInfo = app.auditLog.formatters.userObject({ email: profile.nameID })
+                    const samlGroups = JSON.stringify(profile['http://schemas.microsoft.com/ws/2008/06/identity/claims/groups'])
+                    const displayName = profile['http://schemas.microsoft.com/identity/claims/displayname']
+                    if (user) {
+                        user.saml_groups = samlGroups
+                        user.name = displayName
+
+                        if (providerOptions.allowAllSSO) {
+                            done(null, user)
+                        } else if (samlGroups.includes(providerOptions.samlAdminGroup)) {
+                            user.admin = true
+                            await app.auditLog.User.account.samlAdminLogin(userInfo, null)
+                        } else if (samlGroups.includes(providerOptions.samlUserGroup)) {
+                            user.admin = false
+                        } else {
+                            done(new Error('Not assigned to a User or Admin group'))
+                        }
+                        done(null, user)
+                    } else if (providerOptions.autoCreateUser) {
+                        app.log.info('Create new user')
+
+                        let createUser = false
+                        let admin = false
+
+                        if (providerOptions.allowAllSSO) {
+                            createUser = true
+                        } else if (samlGroups.includes(providerOptions.samlAdminGroup)) {
+                            admin = true
+                            createUser = true
+                            await app.auditLog.User.account.samlAdminLogin(userInfo, null)
+                        } else if (samlGroups.includes(providerOptions.samlUserGroup)) {
+                            createUser = true
+                        } else {
+                            done(new Error('Not assigned to a User or Admin group'))
+                        }
+                        if (createUser) {
+                            const newUser = await app.db.models.User.create({
+                                username: profile.nameID,
+                                name: displayName,
+                                email: profile.nameID,
+                                email_verified: true,
+                                saml_groups: samlGroups,
+                                password: generateRandomString(),
+                                admin: !!admin
+                            })
+
+                            done(null, newUser)
+                        }
+                    } else {
+                        const unknownError = new Error(`Unknown user: ${profile.nameID}`)
+                        unknownError.code = 'unknown_sso_user'
+                        const userInfo = app.auditLog.formatters.userObject({ email: profile.nameID })
+                        const resp = { code: 'unknown_sso_user', error: 'unauthorized' }
+                        await app.auditLog.User.account.login(userInfo, resp, userInfo)
+                        done(unknownError)
+                    }
+                } else {
+                    const missingNameIDError = new Error('SAML response missing nameID')
+                    missingNameIDError.code = 'unknown_sso_user'
+                    done(missingNameIDError)
+                }
             }
-            if (request.query.u) {
-                // This is an initial request to start a SAML flow. The user
-                // email is provided as a query parameter 'u'
-                const providerId = await app.sso.getProviderForEmail(request.query.u)
-                const opts = await app.sso.getProviderOptions(providerId)
-                if (opts) {
-                    request.query.RelayState = JSON.stringify({
-                        provider: providerId,
-                        redirectTo: decodeURIComponent(request.query.r || '/')
-                    })
-                    done(null, opts)
+        )
+    )
+
+    function generateRandomString (length = 16) {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        let result = ''
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length)
+            result += characters[randomIndex]
+        }
+
+        return result
+    }
+
+    app.get(
+        '/ee/sso/login',
+        {
+            config: { allowAnonymous: true },
+            preValidation: fastifyPassport.authenticate('saml', { session: false })
+        },
+        async (request, reply, err, user, info, status) => {
+            // Should never get here as passport will trigger the saml flow
+            // and either result in the error handler, or a POST to /sso/login/callback below
+            reply.redirect('/')
+        }
+    )
+
+    app.post(
+        '/ee/sso/login/callback',
+        {
+            config: { allowAnonymous: true },
+            preValidation: fastifyPassport.authenticate('saml', { session: false })
+        },
+        async (request, reply, err, user, info, status) => {
+            if (request.user) {
+                const userInfo = app.auditLog.formatters.userObject(request.user)
+                // They have completed authentication and we know who they are.
+                const sessionInfo = await app.createSessionCookie(request.user.email)
+                if (sessionInfo) {
+                    request.user.sso_enabled = true
+                    request.user.email_verified = true
+                    await request.user.save()
+                    userInfo.id = sessionInfo.session.UserId
+                    reply.setCookie('sid', sessionInfo.session.sid, sessionInfo.cookieOptions)
+                    await app.auditLog.User.account.login(userInfo, null)
+                    let redirectTo = '/'
+                    if (request.body?.RelayState) {
+                        const state = JSON.parse(request.body.RelayState)
+                        redirectTo = /^\/.*/.test(state.redirectTo) ? state.redirectTo : '/'
+                    }
+                    reply.redirect(redirectTo)
                     return
                 } else {
-                    done(new Error(`No matching SAML provider for email ${request.query.u}`))
+                    const resp = { code: 'user_suspended', error: 'User Suspended' }
+                    await app.auditLog.User.account.login(userInfo, resp, userInfo)
+                    // TODO: how to surface errors
+                    reply.redirect('/')
                     return
                 }
             }
-            done(new Error('Missing u query parameter'))
+            throw new Error('Invalid SAML response')
         }
-    }, async (req, profile, done) => {
-        if (profile.nameID) {
-            const user = await app.db.models.User.byUsernameOrEmail(profile.nameID)
-            const userInfo = app.auditLog.formatters.userObject({ email: profile.nameID })
-            if (user) {
-                user.saml_groups = JSON.stringify(profile['http://schemas.microsoft.com/ws/2008/06/identity/claims/groups'])
-                user.name = profile['http://schemas.microsoft.com/identity/claims/displayname']
-                if (!providerOptions.allowAllSSO && (!user.saml_groups.includes(providerOptions.samlUserGroup) || !user.saml_groups.includes(providerOptions.samlAdminGroup))) {
-                    done(new Error('Not assigned to a User or Admin group'))
-                }
-                if (user.saml_groups.includes(providerOptions.samlAdminGroup)) {
-                    user.admin = true
-                    await app.auditLog.User.account.samlAdminLogin(userInfo, null)
-                } else {
-                    user.admin = false
-                }
-                done(null, user)
-            } else {
-                const unknownError = new Error(`Unknown user: ${profile.nameID}`)
-                unknownError.code = 'unknown_sso_user'
-                const userInfo = app.auditLog.formatters.userObject({ email: profile.nameID })
-                const resp = { code: 'unknown_sso_user', error: 'unauthorized' }
-                await app.auditLog.User.account.login(userInfo, resp, userInfo)
-                done(unknownError)
-            }
-        } else {
-            const missingNameIDError = new Error('SAML response missing nameID')
-            missingNameIDError.code = 'unknown_sso_user'
-            done(missingNameIDError)
-        }
-    }))
-
-    app.get('/ee/sso/login', {
-        config: { allowAnonymous: true },
-        preValidation: fastifyPassport.authenticate('saml', { session: false })
-    }, async (request, reply, err, user, info, status) => {
-        // Should never get here as passport will trigger the saml flow
-        // and either result in the error handler, or a POST to /sso/login/callback below
-        reply.redirect('/')
-    })
-
-    app.post('/ee/sso/login/callback', {
-        config: { allowAnonymous: true },
-        preValidation: fastifyPassport.authenticate('saml', { session: false })
-    }, async (request, reply, err, user, info, status) => {
-        if (request.user) {
-            const userInfo = app.auditLog.formatters.userObject(request.user)
-            // They have completed authentication and we know who they are.
-            const sessionInfo = await app.createSessionCookie(request.user.email)
-            if (sessionInfo) {
-                request.user.sso_enabled = true
-                request.user.email_verified = true
-                await request.user.save()
-                userInfo.id = sessionInfo.session.UserId
-                reply.setCookie('sid', sessionInfo.session.sid, sessionInfo.cookieOptions)
-                await app.auditLog.User.account.login(userInfo, null)
-                let redirectTo = '/'
-                if (request.body?.RelayState) {
-                    const state = JSON.parse(request.body.RelayState)
-                    redirectTo = /^\/.*/.test(state.redirectTo) ? state.redirectTo : '/'
-                }
-                reply.redirect(redirectTo)
-                return
-            } else {
-                const resp = { code: 'user_suspended', error: 'User Suspended' }
-                await app.auditLog.User.account.login(userInfo, resp, userInfo)
-                // TODO: how to surface errors
-                reply.redirect('/')
-                return
-            }
-        }
-        throw new Error('Invalid SAML response')
-    })
+    )
     done()
 })
