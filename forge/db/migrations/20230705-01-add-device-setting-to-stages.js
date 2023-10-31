cd0fb1a89f3d41a2c956f30560763d0b897c07a7
Nick O'Leary <nick.oleary@gmail.com>
2023-11-01 00:29:51 +0100

    Fix applying pipeline migrations on non-EE platforms

--- forge/db/migrations/20230705-01-add-device-setting-to-stages.js
+++ forge/db/migrations/20230705-01-add-device-setting-to-stages.js
@@ -6,6 +6,15 @@ const { DataTypes } = require('sequelize')
 
 module.exports = {
     up: async (context) => {
+        try {
+            await context.describeTable('PipelineStages')
+        } catch (err) {
+            // Missing table means this db was initialised after Pipelines
+            // were introduced and does not have an EE license. So we need to
+            // rerun the Pipeline migrations to ensure the tables exist in the
+            // right state
+            await require('./20230504-01-add-pipelines').up(context)
+        }
         await context.addColumn('PipelineStages', 'deployToDevices', {
             type: DataTypes.BOOLEAN,
             allowNull: false,
--- forge/db/migrations/20230919-01-add-action-to-pipeline-stage.js
+++ forge/db/migrations/20230919-01-add-action-to-pipeline-stage.js
@@ -11,6 +11,17 @@ module.exports = {
      * @param {QueryInterface} context Sequelize.QueryInterface
      */
     up: async (context) => {
+        try {
+            await context.describeTable('PipelineStages')
+        } catch (err) {
+            // Missing table means this db was initialised after Pipelines
+            // were introduced and does not have an EE license. So we need to
+            // rerun the Pipeline migrations to ensure the tables exist in the
+            // right state
+            await require('./20230504-01-add-pipelines').up(context)
+            await require('./20230705-01-add-device-setting-to-stages').up(context)
+        }
+
         const SNAPSHOT_ACTIONS = {
             CREATE_SNAPSHOT: 'create_snapshot',
             USE_LATEST_SNAPSHOT: 'use_latest_snapshot',
