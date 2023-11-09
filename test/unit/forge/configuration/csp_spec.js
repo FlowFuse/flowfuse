f51daf437e3c20d22ba252180608bceca85e42d6
Ben Hardill <ben@flowforge.com>
2023-11-09 17:19:59 +0000

    Slightly better tests

--- test/unit/forge/configuration/csp_spec.js
+++ test/unit/forge/configuration/csp_spec.js
@@ -2,10 +2,14 @@ const should = require('should') // eslint-disable-line
 
 const FF_UTIL = require('flowforge-test-utils')
 
-describe('Check CSP values parsed', async () => {
+describe.only('Check CSP values parsed', async () => {
     let app
 
-    before(async function () {
+    afterEach(async function () {
+        await app.close()
+    })
+
+    it('CSP Report only should be enabled', async function () {
         const config = {
             content_security_policy: {
                 enabled: true,
@@ -14,13 +18,7 @@ describe('Check CSP values parsed', async () => {
             }
         }
         app = await FF_UTIL.setupApp(config)
-    })
 
-    after(async function () {
-        await app.close()
-    })
-
-    it('CSP should be enabled', async function () {
         const response = await app.inject({
             method: 'GET',
             url: '/'
@@ -30,5 +28,22 @@ describe('Check CSP values parsed', async () => {
         headers.should.have.property('content-security-policy-report-only')
         const csp = response.headers['content-security-policy-report-only']
         csp.split(';').should.containEql('report-uri https://example.com')
+    })
+
+    it('CSP should be enabled', async function () {
+        const config = {
+            content_security_policy: {
+                enabled: true
+            }
+        }
+        app = await FF_UTIL.setupApp(config)
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        })
+
+        const headers = response.headers
+        headers.should.have.property('content-security-policy')
+        const csp = response.headers['content-security-policy']
     })
 })
