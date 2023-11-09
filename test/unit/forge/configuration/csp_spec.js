d0453d4ba7eb74767411f639230b649e1dad2ece
Ben Hardill <ben@flowforge.com>
2023-11-09 18:07:40 +0000

    Full coverage

--- test/unit/forge/configuration/csp_spec.js
+++ test/unit/forge/configuration/csp_spec.js
@@ -66,4 +66,134 @@ describe('Check CSP values parsed', async () => {
         csp.split(';').should.containEql('base-uri \'self\'')
         csp.split(';').should.containEql('script-src \'self\' \'unsafe-inline\' \'unsafe-eval\'')
     })
+
+    it('CSP should be enabled, custom directives', async function () {
+        const config = {
+            content_security_policy: {
+                enabled: true,
+                directives: {
+                    'base-uri': 'example.com'
+                }
+            }
+        }
+        app = await FF_UTIL.setupApp(config)
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        })
+
+        const headers = response.headers
+        headers.should.have.property('content-security-policy')
+        const csp = response.headers['content-security-policy']
+        csp.split(';').should.containEql('base-uri example.com')
+    })
+
+    it('CSP should be enabled with plausible', async function () {
+        const config = {
+            telemetry: {
+                frontend: {
+                    plausible: {
+                        domain: 'example.com'
+                    }
+                }
+            },
+            content_security_policy: {
+                enabled: true
+            }
+        }
+        app = await FF_UTIL.setupApp(config)
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        })
+
+        const headers = response.headers
+        headers.should.have.property('content-security-policy')
+        const csp = response.headers['content-security-policy']
+        csp.split(';').should.containEql('script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' plausible.io')
+    })
+
+    it('CSP should be enabled with posthog', async function () {
+        const config = {
+            telemetry: {
+                frontend: {
+                    posthog: {
+                        apikey: 'abcde1234'
+                    }
+                }
+            },
+            content_security_policy: {
+                enabled: true
+            }
+        }
+        app = await FF_UTIL.setupApp(config)
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        })
+
+        const headers = response.headers
+        headers.should.have.property('content-security-policy')
+        const csp = response.headers['content-security-policy']
+        csp.split(';').should.containEql('script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://app.posthog.com')
+    })
+
+    it('CSP should be enabled with hubspot', async function () {
+        const config = {
+            support: {
+                enabled: true,
+                frontend: {
+                    hubspot: {
+                        trackingcode: 'abcde1234'
+                    }
+                }
+            },
+            content_security_policy: {
+                enabled: true
+            }
+        }
+        app = await FF_UTIL.setupApp(config)
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        })
+
+        const headers = response.headers
+        headers.should.have.property('content-security-policy')
+        const csp = response.headers['content-security-policy']
+        csp.split(';').should.containEql('script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' js-eu1.hs-analytics.com js-eu1.hs-banner.com js-eu1.hs-scripts.com js-eu1.hscollectedforms.net js-eu1.hubspot.com js-eu1.usemessages.com')
+    })
+
+    it('CSP should be enabled with hubspot and posthog', async function () {
+        const config = {
+            support: {
+                enabled: true,
+                frontend: {
+                    hubspot: {
+                        trackingcode: 'abcde1234'
+                    }
+                }
+            },
+            telemetry: {
+                frontend: {
+                    posthog: {
+                        apikey: 'abcde1234'
+                    }
+                }
+            },
+            content_security_policy: {
+                enabled: true
+            }
+        }
+        app = await FF_UTIL.setupApp(config)
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        })
+
+        const headers = response.headers
+        headers.should.have.property('content-security-policy')
+        const csp = response.headers['content-security-policy']
+        csp.split(';').should.containEql('script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://app.posthog.com js-eu1.hs-analytics.com js-eu1.hs-banner.com js-eu1.hs-scripts.com js-eu1.hscollectedforms.net js-eu1.hubspot.com js-eu1.usemessages.com')
+    })
 })
