3ea8970a79c729d4745b3e109a5dd47f8dc5ddbd
Joe Pavitt <joepavitt@flowforge.com>
2023-11-17 12:35:39 +0000

    Remove duplicated Instance/DeploymentName component

--- frontend/src/pages/team/Instances.vue
+++ frontend/src/pages/team/Instances.vue
@@ -89,8 +89,8 @@ import { markRaw } from 'vue'
 import teamApi from '../../api/team.js'
 import EmptyState from '../../components/EmptyState.vue'
 import permissionsMixin from '../../mixins/Permissions.js'
+import DeploymentName from '../application/components/cells/DeploymentName.vue'
 import InstanceStatusBadge from '../instance/components/InstanceStatusBadge.vue'
-import InstanceName from '../instance/components/cells/InstanceName.vue'
 
 export default {
     name: 'TeamInstances',
@@ -114,7 +114,7 @@ export default {
             loading: false,
             instances: [],
             columns: [
-                { label: 'Name', class: ['flex-grow'], key: 'name', sortable: true, component: { is: markRaw(InstanceName) } },
+                { label: 'Name', class: ['flex-grow'], key: 'name', sortable: true, component: { is: markRaw(DeploymentName) } },
                 { label: 'Status', class: ['w-44'], key: 'status', sortable: true, component: { is: markRaw(InstanceStatusBadge) } },
                 { label: 'Last Updated', class: ['w-60'], key: 'flowLastUpdatedSince', sortable: true },
                 { label: 'Application', class: ['flex-grow-[0.25]'], key: 'application.name', sortable: true }
