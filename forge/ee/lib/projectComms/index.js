7b482150900d3c9d00ddc108e85bde57e311cbdb
Steve-Mcl <sdmclaughlin@gmail.com>
2023-11-21 22:06:56 +0000

    update project link topic/ACL to use `app:app-id`

--- forge/ee/lib/projectComms/index.js
+++ forge/ee/lib/projectComms/index.js
@@ -24,8 +24,7 @@ module.exports.init = function (app) {
             { topic: /^ff\/v1\/([^/]+)\/p\/([^/]+)\/in\/[^/]+($|\/.*$)$/, verify: 'checkTeamAndObjectIds', shared: true }
         )
         // Receive link-call response messages sent to this project
-        // - ff/v1/<team>/p/<project>/res/+/#
-        // - ff/v1/<team>/p/<project>/res-<id>/+/#
+        // - ff/v1/<team>/p/<project>/res[-id]/+/#
         app.comms.aclManager.addACL(
             'project',
             'sub',
@@ -74,47 +73,42 @@ module.exports.init = function (app) {
         app.comms.aclManager.addACL(
             'device',
             'sub',
-            { topic: /^ff\/v1\/([^/]+)\/p\/([^/]{11,})\/res\/[^/]+($|\/.*$)$/, verify: 'checkDeviceCanAccessProject' }
+            { topic: /^ff\/v1\/([^/]+)\/p\/(?!app:)([^/]+)\/res\/[^/]+($|\/.*$)$/, verify: 'checkDeviceCanAccessProject' }
         )
         // Receive link-call response messages sent to this device (app owned)
-        // - ff/v1/<team>/p/<appid>/res/+/#
+        // - ff/v1/<team>/p/app:<appid>/res/+/#
         app.comms.aclManager.addACL(
             'device',
             'sub',
-            { topic: /^ff\/v1\/([^/]+)\/p\/([^/]{10})\/res\/[^/]+($|\/.*$)$/, verify: 'checkDeviceAssignedToApplication' }
+            { topic: /^ff\/v1\/([^/]+)\/p\/app:([^/]+)\/res\/[^/]+($|\/.*$)$/, verify: 'checkDeviceAssignedToApplication' }
         )
-
         // Send message to specific project
-        // A device ID is exactly 10 chars long, so any topic with an ID longer than that
-        // is a project instance topic
-        // At this time, we don't support sending messages direct to an app owned device
         // - ff/v1/<team>/p/<project>/in/+/#
         app.comms.aclManager.addACL(
             'device',
             'pub',
-            { topic: /^ff\/v1\/([^/]+)\/p\/([^/]{11,})\/in\/[^/]+($|\/.*$)/, verify: 'checkDeviceCanAccessProject' }
+            { topic: /^ff\/v1\/([^/]+)\/p\/(?!app:)([^/]+)\/in\/[^/]+($|\/.*$)/, verify: 'checkDeviceCanAccessProject' }
         )
         // Send broadcast messages (from instance owned devices in the team)
         // - ff/v1/<team>/p/<project>/out/+/#
         app.comms.aclManager.addACL(
             'device',
             'pub',
-            { topic: /^ff\/v1\/([^/]+)\/p\/([^/]{11,})\/out\/[^/]+($|\/.*$)/, verify: 'checkDeviceCanAccessProject' }
+            { topic: /^ff\/v1\/([^/]+)\/p\/(?!app:)([^/]+)\/out\/[^/]+($|\/.*$)/, verify: 'checkDeviceCanAccessProject' }
         )
         // Send broadcast messages (from app owned devices in the team)
-        // - ff/v1/<team>/p/<device>/out/+/#
+        // - ff/v1/<team>/p/app:<appid>/out/+/#
         app.comms.aclManager.addACL(
             'device',
             'pub',
-            { topic: /^ff\/v1\/([^/]+)\/p\/([^/]{10})\/out\/[^/]+($|\/.*$)/, verify: 'checkDeviceAssignedToApplication' }
+            { topic: /^ff\/v1\/([^/]+)\/p\/app:([^/]+)\/out\/[^/]+($|\/.*$)/, verify: 'checkDeviceAssignedToApplication' }
         )
         // Send link-call response messages back to source instance
-        // app owned devices not supported at this time
         // - ff/v1/<team>/p/+/res/+/#
         app.comms.aclManager.addACL(
             'device',
             'pub',
-            { topic: /^ff\/v1\/([^/]+)\/p\/([^/]{11,})\/res\/[^/]+($|\/.*$)/, verify: 'checkDeviceCanAccessProject' }
+            { topic: /^ff\/v1\/([^/]+)\/p\/(?!app:)([^/]+)\/res\/[^/]+($|\/.*$)/, verify: 'checkDeviceCanAccessProject' }
         )
     }
 }
