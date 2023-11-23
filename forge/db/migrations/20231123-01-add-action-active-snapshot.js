26898c5171c9008fea8d51d1e7f38bed54fce921
Pez Cuckow <email@pezcuckow.com>
2023-11-23 12:35:58 +0100

    Guard against PipelineStages not existing

--- forge/db/migrations/20231123-01-add-action-active-snapshot.js
+++ forge/db/migrations/20231123-01-add-action-active-snapshot.js
@@ -10,6 +10,11 @@ module.exports = {
      * @param {QueryInterface} context Sequelize.QueryInterface
      */
     up: async (queryInterface) => {
+        const tableExists = await queryInterface.tableExists('PipelineStages')
+        if (!tableExists) {
+            return queryInterface.sequelize.log('Skipping PipelineStages migration as table does not exist')
+        }
+
         if (queryInterface.sequelize.options.dialog === 'postgres') {
             // Postgres enums need to be explicitly updated
             queryInterface.sequelize.query("ALTER TYPE \"enum_PipelineStages_action\" ADD VALUE 'use_active_snapshot';")
