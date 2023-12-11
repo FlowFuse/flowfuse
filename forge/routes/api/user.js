91ff752b419c6336c41f2a5fba6973d03a6f2a68
Ben Hardill <ben@flowforge.com>
2023-12-11 16:43:26 +0000

    Evaluate password strength on backend as well as UI

--- forge/routes/api/user.js
+++ forge/routes/api/user.js
@@ -77,7 +77,12 @@ module.exports = async function (app) {
             await app.postoffice.send(request.session.User, 'PasswordChanged', { })
             reply.send({ status: 'okay' })
         } catch (err) {
-            const resp = { code: 'password_change_failed', error: 'password change failed' }
+            let resp
+            if (err.message === 'Password Too Weak') {
+                resp = { code: 'password_change_failed_too_weak', error: 'password too weak'}
+            } else { 
+                resp = { code: 'password_change_failed', error: 'password change failed' }
+            }
             await app.auditLog.User.user.updatedPassword(request.session.User, resp)
             reply.code(400).send(resp)
         }
