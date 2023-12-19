8c9c80b9ce3111a6179e3788b420cbc9290c6003
Ben Hardill <ben@flowforge.com>
2023-12-19 10:13:21 +0000

    Add E2E UI test

---
+++ test/e2e/frontend/cypress/tests-ee/admin/templates.spec.js
@@ -0,0 +1,30 @@
+describe('FlowFuse - EE Templates', () => {
+    beforeEach(() => {
+        cy.login('alice', 'aaPassword')
+        cy.home()
+        cy.visit('/admin/templates')
+
+        // wait until two Stacks API calls complete
+        cy.intercept('GET', '/api/*/templates?*').as('getTemplates')
+        cy.wait('@getTemplates')
+    })
+
+    it('Alerts', () => {
+        cy.visit('/admin/templates')
+
+        // wait until two Stacks API calls complete
+        cy.intercept('GET', '/api/*/templates?*').as('getTemplates')
+        cy.wait('@getTemplates')
+
+        cy.get('[data-el="templates"] tbody tr:first td:nth-child(2)').click()
+        
+        cy.get('#ff-app > div > div.ff-layout--platform--wrapper > div.ff-view > main > div > div:nth-child(2) > ul > li').should('have.length', 5)
+        cy.get('#ff-app > div > div.ff-layout--platform--wrapper > div.ff-view > main > div > div:nth-child(2) > ul > li > a').contains('Alerts')
+        cy.get('#ff-app > div > div.ff-layout--platform--wrapper > div.ff-view > main > div > div:nth-child(2) > ul > li:last').click()
+
+        cy.get('#ff-app > div > div.ff-layout--platform--wrapper > div.ff-view > main > div > div:nth-child(2) > div > form > div.ff-radio-group > div > label:first > label').contains('Owners')
+        cy.get('#ff-app > div > div.ff-layout--platform--wrapper > div.ff-view > main > div > div:nth-child(2) > div > form > div.ff-radio-group > div > label:nth-child(2) > label').contains('Owners & Members')
+        cy.get('#ff-app > div > div.ff-layout--platform--wrapper > div.ff-view > main > div > div:nth-child(2) > div > form > div.ff-radio-group > div > label:nth-child(2) > label').contains('Members')
+        
+    })
+})
