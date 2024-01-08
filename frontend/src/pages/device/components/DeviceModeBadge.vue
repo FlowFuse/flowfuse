22243814f073e5c8f51cecf56f5ec26c6a398306
Pez Cuckow <email@pezcuckow.com>
2024-01-08 12:36:08 +0100

    Also label the fleet mode icon

--- frontend/src/pages/device/components/DeviceModeBadge.vue
+++ frontend/src/pages/device/components/DeviceModeBadge.vue
@@ -8,7 +8,7 @@
         }"
         :data-el="`${isBadge ? 'badge' : 'text'}-${mode === 'developer' ? 'dev' : 'fleet'}mode`"
     >
-        <span v-ff-tooltip="isIcon ? 'Developer Mode' : undefined" class="inline-flex space-x-2 items-center">
+        <span v-ff-tooltip="isIcon ? (isDevMode ? 'Developer Mode' : 'Fleet Mode') : undefined" class="inline-flex space-x-2 items-center">
             <template v-if="isDevMode">
                 <BeakerIcon :class="`text-purple-600 w-${size} h-${size}`" />
                 <span v-if="!isIcon" class="ml-1"> Developer Mode</span>
