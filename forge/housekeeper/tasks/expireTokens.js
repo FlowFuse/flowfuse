cbefb0311f0996db1b6557f14d7bf503e95deb19
Nick O'Leary <nick.oleary@gmail.com>
2024-01-11 19:23:14 +0000

    Change expireTokens task schedule

--- forge/housekeeper/tasks/expireTokens.js
+++ forge/housekeeper/tasks/expireTokens.js
@@ -1,9 +1,13 @@
 const { Op } = require('sequelize')
 
+const randomInt = (min, max) => { return min + Math.floor(Math.random() * (max - min)) }
+
 module.exports = {
     name: 'expireTokens',
     startup: true,
-    schedule: '@daily',
+    // Pick a random hour/minute for this task to run at. If the application is
+    // horizontal scaled, this will avoid two instances running at the same time
+    schedule: `${randomInt(0, 60)} ${randomInt(0, 24)} * * *`,
     run: async function (app) {
         await app.db.models.Session.destroy({ where: { expiresAt: { [Op.lt]: Date.now() } } })
         await app.db.models.AccessToken.destroy({ where: { expiresAt: { [Op.lt]: Date.now() } } })
