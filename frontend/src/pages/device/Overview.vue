dd5cf3308832c9ba57dc0b7cf2785387e38d70ca
Steve-Mcl <sdmclaughlin@gmail.com>
2024-01-16 13:06:10 +0000

    fix device overview polling

--- frontend/src/pages/device/Overview.vue
+++ frontend/src/pages/device/Overview.vue
@@ -111,6 +111,8 @@ import InfoCard from '../../components/InfoCard.vue'
 import InfoCardRow from '../../components/InfoCardRow.vue'
 import StatusBadge from '../../components/StatusBadge.vue'
 
+import { createPollTimer } from '../../utils/timers.js'
+
 import DeviceLastSeenBadge from './components/DeviceLastSeenBadge.vue'
 import DeviceModeBadge from './components/DeviceModeBadge.vue'
 
@@ -156,8 +158,18 @@ export default {
             return ''
         }
     },
+    data () {
+        return {
+            /** @type {import('../../utils/timers').PollTimer} */
+            polltimer: null
+        }
+    },
     mounted () {
         this.refreshDevice()
+        createPollTimer(this.refreshDevice, 10000)
+    },
+    unmounted () {
+        this.polltimer.stop()
     },
     methods: {
         refreshDevice: function () {
