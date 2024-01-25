9f2e9e4868545a53bdbc3e2a68e76fbd05f1064a
Steve-Mcl <sdmclaughlin@gmail.com>
2024-01-25 11:58:49 +0000

    add ui elements for Quick Connect feature

--- frontend/src/pages/team/Devices/dialogs/DeviceCredentialsDialog.vue
+++ frontend/src/pages/team/Devices/dialogs/DeviceCredentialsDialog.vue
@@ -16,17 +16,24 @@
                         To connect your device to the platform, use the following
                         configuration. This will need to be placed on your device.
                     </p>
-                    <p class="mt-3 mb-3">
+                    <p class="mt-4">
                         See the
                         <a
                             href="https://flowfuse.com/docs/device-agent/register/#connect-the-device" target="_blank"
                             rel="noreferrer"
                         >Connect Your Device</a> documentation for more information.
                     </p>
-                    <p class="font-bold mt-3 mb-6">
-                        Make a note of this configuration, as this is the only time you will see it.
-                    </p>
-                    <pre class="overflow-auto text-sm p-4 mt-6 border rounded bg-gray-800 text-gray-200">{{ credentials }}</pre>
+
+                    <p class="font-bold mt-4"><i>Make a note of this configuration, as this is the only time you will see it.</i></p>
+                    <pre class="overflow-auto text-sm p-4 my-2 border rounded bg-gray-800 text-gray-200">{{ credentials }}</pre>
+
+                    <template v-if="otc">
+                        <p class="mt-6">
+                            Alternatively, you can use the following Quick Connect command:
+                        </p>
+                        <pre class="overflow-auto text-xs font-light p-4 mt-2 border rounded bg-gray-800 text-gray-200">{{ otcCommand }}</pre>
+                        <p class="text-gray-600 italic text-xs">Note: the Quick Connect command is single use, expires in 24h and requires device-agent v2.1 or later.</p>
+                    </template>
                 </template>
             </form>
         </template>
@@ -36,8 +43,14 @@
                 <ff-button kind="danger" class="ml-4" @click="regenerateCredentials()">Regenerate configuration</ff-button>
             </template>
             <template v-else>
-                <ff-button v-if="clipboardSupported" kind="secondary" @click="copy()">Copy to Clipboard</ff-button>
-                <ff-button kind="secondary" @click="downloadCredentials()"><template #icon-left><DocumentDownloadIcon /></template>Download device-{{ device.id }}.yml</ff-button>
+                <ff-button v-if="clipboardSupported && !otc" kind="secondary" @click="copy(credentials)">Copy to Clipboard</ff-button>
+                <DropdownMenu v-if="clipboardSupported && otc" buttonClass="ff-btn ff-btn--secondary" :options="copyButtonOptions">
+                    Copy to Clipboard
+                </DropdownMenu>
+                <ff-button kind="secondary" @click="downloadCredentials()">
+                    <template #icon-left><DocumentDownloadIcon /></template>
+                    <span class="mr-2 whitespace-nowrap">Download device-{{ device.id }}.yml</span>
+                </ff-button>
                 <ff-button class="ml-4" @click="close()">Done</ff-button>
             </template>
         </template>
@@ -46,18 +59,19 @@
 
 <script>
 // import devicesApi from '../../../../api/devices'
-
 import { DocumentDownloadIcon } from '@heroicons/vue/outline'
 import { mapState } from 'vuex'
 
 import deviceApi from '../../../../api/devices.js'
+import DropdownMenu from '../../../../components/DropdownMenu.vue'
 import clipboardMixin from '../../../../mixins/Clipboard.js'
 import Alerts from '../../../../services/alerts.js'
 
 export default {
     name: 'DeviceCredentialsDialog',
     components: {
-        DocumentDownloadIcon
+        DocumentDownloadIcon,
+        DropdownMenu
     },
     mixins: [clipboardMixin],
     props: ['team'],
@@ -84,8 +98,8 @@ export default {
             this.$refs.dialog.close()
             this.device.credentials = undefined
         },
-        copy () {
-            this.copyToClipboard(this.credentials).then(() => {
+        copy (text) {
+            this.copyToClipboard(text).then(() => {
                 Alerts.emit('Copied to Clipboard.', 'confirmation')
             }).catch((err) => {
                 console.warn('Clipboard write permission denied: ', err)
@@ -97,6 +111,12 @@ export default {
         ...mapState('account', ['settings']),
         hasCredentials: function () {
             return this.device && this.device.credentials
+        },
+        otc: function () {
+            return this.device?.credentials?.otc
+        },
+        otcCommand: function () {
+            return `flowfuse-device-agent --qc -o ${this.otc} -u ${this.settings.base_url}`
         },
         credentials: function () {
             let result = ''
@@ -112,6 +132,15 @@ brokerUsername: ${this.device.credentials.broker.username}
 brokerPassword: ${this.device.credentials.broker.password}
 `
                 }
+            }
+            return result
+        },
+        copyButtonOptions: function () {
+            const result = [
+                { name: 'Device Configuration', action: () => this.copy(this.credentials), disabled: false }
+            ]
+            if (this.otc) {
+                result.push({ name: 'Quick Connect command', action: () => this.copy(this.otcCommand), disabled: false })
             }
             return result
         }
