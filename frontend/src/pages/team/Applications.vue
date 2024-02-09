1f9808911083c2f021c5cc96c0acb5247c36d676
Joe Pavitt <joepavitt@flowforge.com>
2024-02-09 13:28:42 +0000

    Improved responsiveness

--- frontend/src/pages/team/Applications.vue
+++ frontend/src/pages/team/Applications.vue
@@ -37,9 +37,11 @@
             <template v-else-if="!loading && applications.size > 0">
                 <ul class="ff-applications-list" data-el="applications-list">
                     <li v-for="application in Array.from(applications.values())" :key="application.id">
-                        <div class="ff-application-list--app gap-x-4 flex items-center !justify-start" data-action="view-application" @click="openApplication(application)">
-                            <span class="ff-application-list--icon flex flex-shrink-0 flex-grow-0 whitespace-nowrap"><TemplateIcon class="ff-icon text-gray-600" />{{ application.name }}</span>
-                            <span class="!inline-block !flex-shrink !flex-grow italic text-gray-500 dark:text-gray-400 truncate"> {{ application.description }} </span>
+                        <div class="ff-application-list--app gap-x-4 flex flex-col gap-2 sm:gap-0 justify-between sm:flex-row sm:items-center" data-action="view-application" @click="openApplication(application)">
+                            <div class="flex items-cente flex-wrap">
+                                <span class="ff-application-list--icon flex flex-shrink-0 flex-grow-0 whitespace-nowrap gap-2 w-full"><TemplateIcon class="ff-icon text-gray-600" />{{ application.name }}</span>
+                                <span class="!inline-block !flex-shrink !flex-grow italic text-gray-500 dark:text-gray-400 truncate"> {{ application.description }} </span>
+                            </div>
                             <ApplicationSummaryLabel :application="application" />
                         </div>
 
