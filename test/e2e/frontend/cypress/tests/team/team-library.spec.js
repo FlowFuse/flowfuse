ca40af3e0414e90709ed4473dc7406db6b494258
cstns <cstn.serban@gmail.com>
2024-04-19 18:46:56 +0300

    rm leftovers

--- test/e2e/frontend/cypress/tests/team/team-library.spec.js
+++ test/e2e/frontend/cypress/tests/team/team-library.spec.js
@@ -16,11 +16,4 @@ describe('FlowForge - Shared Team Library', () => {
         cy.contains('Create your own Team Library')
         cy.contains('This is a FlowFuse Premium feature. Please upgrade your instance of FlowFuse in order to use it.')
     })
-
-    // it.only('', () => {
-    // cy.intercept('/api/*/flow-blueprints?*', { meta: {}, count: 2, blueprints: [{ id: 'yJR1DQ9NbK', active: true, name: 'My first Blueprint', description: 'My first team', category: 'Basic Blueprint', icon: 'cog', order: 1, default: false, createdAt: '2024-04-16T15:14:16.780Z', updatedAt: '2024-04-17T08:02:35.066Z' }, { id: 'v0J85bLeA5', active: true, name: 'Other blueprint (blank)', description: 'some other description', category: 'Some other category', icon: 'briefcase', order: 0, default: true, createdAt: '2024-04-17T08:02:35.065Z', updatedAt: '2024-04-17T08:02:35.065Z' }] })
-    //     .as('getBlueprints')
-    // cy.wait(['@getBlueprints'])
-
-    // })
 })
