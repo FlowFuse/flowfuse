0c75ece67cd0d6b532fa2f88a8779d01d5175e85
ppawlowski <piotr@devopswizards.com>
2024-04-24 13:59:43 +0200

    Link default stack only to project type

--- .github/scripts/initial-setup.sh
+++ .github/scripts/initial-setup.sh
@@ -51,6 +51,16 @@ get_team_id() {
   echo "$TEAM_ID"
 }
 
+get_latest_image_tag() {
+  local NR_VERSION=$1
+  local IMAGE="flowfuse/node-red"
+  local TOKEN
+  local TAG
+  TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${IMAGE}:pull" | jq -r '.token')
+  TAG=$(curl -s -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/${IMAGE}/tags/list | jq -r '.tags[]' | grep -vE '^v' | grep "${NR_VERSION}" | sort -rV | head -n 1)
+  echo "$TAG"
+}
+
 create_suspended_instance() {
   local INSTANCE_NAME=$1
   local TEAM_APPLICATION_ID=$2
@@ -85,8 +95,23 @@ create_device() {
       "type": "'"$DEVICE_TYPE"'",
       "team": "'"$TEAM_ID"'"
       }' https://$FLOWFUSE_URL/api/v1/devices/
-
 }
+
+create_stack() {
+  local STACK_NAME=$1
+  local STACK_LABEL=$2
+  local STACK_CPU=$3
+  local STACK_MEMORY=$4
+  local STACK_CONTAINER=$5
+  echo "Creating $STACK_NAME stack"
+  projectTypeId=$(curl -ks -XGET -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" https://$FLOWFUSE_URL/api/v1/project-types/ | jq -r '.types[].id')
+  curl -ks -w "\n" -XPOST \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" \
+    -d '{"name":"'"$STACK_NAME"'","label":"'"$STACK_LABEL"'", "projectType":"'"$projectTypeId"'","properties":{ "cpu":'"$STACK_CPU"',"memory":'"$STACK_MEMORY"',"container":"'"$STACK_CONTAINER"'"}}' \
+    https://$FLOWFUSE_URL/api/v1/stacks/
+}
+
 
 DBPASSWORD=$(kubectl --namespace "pr-$PR_NUMBER" get secret flowfuse-pr-$PR_NUMBER-postgresql -o jsonpath='{.data.password}' | base64 -d)
 kubectl run flowfuse-setup-0 \
@@ -125,48 +150,14 @@ curl -ks -w "\n" -XPOST \
   -d '{"name":"Default", "description":"DefaultInstanceType","active": true}' \
   https://$FLOWFUSE_URL/api/v1/project-types/
 
-# ### Create default stack
-# echo "Creating default stack"
-# projectTypeId=$(curl -ks -XGET -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" https://$FLOWFUSE_URL/api/v1/project-types/ | jq -r '.types[].id')
-# curl -ks -XPOST \
-#   -H "Content-Type: application/json" \
-#   -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" \
-#   -d '{"name":"Default","label":"Default", "projectType":"'"$projectTypeId"'","properties":{ "cpu":10,"memory":256,"container":"flowfuse/node-red"}}' \
-#   https://$FLOWFUSE_URL/api/v1/stacks/
-
-create_stack() {
-  local STACK_NAME=$1
-  local STACK_LABEL=$2
-  local STACK_CPU=$3
-  local STACK_MEMORY=$4
-  local STACK_CONTAINER=$5
-  echo "Creating $STACK_NAME stack"
-  projectTypeId=$(curl -ks -XGET -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" https://$FLOWFUSE_URL/api/v1/project-types/ | jq -r '.types[].id')
-  curl -ks -w "\n" -XPOST \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" \
-    -d '{"name":"'"$STACK_NAME"'","label":"'"$STACK_LABEL"'", "projectType":"'"$projectTypeId"'","properties":{ "cpu":'"$STACK_CPU"',"memory":'"$STACK_MEMORY"',"container":"'"$STACK_CONTAINER"'"}}' \
-    https://$FLOWFUSE_URL/api/v1/stacks/
-}
-
-get_latest_image_tag() {
-  local NR_VERSION=$1
-  local IMAGE="flowfuse/node-red"
-  local TOKEN
-  local TAG
-  TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${IMAGE}:pull" | jq -r '.token')
-  TAG=$(curl -s -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/${IMAGE}/tags/list | jq -r '.tags[]' | grep -vE '^v' | grep "${NR_VERSION}" | sort -rV | head -n 1)
-  echo "$TAG"
-}
-
+### Create stacks
 create_stack "Default" "Default" 100 256 "flowfuse/node-red"
 create_stack "NR-31x" "3.1.x" 100 256 "flowfuse/node-red:$(get_latest_image_tag "3.1.x")"
 create_stack "NR-40x" "4.0.x" 100 256 "flowfuse/node-red:$(get_latest_image_tag "4.0.x")"
 
-
-### Link stack to project type
+### Link Default to project type
 echo "Linking stack to project type"
-stackId=$(curl -ks -XGET -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" https://$FLOWFUSE_URL/api/v1/stacks/ | jq -r '.stacks[].id')
+stackId=$(curl -ks -XGET -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" https://$FLOWFUSE_URL/api/v1/stacks/ | jq -r '.stacks[] | select(.name=="Default") | .id')
 curl -ks -XPUT \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $INIT_CONFIG_ACCESS_TOKEN" \
