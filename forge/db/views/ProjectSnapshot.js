df0b2afb6a0dde24acb056722f4d7baeca317d1b
Steve-Mcl <sdmclaughlin@gmail.com>
2024-04-29 18:12:19 +0100

    sanitise API output of snapshotExport

--- forge/db/views/ProjectSnapshot.js
+++ forge/db/views/ProjectSnapshot.js
@@ -80,14 +80,36 @@ module.exports = function (app) {
             updatedAt: { type: 'string' },
             user: { $ref: 'UserSummary' },
             exportedBy: { $ref: 'UserSummary' },
-            modules: { type: 'object', additionalProperties: true },
             flows: { type: 'object', additionalProperties: true },
             settings: { type: 'object', additionalProperties: true }
         }
     })
+    function snapshotExport (snapshot) {
+        if (snapshot) {
+            const result = snapshot.toJSON ? snapshot.toJSON() : snapshot
+            const filtered = {
+                id: result.hashid,
+                name: result.name,
+                description: result.description || '',
+                createdAt: result.createdAt,
+                updatedAt: result.updatedAt,
+                credentialSecret: result.credentialSecret
+            }
+            if (snapshot.User) {
+                filtered.user = app.db.views.User.userSummary(snapshot.User)
+            }
+            if (snapshot.exportedBy) {
+                filtered.user = app.db.views.User.userSummary(snapshot.exportedBy)
+            }
+            return filtered
+        } else {
+            return null
+        }
+    }
 
     return {
         snapshot,
-        snapshotSummary
+        snapshotSummary,
+        snapshotExport
     }
 }
