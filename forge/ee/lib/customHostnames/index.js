eb174637c46921b1121c6989b0554f24123d7574
Ben Hardill <ben@flowforge.com>
2024-05-13 17:55:00 +0100

    Fix driver options test

--- forge/ee/lib/customHostnames/index.js
+++ forge/ee/lib/customHostnames/index.js
@@ -1,7 +1,7 @@
 module.exports.init = function (app) {
     // TODO this should read kubernetes before check in
     if (app.config.driver.type === 'localfs' || app.config.driver.type === 'stub') {
-        if (app.config.driver.options.customHostnames) {
+        if (app.config.driver.options?.customHostnames) {
             app.config.features.register('customHostnames', true, true)
 
             app.db.models.Project.prototype.getCustomHostname = async function () {
