8562de1cd81079e0ff7f549ba7af35efba936d8e
cstns <cstn.serban@gmail.com>
2024-05-16 10:40:57 +0300

    Add blueprint preview on blueprint tiles

--- frontend/src/components/blueprints/BlueprintTile.vue
+++ frontend/src/components/blueprints/BlueprintTile.vue
@@ -12,6 +12,16 @@
                 <CheckCircleIcon class="ff-icon-lg" />
                 <label class="text-green-800">Default</label>
             </div>
+            <ff-button
+                v-if="!editable"
+                data-action="show-blueprint"
+                class="ff-btn--secondary"
+                @click="$refs['dialog'].show()"
+            >
+                <template #icon>
+                    <ProjectIcon />
+                </template>
+            </ff-button>
             <ff-button v-if="!editable" data-action="select-blueprint" @click="choose(blueprint)">
                 Select
             </ff-button>
@@ -19,6 +29,18 @@
                 Edit
             </ff-button>
         </div>
+        <ff-dialog
+            ref="dialog"
+            data-el="preview-blueprint-dialog"
+            :header="`Flow Preview: ${blueprint.name}`"
+            :closeOnConfirm="true"
+            confirmLabel="Close"
+            :canBeCanceled="false"
+        >
+            <template #default>
+                <ff-flow-viewer :flow="blueprint.flows" />
+            </template>
+        </ff-dialog>
     </div>
 </template>
 
@@ -27,12 +49,20 @@ import { CheckCircleIcon, QuestionMarkCircleIcon } from '@heroicons/vue/outline'
 import { defineAsyncComponent } from 'vue'
 import { mapState } from 'vuex'
 
+import ProjectIcon from '../../components/icons/Projects.js'
 import product from '../../services/product.js'
+import FfDialog from '../../ui-components/components/DialogBox.vue'
+import FormRow from '../FormRow.vue'
+import FlowViewer from '../flow-viewer/FlowViewer.vue'
 
 export default {
     name: 'BlueprintTile',
     components: {
-        CheckCircleIcon
+        FfDialog,
+        FormRow,
+        'ff-flow-viewer': FlowViewer,
+        CheckCircleIcon,
+        ProjectIcon
     },
     props: {
         blueprint: {
@@ -90,3 +120,19 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+.ff-dialog-container {
+  .ff-dialog-box {
+    max-width: 75rem;
+
+    .ff-dialog-content {
+      padding: 0;
+    }
+
+    .ff-dialog-actions {
+      padding: 5px 15px;
+    }
+  }
+}
+</style>
