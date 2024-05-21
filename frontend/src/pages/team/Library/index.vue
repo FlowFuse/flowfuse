2b4a1c38868f1eb0db5ce0ab34356d6d7a603db5
FriyadDev <riyadhasanblue2323@gmail.com>
2024-05-21 18:31:30 +0600

    fix: Lint errors has been resolved

--- frontend/src/pages/team/Library/index.vue
+++ frontend/src/pages/team/Library/index.vue
@@ -39,7 +39,7 @@ export default {
                         name: 'LibraryBlueprints'
 
                     }
-                },
+                }
             ]
         }
     }
--- frontend/src/pages/team/Library/routes.js
+++ frontend/src/pages/team/Library/routes.js
@@ -2,6 +2,6 @@ import Blueprints from './Blueprints.vue'
 import TeamLibrary from './TeamLibrary.vue'
 
 export default [
-    { name: 'LibraryTeamLibrary', path: 'team-library/:entryPath*', component: TeamLibrary },
     { name: 'LibraryBlueprints', path: 'blueprints', component: Blueprints },
+    { name: 'LibraryTeamLibrary', path: 'team-library/:entryPath*', component: TeamLibrary }
 ]
