24f8fbec62f225c7565de728519da088e2d5d895
cstns <cstn.serban@gmail.com>
2024-06-10 14:27:49 +0300

    Update e2e tests

--- test/e2e/frontend/cypress/tests/team/library.spec.js
+++ test/e2e/frontend/cypress/tests/team/library.spec.js
@@ -1,3 +1,12 @@
+import multipleBlueprints from '../../fixtures/blueprints/multiple-blueprints.json'
+
+function interceptBlueprints (blueprints = []) {
+    cy.intercept('/api/*/flow-blueprints?*', {
+        meta: {},
+        ...blueprints
+    }).as('getBlueprints')
+}
+
 describe('FlowForge - Library', () => {
     beforeEach(() => {
         cy.login('alice', 'aaPassword')
@@ -5,15 +14,39 @@ describe('FlowForge - Library', () => {
     })
 
     describe('Blueprints', () => {
-        it('should load the Library page and display the unavailable feature banner for the Blueprints tab', () => {
+        it.only('should load the Library page and display the unavailable feature banner for the Blueprints tab', () => {
+            interceptBlueprints(multipleBlueprints)
+
             cy.visit('team/ateam/library')
 
             cy.get('[data-el="page-name"]').contains('Library')
             cy.get('[data-el="ff-tab"]').contains('Blueprints').click()
 
-            cy.contains('Shared repository to store common flows and nodes.')
-            cy.contains('No Blueprints Available')
-            cy.contains('This is a FlowFuse Enterprise feature. Please upgrade your instance of FlowFuse in order to use it.')
+            cy.get('[data-el="category"]')
+                .contains('Category A')
+                .parent()
+                .within(() => {
+                    cy.get('[data-el="tiles-wrapper"]')
+                        .children()
+                        .should('have.length', 1)
+
+                    cy.contains('Blueprint 1')
+                    cy.contains('This is a blueprint')
+                })
+
+            cy.get('[data-el="category"]')
+                .contains('Category B')
+                .parent()
+                .within(() => {
+                    cy.get('[data-el="tiles-wrapper"]')
+                        .children()
+                        .should('have.length', 2)
+
+                    cy.contains('Blueprint 2')
+                    cy.contains('Blueprint 3')
+                    cy.contains('This is another blueprint')
+                    cy.contains('This is yet another blueprint')
+                })
         })
     })
     describe('Team Library', () => {
