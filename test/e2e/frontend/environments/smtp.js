d0e53e7a60495ec088835b3076e190ce09ba2459
cstns <cstn.serban@gmail.com>
2024-06-20 19:58:51 +0300

    Fix smtp server spawning after the forge app, causing the app to default to no email configuration

--- test/e2e/frontend/environments/smtp.js
+++ test/e2e/frontend/environments/smtp.js
@@ -4,46 +4,54 @@ const mailpitLogger = (data) => {
     console.info(`Mailpit: ${data}`)
 }
 
-module.exports = async function (config = {}) {
-    let { webPort, smtpPort } = config
+module.exports = function (config = {}) {
+    return new Promise((resolve, reject) => {
+        let { webPort, smtpPort } = config
 
-    webPort = webPort || 8025
-    smtpPort = smtpPort || 1025
+        webPort = webPort || 8025
+        smtpPort = smtpPort || 1025
 
-    mailpitLogger('Starting e-mail server...')
+        mailpitLogger('Starting e-mail server...')
 
-    const dockerCommand = 'docker'
-    const args = [
-        'run',
-        '--rm',
-        `-p=${webPort}:8025`,
-        `-p=${smtpPort}:1025`,
-        '-e=MP_MAX_MESSAGES=5000',
-        '-e=MP_SMTP_AUTH_ACCEPT_ANY=1',
-        '-e=MP_SMTP_AUTH_ALLOW_INSECURE=1',
-        'axllent/mailpit'
-    ]
+        const dockerCommand = 'docker'
+        const args = [
+            'run',
+            '--rm',
+            `-p=${webPort}:8025`,
+            `-p=${smtpPort}:1025`,
+            '-e=MP_MAX_MESSAGES=5000',
+            '-e=MP_SMTP_AUTH_ACCEPT_ANY=1',
+            '-e=MP_SMTP_AUTH_ALLOW_INSECURE=1',
+            'axllent/mailpit'
+        ]
 
-    const dockerProcess = childProcess.spawn(dockerCommand, args)
+        const dockerProcess = childProcess.spawn(dockerCommand, args)
 
-    dockerProcess.on('spawn', () => {
-        mailpitLogger(`Web UI available at http://localhost:${webPort}/ with SMTP listening on port ${smtpPort}`)
+        dockerProcess.on('spawn', () => {
+            mailpitLogger(`Web UI available at http://localhost:${webPort}/ with SMTP listening on port ${smtpPort}`)
+            resolve(dockerProcess)
+        })
+
+        // dockerProcess.stdout.on('data', mailpitLogger)
+        dockerProcess.stderr.on('data', mailpitLogger)
+
+        dockerProcess.on('close', (code) => {
+            console.error(`Child process exited with code ${code}`)
+        })
+
+        dockerProcess.on('error', (error) => {
+            mailpitLogger(error)
+            reject(error)
+        })
+
+        // Handle process exit to clean up the Docker container
+        // const cleanup = () => {
+        // ... clean up code here
+        //     process.exit(0)
+        // }
+
+        // process.on('exit', cleanup)
+        // process.on('SIGINT', cleanup)
+        // process.on('SIGTERM', cleanup)
     })
-
-    // dockerProcess.stdout.on('data', mailpitLogger)
-    dockerProcess.stderr.on('data', mailpitLogger)
-
-    dockerProcess.on('close', (code) => {
-        console.error(`Child process exited with code ${code}`)
-    })
-
-    // Handle process exit to clean up the Docker container
-    // const cleanup = () => {
-    // ... clean up code here
-    //     process.exit(0)
-    // }
-
-    // process.on('exit', cleanup)
-    // process.on('SIGINT', cleanup)
-    // process.on('SIGTERM', cleanup)
 }
