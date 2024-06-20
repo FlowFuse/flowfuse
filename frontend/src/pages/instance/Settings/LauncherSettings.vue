49aed17b6d2e81e385bb94a72779f99b57edd2c4
Steve-Mcl <sdmclaughlin@gmail.com>
2024-06-20 10:26:57 +0100

    reword to remove doubt about this being auto or manual restart

--- frontend/src/pages/instance/Settings/LauncherSettings.vue
+++ frontend/src/pages/instance/Settings/LauncherSettings.vue
@@ -109,7 +109,7 @@ export default {
             }
             await InstanceApi.updateInstance(this.project.id, { launcherSettings })
             this.$emit('instance-updated')
-            alerts.emit('Instance settings successfully updated. NOTE: changes will be applied once the instance is restarted.', 'confirmation', 6000)
+            alerts.emit('Instance settings successfully updated. Restart the instance to apply the changes.', 'confirmation', 6000)
         }
     }
 }
