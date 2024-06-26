bc7c686de4b8eea0488927e8aefd7e1e1a91f6f5
cstns <cstn.serban@gmail.com>
2024-06-26 14:38:52 +0300

    refactor applications directory structure, split into smaller chunks

---
+++ frontend/src/pages/team/Applications/components/ApplicationHeader.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="ff-application-list--app gap-x-4 flex flex-col gap-2 sm:gap-0 justify-between sm:flex-row sm:items-center" data-action="view-application" @click="openApplication(application)">
+        <div class="flex items-cente flex-wrap">
+            <span class="ff-application-list--icon flex flex-shrink-0 flex-grow-0 whitespace-nowrap gap-2 w-full"><TemplateIcon class="ff-icon text-gray-600" />{{ application.name }}</span>
+            <span class="!inline-block !flex-shrink !flex-grow italic text-gray-500 dark:text-gray-400 truncate"> {{ application.description }} </span>
+        </div>
+        <ApplicationSummaryLabel :application="application" />
+    </div>
+</template>
+
+<script>
+import { TemplateIcon } from '@heroicons/vue/outline'
+
+import ApplicationSummaryLabel from '../../components/ApplicationSummaryLabel.vue'
+
+export default {
+    name: 'ApplicationHeader',
+    components: {
+        ApplicationSummaryLabel,
+        TemplateIcon
+    },
+    props: {
+        application: {
+            type: Object,
+            required: true,
+            default: null
+        }
+    },
+    methods: {
+        openApplication (application) {
+            this.$router.push({
+                name: 'Application',
+                params: {
+                    id: application.id
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/pages/team/Applications/components/DevicesWrapper.vue
@@ -0,0 +1,58 @@
+<template>
+    <div v-if="hasNoDevices" class="ff-no-data">
+        This Application currently has no
+        <router-link :to="`/application/${application.id}/devices`" class="ff-link">attached devices</router-link>
+        .
+    </div>
+
+    <section v-else class="ff-applications-list-instances" data-el="application-devices">
+        <label>Devices</label>
+        <div v-for="device in Array.from(application.devices.values())" :key="device.id" @click.stop="openDevice(device)">
+            <ApplicationDevice :device="device" />
+        </div>
+    </section>
+
+    <div v-if="hasMoreDevices" class="ff-applications-list--details">
+        Only the {{ application.devices.size }}
+        <router-link :to="`/application/${application.id}/devices`" class="ff-link">devices</router-link>
+        with the most recent activity are being displayed.
+    </div>
+</template>
+
+<script>
+import ApplicationDevice from './ApplicationDevice.vue'
+
+export default {
+    name: 'DevicesWrapper',
+    components: { ApplicationDevice },
+    props: {
+        application: {
+            type: Object,
+            required: true,
+            default: null
+        }
+    },
+    computed: {
+        hasMoreDevices () {
+            return this.application.deviceCount > this.application.devices.size
+        },
+        hasNoDevices () {
+            return this.application.devices.size === 0
+        }
+    },
+    methods: {
+        openDevice (device) {
+            this.$router.push({
+                name: 'Device',
+                params: {
+                    id: device.id
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
