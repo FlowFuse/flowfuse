51d81be07cdad925415a79924521893d2ca2444e
cstns <cstn.serban@gmail.com>
2024-07-04 17:44:59 +0300

    adding the concept of email layouts, starting with a default template that mimics inbound marketing templates

---
+++ forge/postoffice/layouts/default.js
@@ -0,0 +1,131 @@
+module.exports = (htmlContent) => {
+    return `
+    <div lang="en" style="background-color:#eff6ff; height: 100%;" bgcolor="#eff6ff">
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+        }
+        p {
+            line-height: 175%;
+            margin-bottom: 10px
+        }
+    </style>
+    <table role="presentation" cellpadding="0" cellspacing="0"
+           style="border-spacing:0!important;border-collapse:collapse;margin:0;padding:0;width:100%!important;min-width:320px!important;height:100%!important"
+           width="100%" height="100%">
+        <tbody>
+        <tr>
+            <td valign="top"
+                style="border-collapse:collapse;font-family:Helvetica,Arial,sans-serif;font-size:15px;color:#33475b;word-break:break-word;padding-top:20px;padding-bottom:20px">
+                <div style="color:inherit;font-size:inherit;line-height:inherit">
+                    <div style="padding-left:10px;padding-right:10px">
+                        <div style="min-width:280px;max-width:600px;Margin-left:auto;Margin-right:auto;border-collapse:collapse;border-spacing:0;background-color:#ffffff;background-image:url('https://ci3.googleusercontent.com/meips/ADKq_NYZ5s3vhxS876pXulEbo8DVtN8wNlpMAbGoFQVLNFJYPoyh2AAaCuQ_J0ioqXn96D3rfqx6Qn93fQtle4KYAd8UkzYBzwsrvY2Y5DXIs32PhpdN2jkEvWg7GeO2xbF30QhqRGXgIAHAk-vXW91N-1e7wjM=s0-d-e1-ft#https://hs-26586079.f.hubspotemail-eu1.net/hub/26586079/hubfs/600x70-HS-newsletter-header.png');background-position:center;background-repeat:no-repeat;background-size:100% 100%"
+                             bgcolor="#ffffff">
+                            <div>
+                                <div style="color:inherit;font-size:inherit;line-height:inherit">
+                                    <table role="presentation" width="100%" cellpadding="0" cellspacing="0"
+                                           style="border-spacing:0!important;border-collapse:collapse">
+                                        <tbody>
+                                        <tr>
+                                            <td align="center" valign="top"
+                                                style="border-collapse:collapse;font-family:Helvetica,Arial,sans-serif;color:#33475b;word-break:break-word;text-align:center;padding:16px 20px;font-size:0px">
+                                                <a href="https://d36SjQ04.eu1.hubspotlinks.com/Ctc/2O+113/d36SjQ04/VXhyfN1XfxvSN7DVt9RVX1RXW6fpr4J5h26zmN1HNyZC3m2ndW69sMD-6lZ3lmW5Xd9565BgPszW8Fpcbj3NcTt7W1tN6lL8yWfgJW3s_56k5m4TQhW2BBprf87LLNmW4rn-zF2BCG8ZW3RF6S07hb5vHW75RF3p38tx39W5WGBPG1FSYcFW30DQr356J295W8bgdV95VvCn5Vb7SC57YD3PLW6ZLSsY6Dgml0W8d0sss8SmR7zW8V0pDB4w2LDCW1B_9cd8GHwkrW3_jjt88JtddqW776mvY75pBw_W6kcDXd3Sb2R5W7HVsMm1g4G02f7l3Lqs04"
+                                                   style="color:#00a4bd" target="_blank"
+                                                   data-saferedirecturl="https://www.google.com/url?q=https://d36SjQ04.eu1.hubspotlinks.com/Ctc/2O%2B113/d36SjQ04/VXhyfN1XfxvSN7DVt9RVX1RXW6fpr4J5h26zmN1HNyZC3m2ndW69sMD-6lZ3lmW5Xd9565BgPszW8Fpcbj3NcTt7W1tN6lL8yWfgJW3s_56k5m4TQhW2BBprf87LLNmW4rn-zF2BCG8ZW3RF6S07hb5vHW75RF3p38tx39W5WGBPG1FSYcFW30DQr356J295W8bgdV95VvCn5Vb7SC57YD3PLW6ZLSsY6Dgml0W8d0sss8SmR7zW8V0pDB4w2LDCW1B_9cd8GHwkrW3_jjt88JtddqW776mvY75pBw_W6kcDXd3Sb2R5W7HVsMm1g4G02f7l3Lqs04&amp;source=gmail&amp;ust=1720088505083000&amp;usg=AOvVaw1OD6cimWfS-QbEB0HECrQW">
+                                                    <img alt="ff-logo--wordmark--dark"
+                                                         src="https://ci3.googleusercontent.com/meips/ADKq_Nbd7Br3HsYg6RUTevzZIWsq3vk6TkN7vp3-0SLJ4tLGizyyUgSyegc67Pwl6DMKN5Q26l3scS8yk1obwffEoxD_HG2czDsEplDIx1Y0NsxffgeQSjtcTFZywDukys79bItk5TSh7SNv8H7e2AYjZHJ5M5635Wv3RmP5ueDbpMLnBhde_N0_CLIpJwxxmVYzA9L3qDHgqt1TWbIx0WjhMqRUz1cmwwkKig=s0-d-e1-ft#https://hs-26586079.f.hubspotemail-eu1.net/hub/26586079/hubfs/ff-logo--wordmark--dark.png?width=366&amp;upscale=true&amp;name=ff-logo--wordmark--dark.png"
+                                                         style="outline:none;text-decoration:none;border:none;max-width:100%;font-size:16px"
+                                                         width="183" align="middle" data-bit="iit">
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding-left:10px;padding-right:10px">
+                        <div style="min-width:280px;max-width:600px;Margin-left:auto;Margin-right:auto;border-collapse:collapse;border-spacing:0;background-color:#ffffff"
+                             bgcolor="#FFFFFF">
+                            <div>
+                                <table role="presentation" cellpadding="0" cellspacing="0" width="100%"
+                                       style="border-spacing:0!important;border-collapse:collapse">
+                                    <tbody>
+                                    <tr>
+                                        <td style="border-collapse:collapse;font-family:Helvetica,Arial,sans-serif;font-size:15px;color:#33475b;word-break:break-word;padding:20px 40px 0">
+                                            <div style="color:inherit;font-size:inherit;line-height:inherit">
+                                                <div style="color:inherit;font-size:inherit;line-height:inherit">
+                                                    ${htmlContent}
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div style="padding-left:10px;padding-right:10px">
+                        <div style="min-width:280px;max-width:600px;Margin-left:auto;Margin-right:auto;border-collapse:collapse;border-spacing:0;background-color:#ffffff"
+                             bgcolor="#FFFFFF">
+                            <div>
+                                <table role="presentation" cellpadding="0" cellspacing="0" width="100%"
+                                       style="border-spacing:0!important;border-collapse:collapse">
+                                    <tbody>
+                                    <tr>
+                                        <td style="border-collapse:collapse;font-family:Helvetica,Arial,sans-serif;font-size:15px;color:#33475b;word-break:break-word;padding:30px 20px 0px">
+                                            <div style="color:inherit;font-size:inherit;line-height:inherit">
+                                                <table role="none" border="0" cellpadding="0" cellspacing="0"
+                                                       style="border-spacing:0!important;border-collapse:collapse;vertical-align:top"
+                                                       width="100%">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td align="center"
+                                                            style="border-collapse:collapse;font-family:Helvetica,Arial,sans-serif;color:#33475b;font-size:0;word-break:break-word">
+                                                            <p style="font-size:1px;border-top:1px solid #eff6ff;width:90%;margin:0 auto"></p>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding-left:10px;padding-right:10px">
+                        <div style="min-width:280px;max-width:600px;Margin-left:auto;Margin-right:auto;border-collapse:collapse;border-spacing:0;padding-bottom:20px">
+                            <div>
+                                <div style="color:inherit;font-size:inherit;line-height:inherit">
+                                    <table role="presentation" width="100%" cellpadding="0" cellspacing="0"
+                                           style="border-spacing:0!important;border-collapse:collapse;font-family:Arial,sans-serif;font-size:12px;line-height:135%;color:#23496d;margin-bottom:0;padding:0">
+                                        <tbody>
+                                        <tr>
+                                            <td align="center" valign="top"
+                                                style="border-collapse:collapse;font-family:Helvetica,Arial,sans-serif;font-size:15px;color:#33475b;word-break:break-word;text-align:center;margin-bottom:0;line-height:135%;padding:10px 20px">
+                                                <p style="font-family:Helvetica,Arial,sans-serif;font-size:12px;font-weight:normal;text-decoration:none;font-style:normal;color:#33475b">
+                                                    FlowFuse Inc, PO Box 7775 #29439, San Francisco, California
+                                                </p>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+`
+}
