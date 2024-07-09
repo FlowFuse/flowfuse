51320481732dcbe9297875e2feabe9565519ad04
ppawlowski <piotr@devopswizards.com>
2024-07-09 16:05:51 +0200

    Revert random code change

--- forge/app.js
+++ forge/app.js
@@ -67,7 +67,6 @@ const forge = require('./forge')
                 server.log.info('* FlowFuse is now running and can be accessed at: *')
                 server.log.info(`*   ${server.config.base_url.padEnd(47, ' ')}*`)
                 server.log.info('****************************************************')
-                server.log.info('****************************************************')
             }
             if (enableRepl) {
                 const repl = require('repl')
