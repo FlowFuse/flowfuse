297eb1bda601fd5cec5212a39bf2c0b53a1c790a
cstns <cstn.serban@gmail.com>
2024-07-12 11:29:26 +0300

    carry the search query through the applications component and pass it to the applicationDevices & applicationInstances routes

---
+++ frontend/src/pages/team/Applications/components/compact/HasMoreTile.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="has-more item-wrapper">
+        <router-link :to="{name: linkTo, params: {id: application.id, searchQuery: searchQuery}}">
+            <span>
+                {{ remaining }}
+                More...
+            </span>
+            <ChevronRightIcon class="ff-icon" />
+        </router-link>
+    </div>
+</template>
+
+<script>
+import { ChevronRightIcon } from '@heroicons/vue/solid'
+
+export default {
+    name: 'HasMoreTile',
+    components: { ChevronRightIcon },
+    props: {
+        application: {
+            type: Object,
+            required: true
+        },
+        remaining: {
+            type: Number,
+            required: true
+        },
+        linkTo: {
+            type: String,
+            required: true
+        },
+        searchQuery: {
+            type: String,
+            required: false,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
