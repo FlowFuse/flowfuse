27aedb280da5b8946ed26c42595ec0c362f3ed06
Joe Pavitt <99246719+joepavitt@users.noreply.github.com>
2024-07-17 10:02:04 +0100

    Update frontend/src/components/audit-log/AuditEntryVerbose.vue

--- frontend/src/components/audit-log/AuditEntryVerbose.vue
+++ frontend/src/components/audit-log/AuditEntryVerbose.vue
@@ -629,7 +629,7 @@
 
     <template v-else-if="entry.event === 'resource.cpu'">
         <label>Instance High CPU usage</label>
-        <span>Instance has spent more than {{ Math.floor(entry.body.interval / 60) }} minutes at more than {{ entry.body.threshold }}% of CPU limit. This instance may benefit from a larger Instance</span>
+        <span>Instance has spent more than {{ Math.floor(entry.body.interval / 60) }} minutes at more than {{ entry.body.threshold }}% of CPU limit. This instance may benefit from being upgraded to a larger Instance</span>
     </template>
 
     <template v-else-if="entry.event === 'resource.memory'">
