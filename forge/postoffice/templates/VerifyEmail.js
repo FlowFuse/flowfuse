59ce309e70302ccff24b3e2daed75ef40fd264d1
Nick O'Leary <nick.oleary@gmail.com>
2024-07-17 16:02:51 +0100

    Apply suggestions from code review
    Co-authored-by: Stephen McLaughlin <44235289+Steve-Mcl@users.noreply.github.com>

--- forge/postoffice/templates/VerifyEmail.js
+++ forge/postoffice/templates/VerifyEmail.js
@@ -8,11 +8,14 @@ Please use this code to verify your email address:
 {{{ token.token }}}
 
 Do not share this code with anyone else.
+
+This token will expire in 30 minutes.
 `,
     html:
 `<p>Hello, <b>{{{safeName.html}}}</b>,</p>
 <p>Please use this code to verify your email address:</p>
 <p><b>{{{ token.token }}}</b></p>
-<p>Do not share this code with anyone else.
+<p>Do not share this code with anyone else.</p>
+<p>This token will expire in 30 minutes.</p>
 `
 }
