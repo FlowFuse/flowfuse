4fa5064f15e71e38ef4db9fb1ede1cb8c1606f01
Joe Pavitt <joepavitt@flowforge.com>
2024-07-18 11:26:15 +0100

    Hide the invite button in header when on the invite page

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -37,7 +37,7 @@
         </div>
         <div class="hidden lg:flex">
             <ff-team-selection data-action="team-selection" />
-            <div class="px-4 flex flex-col justify-center ff-border-left">
+            <div class="px-4 flex flex-col justify-center ff-border-left" v-if="showInviteButton">
                 <ff-button kind="secondary" @click="inviteTeamMembers">
                     <template #icon-left><UserAddIcon /></template>
                     Invite Members
@@ -122,6 +122,9 @@ export default {
                     onclick: this.signOut
                 }
             ].filter(option => option !== undefined)
+        },
+        showInviteButton () {
+            return this.$route.name !== 'TeamMembers'
         }
     },
     watch: {
