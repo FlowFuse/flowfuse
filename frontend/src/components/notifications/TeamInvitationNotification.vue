3317e35cba7643d24bf777c89ba2fb1f3c261b08
Joe Pavitt <joepavitt@flowforge.com>
2024-07-19 12:34:21 +0100

    Improve structure of notifications/message & cleaner CSS

--- frontend/src/components/notifications/TeamInvitationNotification.vue
+++ frontend/src/components/notifications/TeamInvitationNotification.vue
@@ -1,34 +1,30 @@
 <template>
-    <div class="message" data-el="invitation-message">
-        <div class="icon">
-            <UserAddIcon class="ff-btn--icon" />
-        </div>
-        <div class="body">
-            <div class="header">
-                <router-link class="title" :to="{name: 'User Invitations'}" @click="closeRightDrawer">
-                    <h4>Team Invitation</h4>
-                </router-link>
-            <!--            <ff-checkbox :model-value="isSelected" label="" @click.prevent.stop="toggleSelection" />-->
-            </div>
-            <div class="content">
-                You have been invited by <i>{{ invitorName }}</i> to join <i>{{ teamName }}</i>.
-            </div>
-            <div class="footer">
-                {{ notification.createdSince }}
-            </div>
-        </div>
-    </div>
+    <NotificationMessage data-el="invitation-message" :to="{name: 'User Invitations'}">
+        <template #icon>
+            <UserAddIcon />
+        </template>
+        <template #title>
+            Team Invitation
+        </template>
+        <template #message>
+            You have been invited by <i>"{{ invitorName }}"</i> to join <i>"{{ teamName }}"</i>.
+        </template>
+        <template #timestamp>
+            {{ notification.createdSince }}
+        </template>
+    </NotificationMessage>
 </template>
 
 <script>
 import { UserAddIcon } from '@heroicons/vue/solid'
-import { mapActions } from 'vuex'
 
 import NotificationMessageMixin from '../../mixins/NotificationMessage.js'
 
+import NotificationMessage from './Notification.vue'
+
 export default {
     name: 'TeamInvitationNotification',
-    components: { UserAddIcon },
+    components: { NotificationMessage, UserAddIcon },
     mixins: [NotificationMessageMixin],
     computed: {
         invitorName () {
@@ -37,9 +33,6 @@ export default {
         teamName () {
             return this.notification.team.name
         }
-    },
-    methods: {
-        ...mapActions('ux', ['closeRightDrawer'])
     }
 }
 </script>
