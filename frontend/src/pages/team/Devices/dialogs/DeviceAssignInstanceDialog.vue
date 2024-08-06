ca8584e3539823be26acf10f6875d76eef827a46
Steve-Mcl <sdmclaughlin@gmail.com>
2024-08-06 20:39:23 +0100

    Modify device assign dialog to support bulk ops

--- frontend/src/pages/team/Devices/dialogs/DeviceAssignInstanceDialog.vue
+++ frontend/src/pages/team/Devices/dialogs/DeviceAssignInstanceDialog.vue
@@ -1,17 +1,24 @@
 <template>
     <ff-dialog
         ref="dialog"
-        header="Add Device to Instance"
+        :header="bulkOp ? 'Move Selected Devices to Instance' : 'Add Device to Instance'"
         class="ff-dialog-fixed-height"
-        confirm-label="Add"
+        :confirm-label="bulkOp ? 'Move' : 'Add'"
         data-el="assign-device-to-instance-dialog"
         @confirm="assignDevice()"
     >
         <template #default>
             <form class="space-y-6 mt-2 mb-2">
-                <p class="text-sm text-gray-500">
-                    Select the Node-RED instance you want to bind the device to.
-                </p>
+                <p>The following device{{ devices.length > 1 ? 's' : '' }} will be affected by this operation:</p>
+                <div class="max-h-48 overflow-y-auto">
+                    <ul class="list-disc list-inside">
+                        <li v-for="device in devices" :key="device.id">
+                            <span class="font-bold">{{ device.name }}</span> <span class="text-gray-500 text-sm"> ({{ device.id }})</span>
+                        </li>
+                    </ul>
+                </div>
+                <hr>
+                <p>Select the Node-RED instance you want to bind the device{{ devices.length > 1 ? 's' : '' }} to.</p>
                 <SelectInstance v-model="input.instance" :team="team" />
             </form>
         </template>
@@ -28,29 +35,39 @@ export default {
     components: {
         SelectInstance
     },
-    emits: ['assignDevice'],
+    emits: ['assignDevice', 'moveDevices'],
     setup () {
         return {
-            async show (device) {
+            async show (selection) {
                 this.$refs.dialog.show()
-                this.device = device
+                this.selection = selection
             }
         }
     },
     data () {
         return {
-            device: null,
+            selection: null,
             input: {
                 instance: null
             }
         }
     },
     computed: {
-        ...mapState('account', ['team'])
+        ...mapState('account', ['team']),
+        bulkOp () {
+            return Array.isArray(this.selection)
+        },
+        devices () {
+            return this.bulkOp ? this.selection : [this.selection]
+        }
     },
     methods: {
         assignDevice () {
-            this.$emit('assignDevice', this.device, this.input.instance.id)
+            if (this.bulkOp) {
+                this.$emit('moveDevices', this.selection, this.input.instance.id)
+            } else {
+                this.$emit('assignDevice', this.selection, this.input.instance.id)
+            }
         }
     }
 }
