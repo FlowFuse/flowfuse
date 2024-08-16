4817197722257004f27c7e4b12ef883e0aa6502c
Joe Pavitt <joepavitt@flowforge.com>
2024-08-16 15:23:34 +0100

    Rework the product code to match PH's NodeJS API & remove client-side event

--- forge/product/index.js
+++ forge/product/index.js
@@ -7,28 +7,18 @@ module.exports = fp(async function (app, _opts) {
         const apiHost = _opts?.apiurl || 'https://app.posthog.com'
         const apiKey = _opts?.apikey
         const options = {
-            api_host: apiHost
-        }
-        if ('capture_pageview' in _opts) {
-            options.capture_pageview = _opts.capture_pageview
+            host: apiHost
         }
 
-        client = new PostHog(apiKey, options.opts)
+        client = new PostHog(apiKey, options)
     }
 
-    function capture (event, properties, groups) {
+    function capture (distinctId, event, properties, groups) {
         if (client) {
-            // need to add in team data somehow?
-
             if (!properties) {
                 properties = {}
             }
-            if (groups) {
-                properties.$groups = groups
-            }
-            // Get User E-Mail
-            const distinctId = 'email-address'
-            client.capture({ distinctId, event, properties })
+            client.capture({ distinctId, event, properties, groups })
         }
     }
 
