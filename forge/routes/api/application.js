6ce0bbdaf45b2a40a36a8235b5c751c7f554d2ef
Steve-Mcl <sdmclaughlin@gmail.com>
2024-08-22 18:03:16 +0100

    Refactor application route preHandler logic into shared module

--- forge/routes/api/application.js
+++ forge/routes/api/application.js
@@ -1,30 +1,7 @@
+const applicationShared = require('./shared/application.js')
+
 module.exports = async function (app) {
-    app.addHook('preHandler', async (request, reply) => {
-        const applicationId = request.params.applicationId
-        if (applicationId === undefined) {
-            return
-        }
-
-        if (!applicationId) {
-            return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
-        }
-
-        try {
-            request.application = await app.db.models.Application.byId(applicationId)
-            if (!request.application) {
-                return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
-            }
-
-            if (request.session.User) {
-                request.teamMembership = await request.session.User.getTeamMembership(request.application.Team.id)
-                if (!request.teamMembership && !request.session.User.admin) {
-                    return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
-                }
-            }
-        } catch (err) {
-            return reply.code(500).send({ code: 'unexpected_error', error: err.toString() })
-        }
-    })
+    app.addHook('preHandler', applicationShared.defaultPreHandler.bind(null, app))
 
     /**
      * Create an application
