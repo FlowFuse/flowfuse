dbe5dbe285ef6d09b9c7abe0335a84d10dc5e741
Ben Hardill <ben@flowforge.com>
2024-08-28 10:40:49 +0100

    Update forge/ee/lib/sso/index.js

--- forge/ee/lib/sso/index.js
+++ forge/ee/lib/sso/index.js
@@ -431,7 +431,7 @@ module.exports.init = async function (app) {
     // LDAP Group Membership
     async function updateTeamMembershipLDAP (adminClient, user, userDN, providerOpts) {
         const filter = `(|(uniqueMember=${userDN})(member=${userDN}))`
-        const { searchEntries } = await adminClient.search(providerOpts.options.groupsDN, {
+        const { searchEntries } = await adminClient.search(providerOpts.groupsDN, {
             filter,
             attributes: ['cn']
         })
