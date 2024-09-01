6972360322c7eb20045948a26f8d7aaf1e60b0c0
ppawlowski <piotr@devopswizards.com>
2024-09-01 12:09:16 +0200

    Enable all features for Enterprise team type

--- .github/scripts/initial-setup.sh
+++ .github/scripts/initial-setup.sh
@@ -273,19 +273,21 @@ curl -ks -w "\n" -XPOST \
                         "limit": 10
                     },
                     "features": {
-                        "shared-library": true,
-                        "projectComms": true,
-                        "ha": true,
-                        "teamHttpSecurity": true,
-                        "customCatalogs": true,
-                        "deviceGroups": true,
-                        "emailAlerts": true,
-                        "protectedInstance": true,
-                        "deviceAutoSnapshot": true,
-                        "instanceAutoSnapshot": true,
-                        "editorLimits": true,
-                        "fileStorageLimit": null,
-                        "contextLimit": null
+                        "ha":true,
+                        "shared-library":true,
+                        "projectComms":true,
+                        "teamHttpSecurity":true,
+                        "fileStorageLimit":null,
+                        "contextLimit":null,
+                        "customCatalogs":true,
+                        "deviceGroups":true,
+                        "emailAlerts":true,
+                        "deviceAutoSnapshot":true,
+                        "instanceAutoSnapshot":true,
+                        "protectedInstance":true,
+                        "editorLimits":true,
+                        "customHostnames":true,
+                        "staticAssets":true
                     },
                     "instances": {
                         "'"$projectTypeId"'": {
