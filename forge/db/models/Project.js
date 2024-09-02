279dd380e10426a5e05e52ebf0384f22a3902dc3
cstns <cstn.serban@gmail.com>
2024-09-02 19:31:14 +0300

    parametrize the byTeam Project static query to be able to include projectSettings relations in order to be able to display the dashboard button on the instances list page
    Also simplifies the byTeamForDashboard query to only include the settings key table entries

--- forge/db/models/Project.js
+++ forge/db/models/Project.js
@@ -432,32 +432,40 @@ module.exports = {
                         include
                     })
                 },
-                byTeam: async (teamHashId) => {
+                byTeam: async (teamHashId, { includeSettings = false } = {}) => {
                     const teamId = M.Team.decodeHashid(teamHashId)
-                    return this.findAll({
-                        include: [
-                            {
-                                model: M.Team,
-                                where: { id: teamId },
-                                attributes: ['hashid', 'id', 'name', 'slug', 'links', 'TeamTypeId']
-                            },
-                            {
-                                model: M.Application,
-                                attributes: ['hashid', 'id', 'name', 'links']
-                            },
-                            {
-                                model: M.ProjectType,
-                                attributes: ['hashid', 'id', 'name']
-                            },
-                            {
-                                model: M.ProjectStack
-                            },
-                            {
-                                model: M.ProjectTemplate,
-                                attributes: ['hashid', 'id', 'name', 'links']
-                            }
-                        ]
-                    })
+                    const include = [
+                        {
+                            model: M.Team,
+                            where: { id: teamId },
+                            attributes: ['hashid', 'id', 'name', 'slug', 'links', 'TeamTypeId']
+                        },
+                        {
+                            model: M.Application,
+                            attributes: ['hashid', 'id', 'name', 'links']
+                        },
+                        {
+                            model: M.ProjectType,
+                            attributes: ['hashid', 'id', 'name']
+                        },
+                        {
+                            model: M.ProjectStack
+                        },
+                        {
+                            model: M.ProjectTemplate,
+                            attributes: ['hashid', 'id', 'name', 'links']
+                        }
+                    ]
+
+                    if (includeSettings) {
+                        include.push({
+                            model: M.ProjectSettings,
+                            attributes: ['id', 'key', 'value', 'projectId'],
+                            where: { key: 'settings' }
+                        })
+                    }
+
+                    return this.findAll({ include })
                 },
                 getProjectTeamId: async (id) => {
                     const project = await this.findOne({
@@ -488,7 +496,8 @@ module.exports = {
                             },
                             {
                                 model: M.ProjectSettings,
-                                attributes: ['id', 'key', 'value', 'projectId']
+                                attributes: ['id', 'key', 'value', 'projectId'],
+                                where: { key: 'settings' }
                             }
                         ]
                     })
