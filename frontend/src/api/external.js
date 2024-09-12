dda04d0094217fb63d2034cdff356c848b208b7f
cstns <cstn.serban@gmail.com>
2024-09-12 12:33:11 +0300

    allow querying specific dependency versions

--- frontend/src/api/external.js
+++ frontend/src/api/external.js
@@ -1,7 +1,7 @@
 import client from './client.js'
 
-const getNpmDependency = async (dependency) => {
-    return client.get(`https://registry.npmjs.com/${dependency}`)
+const getNpmDependency = async (dependency, version = '') => {
+    return client.get(`https://registry.npmjs.com/${dependency}/${version}`)
         .then(res => {
             return res.data
         })
