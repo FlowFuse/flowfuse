55fc8dc72b229e03359b824d25d4deea8bba85e7
cstns <cstn.serban@gmail.com>
2024-10-01 17:00:18 +0300

    add a new accepted invitation notification e2e test

--- test/e2e/frontend/cypress/tests/notifications.spec.js
+++ test/e2e/frontend/cypress/tests/notifications.spec.js
@@ -1,5 +1,7 @@
 const aliceInviteToATeam = require('../fixtures/notifications/alice-invites-to-ateam.json')
 const bobInviteToATeam = require('../fixtures/notifications/bob-invites-to-bteam.json')
+const eddyAcceptedInvite = require('../fixtures/notifications/eddy-accepted-invite-to-ateam.json')
+
 describe('FlowForge - Notifications', () => {
     describe('Team Invitations', () => {
         describe('appear as notification messages', () => {
@@ -135,6 +137,59 @@ describe('FlowForge - Notifications', () => {
                 cy.get('[data-el="notifications-button"]')
                     .should('exist')
                     .contains(1).should('not.exist')
+            })
+
+            it.only('shows a user accepted invitation notification', () => {
+                cy.login('alice', 'aaPassword')
+
+                cy.intercept('/api/*/user').as('getUser')
+                cy.intercept('/api/*/settings').as('getSettings')
+                cy.intercept('/api/*/user/teams').as('getTeams')
+                cy.intercept('/api/*/user/notifications', {
+                    meta: {},
+                    count: 2,
+                    notifications: [
+                        {
+                            ...eddyAcceptedInvite,
+                            createdAt: new Date().setTime((new Date()).getTime() - 3600000)
+                        }
+                    ]
+                }).as('getNotifications')
+
+                cy.intercept('PUT', '/api/*/user/notifications/*', {}).as('markInvitationRead')
+
+                cy.intercept('/api/*/admin/stats').as('getAdminStats')
+                cy.intercept('/api/*/admin/license').as('getAdminLicense')
+
+                cy.visit('/')
+
+                cy.wait('@getUser')
+                cy.wait('@getSettings')
+                cy.wait('@getTeams')
+                cy.wait('@getNotifications')
+
+                cy.get('[data-el="desktop-nav-right"]').within(() => {
+                    cy.get('[data-el="notifications-button"]')
+                        .should('exist')
+                        .contains(1)
+
+                    cy.get('[data-el="notifications-button"]').click()
+                })
+
+                cy.get('[data-el="right-drawer"]').should('be.visible')
+
+                cy.get('[data-el="right-drawer"]').within(() => {
+                    cy.get('[data-el="notifications-drawer"]')
+
+                    cy.get('[data-el="invitation-message"]').should('have.length', 1)
+
+                    cy.get('[data-el="invitation-message"]').contains('Team Invitation: Accepted')
+                    cy.get('[data-el="invitation-message"]').contains('"eddy" has accepted your invitation to join "Alice\'s Team" as a "dashboard".')
+
+                    cy.get('[data-el="invitation-message"]').contains('Team Invitation').click()
+                    cy.wait('@markInvitationRead')
+                    cy.url().should('include', '/members/general')
+                })
             })
         })
     })
