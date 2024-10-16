4e0b3a8a3765299c001f6688c89a4eb48c53a17b
cstns <cstn.serban@gmail.com>
2024-10-16 10:21:27 +0300

    adding timeline scaffolding

---
+++ frontend/src/pages/instance/VersionHistory/Timeline/components/TimelineEvent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="event flex justify-between gap-2 items-center">
+        <div class="body flex flex-1 justify-between gap-2 items-center">
+            <div class="graph">
+                graph
+            </div>
+            <div class="content flex flex-1 flex-col justify-start">
+                <div class="title">title</div>
+                <div class="details">details</div>
+            </div>
+            <div class="username">username</div>
+        </div>
+        <div class="actions">
+            ...
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TimelineEvent',
+    props: {
+        event: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.event {
+    padding: 15px 10px;
+}
+</style>
--- frontend/src/pages/instance/VersionHistory/Timeline/index.vue
+++ frontend/src/pages/instance/VersionHistory/Timeline/index.vue
@@ -1,13 +1,50 @@
 <template>
-    <h1 class="text-center">  History Timeline Goes Here  </h1>
+    <section id="visual-timeline">
+        <ul class="timeline">
+            <li v-for="event in timeline" :key="event.id">
+                <timeline-event :event="event" />
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
+import projectHistoryAPI from '../../../../api/projectHistory.js'
+
+import TimelineEvent from './components/TimelineEvent.vue'
 export default {
-    name: 'HistoryTimeline'
+    name: 'HistoryTimeline',
+    components: { TimelineEvent },
+    inheritAttrs: false,
+    props: {
+        instance: {
+            type: Object,
+            required: true
+        }
+    },
+    data () {
+        return {
+            timeline: []
+        }
+    },
+    mounted () {
+        this.fetchData()
+    },
+    methods: {
+        async fetchData () {
+            const payload = await projectHistoryAPI.getHistory(this.instance.id)
+
+            this.timeline = payload.timeline
+        }
+    }
 }
 </script>
 
 <style scoped lang="scss">
-
+#visual-timeline {
+    .timeline {
+        border: 1px solid $ff-grey-300;
+        border-radius: 3px;
+    }
+}
 </style>
