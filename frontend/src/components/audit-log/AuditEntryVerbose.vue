f6e6971781f55af20c25ea08423e2d21fc280ea3
Ben Hardill <ben@flowforge.com>
2024-10-23 09:28:44 +0100

    Audit log shows empty username for password resets
    While looking at a customer issue I noticed that if a user asks for a reset from the login page the audit log entry doesn't show anything useful as it only tries to display the username but only the email address is available.

--- frontend/src/components/audit-log/AuditEntryVerbose.vue
+++ frontend/src/components/audit-log/AuditEntryVerbose.vue
@@ -192,7 +192,7 @@
     </template>
     <template v-else-if="entry.event === 'account.forgot-password'">
         <label>{{ AuditEvents[entry.event] }}</label>
-        <span v-if="!error && entry.body?.user">'{{ entry.body.user?.name }}' has forgotten their password.</span>
+        <span v-if="!error && entry.body?.user">'{{ entry.body.user?.name || entry.body.user?.email }}' has forgotten their password.</span>
         <span v-else-if="!error">User data not found in audit entry.</span>
     </template>
     <template v-else-if="entry.event === 'account.reset-password'">
