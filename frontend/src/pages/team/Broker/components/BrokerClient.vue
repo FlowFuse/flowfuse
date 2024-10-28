b425ce3858db1911aad58c6e3270261355313c99
cstns <cstn.serban@gmail.com>
2024-10-28 11:41:34 +0200

    extract broker client into separate component, fix client name text copy not working on ffox by using existing text-copier component

---
+++ frontend/src/pages/team/Broker/components/BrokerClient.vue
@@ -0,0 +1,188 @@
+<template>
+    <ff-accordion class="max-w-full w-full broker-client">
+        <template #label>
+            <div class="username text-left flex">
+                <text-copier :text="client.username + '@' + team.id" @click.prevent.stop>
+                    <span :title="client.username + '@' + team.id">
+                        <span class="mt-1 font-bold">{{ client.username }}</span>
+                        <span class="italic mt-1">@{{ team.id }}</span>
+                    </span>
+                </text-copier>
+            </div>
+            <div class="rules text-left">
+                <span>{{ client.acls.length }} Rule{{ client.acls.length > 1 ? 's' : '' }}</span>
+            </div>
+        </template>
+        <template #meta>
+            <span
+                class="edit hover:cursor-pointer"
+                data-action="edit-client"
+                @click.prevent.stop="$emit('edit-client', client)"
+            >
+                <PencilIcon
+                    v-if="hasAMinimumTeamRoleOf(Roles.Owner)"
+                    class="ff-icon-sm"
+                />
+            </span>
+            <span
+                class="delete hover:cursor-pointer "
+                data-action="delete-client"
+                @click.prevent.stop="$emit('delete-client',client)"
+            >
+                <TrashIcon
+                    v-if="hasAMinimumTeamRoleOf(Roles.Owner)"
+                    class="ff-icon-sm text-red-500"
+                />
+            </span>
+        </template>
+        <template #content>
+            <ul class="acl-list">
+                <li v-for="(acl, $key) in client.acls" :key="$key" class="acl grid grid-cols-6 gap-4" data-el="acl">
+                    <div class="action col-start-2 flex gap-2.5">
+                        <span
+                            :class="{
+                                'text-green-500': ['publish', 'both'].includes(acl.action),
+                                'text-red-500': ['subscribe'].includes(acl.action)
+                            } "
+                        >
+                            <CheckIcon v-if="['publish', 'both'].includes(acl.action)" class="ff-icon-sm" />
+                            <XIcon v-else class="ff-icon-sm" />
+                            pub
+                        </span>
+                        <span
+                            :class="{
+                                'text-green-500': ['subscribe', 'both'].includes(acl.action),
+                                'text-red-500': ['publish'].includes(acl.action)
+                            } "
+                        >
+                            <CheckIcon v-if="['subscribe', 'both'].includes(acl.action)" class="ff-icon-sm" />
+                            <XIcon v-else class="ff-icon-sm" />
+                            sub
+                        </span>
+                    </div>
+                    <div class="pattern">
+                        {{ acl.pattern }}
+                    </div>
+                </li>
+            </ul>
+        </template>
+    </ff-accordion>
+</template>
+
+<script>
+import { CheckIcon, PencilIcon, TrashIcon, XIcon } from '@heroicons/vue/outline'
+
+import FfAccordion from '../../../../components/Accordion.vue'
+import TextCopier from '../../../../components/TextCopier.vue'
+import permissionsMixin from '../../../../mixins/Permissions.js'
+import { Roles } from '../../../../utils/roles.js'
+
+export default {
+    name: 'BrokerClient',
+    components: {
+        TextCopier,
+        PencilIcon,
+        CheckIcon,
+        FfAccordion,
+        TrashIcon,
+        XIcon
+    },
+    mixins: [permissionsMixin],
+    props: {
+        client: {
+            required: true,
+            type: Object
+        }
+    },
+    emits: ['edit-client', 'delete-client'],
+    computed: {
+        Roles () {
+            return Roles
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.ff-accordion.broker-client {
+    margin-bottom: 0;
+
+    button {
+        border: none;
+        background: none;
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        gap: 15px;
+        padding: 0;
+
+        .username {
+            padding: 15px 10px;
+            grid-column: span 2;
+
+            .ff-icon {
+                margin-left: 0;
+                min-width: 20px;
+            }
+        }
+
+        .rules {
+            padding: 15px 10px;
+
+        }
+
+        .toggle {
+            grid-column: span 3;
+            text-align: right;
+            padding-right: 10px;
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+
+            .edit, .delete {
+                padding: 24px 15px;
+                display: inline-block;
+                position: relative;
+                align-self: stretch;
+
+                .ff-icon-sm {
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    transition: ease-in-out .3s;
+                }
+
+                &:hover {
+                    .ff-icon-sm {
+                        width: 20px;
+                        height: 20px;
+                    }
+                }
+            }
+
+            .edit:hover {
+                color: $ff-grey-700;
+            }
+            .delete:hover {
+                color: $ff-red-700;
+            }
+        }
+    }
+
+    .ff-accordion--content {
+        background: $ff-grey-100;
+        .acl-list {
+            .acl {
+                border-bottom: 1px solid $ff-grey-200;
+                padding: 15px 10px;
+                gap: 10px;
+                font-size: 80%;
+
+                &:last-of-type {
+                    border: none;
+                }
+            }
+        }
+    }
+}
+</style>
--- frontend/src/pages/team/Broker/index.vue
+++ frontend/src/pages/team/Broker/index.vue
@@ -67,72 +67,11 @@
                             </div>
                             <ul data-el="clients-list" class="clients-list">
                                 <li v-for="client in filteredClients" :key="client.id" class="client" data-el="client">
-                                    <ff-accordion class="max-w-full w-full">
-                                        <template #label>
-                                            <div class="username text-left flex">
-                                                <span class="mt-1 font-bold">{{ client.username }}</span><span class="italic mt-1">@{{ team.id }}</span>
-                                                <ff-button v-if="!!clipboardSupported" class="ml-2" kind="tertiary" size="small" @click.prevent.stop="copy(client)">
-                                                    <template #icon-right><ClipboardCopyIcon /></template>
-                                                </ff-button>
-                                            </div>
-                                            <div class="rules text-left">
-                                                <span>{{ client.acls.length }} Rule{{ client.acls.length > 1 ? 's' : '' }}</span>
-                                            </div>
-                                        </template>
-                                        <template #meta>
-                                            <span
-                                                class="edit hover:cursor-pointer"
-                                                data-action="edit-client"
-                                                @click.prevent.stop="editClient(client)"
-                                            >
-                                                <PencilIcon
-                                                    v-if="hasAMinimumTeamRoleOf(Roles.Owner)"
-                                                    class="ff-icon-sm"
-                                                />
-                                            </span>
-                                            <span
-                                                class="delete hover:cursor-pointer "
-                                                data-action="delete-client"
-                                                @click.prevent.stop="deleteClient(client)"
-                                            >
-                                                <TrashIcon
-                                                    v-if="hasAMinimumTeamRoleOf(Roles.Owner)"
-                                                    class="ff-icon-sm text-red-500"
-                                                />
-                                            </span>
-                                        </template>
-                                        <template #content>
-                                            <ul class="acl-list">
-                                                <li v-for="(acl, $key) in client.acls" :key="$key" class="acl grid grid-cols-6 gap-4" data-el="acl">
-                                                    <div class="action col-start-2 flex gap-2.5">
-                                                        <span
-                                                            :class="{
-                                                                'text-green-500': ['publish', 'both'].includes(acl.action),
-                                                                'text-red-500': ['subscribe'].includes(acl.action)
-                                                            } "
-                                                        >
-                                                            <CheckIcon v-if="['publish', 'both'].includes(acl.action)" class="ff-icon-sm" />
-                                                            <XIcon v-else class="ff-icon-sm" />
-                                                            pub
-                                                        </span>
-                                                        <span
-                                                            :class="{
-                                                                'text-green-500': ['subscribe', 'both'].includes(acl.action),
-                                                                'text-red-500': ['publish'].includes(acl.action)
-                                                            } "
-                                                        >
-                                                            <CheckIcon v-if="['subscribe', 'both'].includes(acl.action)" class="ff-icon-sm" />
-                                                            <XIcon v-else class="ff-icon-sm" />
-                                                            sub
-                                                        </span>
-                                                    </div>
-                                                    <div class="pattern">
-                                                        {{ acl.pattern }}
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </template>
-                                    </ff-accordion>
+                                    <broker-client
+                                        :client="client"
+                                        @edit-client="onEditClient"
+                                        @delete-client="onDeleteClient"
+                                    />
                                 </li>
                                 <li v-if="!filteredClients.length" class="text-center p-5">
                                     No clients found by that name.
@@ -177,11 +116,10 @@
 </template>
 
 <script>
-import { CheckIcon, ClipboardCopyIcon, PencilIcon, PlusSmIcon, SearchIcon, TrashIcon, XIcon } from '@heroicons/vue/outline'
+import { PlusSmIcon, SearchIcon } from '@heroicons/vue/outline'
 import { mapState } from 'vuex'
 
 import brokerApi from '../../../api/broker.js'
-import FfAccordion from '../../../components/Accordion.vue'
 import EmptyState from '../../../components/EmptyState.vue'
 import clipboardMixin from '../../../mixins/Clipboard.js'
 import featuresMixin from '../../../mixins/Features.js'
@@ -190,21 +128,18 @@ import Alerts from '../../../services/alerts.js'
 import Dialog from '../../../services/dialog.js'
 import { Roles } from '../../../utils/roles.js'
 
+import BrokerClient from './components/BrokerClient.vue'
+
 import ClientDialog from './dialogs/ClientDialog.vue'
 
 export default {
     name: 'TeamBroker',
     components: {
-        FfAccordion,
+        BrokerClient,
         SearchIcon,
         PlusSmIcon,
-        PencilIcon,
-        TrashIcon,
-        CheckIcon,
-        XIcon,
         EmptyState,
-        ClientDialog,
-        ClipboardCopyIcon
+        ClientDialog
     },
     mixins: [permissionsMixin, featuresMixin, clipboardMixin],
     data () {
@@ -259,10 +194,10 @@ export default {
         async createClient () {
             this.$refs.clientDialog.showCreate()
         },
-        async editClient (client) {
+        async onEditClient (client) {
             this.$refs.clientDialog.showEdit(client)
         },
-        async deleteClient (client) {
+        async onDeleteClient (client) {
             await Dialog.show({
                 header: 'Delete Client',
                 text: 'Are you sure you want to delete this Client?',
@@ -272,15 +207,6 @@ export default {
                 await brokerApi.deleteClient(this.team.id, client.username)
                 await this.fetchData()
                 Alerts.emit('Successfully deleted Client.', 'confirmation')
-            })
-        },
-        copy (client) {
-            const username = `${client.username}@${this.team.id}`
-            this.copyToClipboard(username).then(() => {
-                Alerts.emit('Copied to Clipboard.', 'confirmation')
-            }).catch((err) => {
-                console.warn('Clipboard write permission denied: ', err)
-                Alerts.emit('Clipboard write permission denied.', 'warning')
             })
         }
     }
@@ -316,85 +242,6 @@ export default {
                 &:last-of-type {
                     border-bottom: none;
                 }
-
-               .ff-accordion {
-                   margin-bottom: 0;
-
-                   button {
-                       border: none;
-                       background: none;
-                       display: grid;
-                       grid-template-columns: repeat(6, 1fr);
-                       gap: 15px;
-                       padding: 0;
-
-                       .username {
-                           padding: 15px 10px;
-                           grid-column: span 2;
-                       }
-
-                       .rules {
-                           padding: 15px 10px;
-
-                       }
-
-                       .toggle {
-                           grid-column: span 3;
-                           text-align: right;
-                           padding-right: 10px;
-                           display: flex;
-                           align-items: center;
-                           justify-content: flex-end;
-
-                           .edit, .delete {
-                               padding: 24px 15px;
-                               display: inline-block;
-                               position: relative;
-                               align-self: stretch;
-
-                               .ff-icon-sm {
-                                   position: absolute;
-                                   top: 50%;
-                                   left: 50%;
-                                   transform: translate(-50%, -50%);
-                                   transition: ease-in-out .3s;
-                               }
-
-                               &:hover {
-                                   .ff-icon-sm {
-                                       width: 20px;
-                                       height: 20px;
-                                   }
-                               }
-                           }
-
-                           .edit:hover {
-                               //color: green;
-                               color: $ff-grey-700;
-                           }
-                           .delete:hover {
-                               //color: yellow;
-                               color: $ff-red-700;
-                           }
-                       }
-                   }
-
-                   .ff-accordion--content {
-                       background: $ff-grey-100;
-                       .acl-list {
-                           .acl {
-                               border-bottom: 1px solid $ff-grey-200;
-                               padding: 15px 10px;
-                               gap: 10px;
-                               font-size: 80%;
-
-                               &:last-of-type {
-                                   border: none;
-                               }
-                           }
-                       }
-                   }
-               }
             }
         }
     }
