4349e980b0b9d455f32a3850e39eaf3306bd6ce7
Steve-Mcl <sdmclaughlin@gmail.com>
2024-11-09 15:09:24 +0000

    Add support for querying children

--- forge/db/models/AuditLog.js
+++ forge/db/models/AuditLog.js
@@ -33,24 +33,15 @@ module.exports = {
                     return M.AuditLog.forEntity(where, pagination)
                 },
                 forProject: async (projectId, pagination = {}) => {
-                    const where = {
-                        entityId: projectId.toString(),
-                        entityType: 'project'
-                    }
+                    const where = await M.AuditLog.buildScopeClause('project', projectId, pagination)
                     return M.AuditLog.forEntity(where, pagination)
                 },
                 forTeam: async (teamId, pagination = {}) => {
-                    const where = {
-                        entityId: teamId.toString(),
-                        entityType: 'team'
-                    }
+                    const where = await M.AuditLog.buildScopeClause('team', teamId, pagination)
                     return M.AuditLog.forEntity(where, pagination)
                 },
                 forApplication: async (applicationId, pagination = {}) => {
-                    const where = {
-                        entityId: applicationId.toString(),
-                        entityType: 'application'
-                    }
+                    const where = await M.AuditLog.buildScopeClause('application', applicationId, pagination)
                     return M.AuditLog.forEntity(where, pagination)
                 },
                 forDevice: async (deviceId, pagination = {}) => {
@@ -69,18 +60,19 @@ module.exports = {
                         where.id = { [Op.lt]: M.AuditLog.decodeHashid(pagination.cursor) }
                         delete pagination.cursor
                     }
+                    const whereFinal = buildPaginationSearchClause(
+                        pagination,
+                        where,
+                        // These are the columns that are searched using the `query` query param
+                        ['AuditLog.event', 'AuditLog.body', 'User.username', 'User.name'],
+                        // These map additional query params to specific columns to allow filtering
+                        {
+                            event: 'AuditLog.event',
+                            username: 'User.username'
+                        }
+                    )
                     const { count, rows } = await this.findAndCountAll({
-                        where: buildPaginationSearchClause(
-                            pagination,
-                            where,
-                            // These are the columns that are searched using the `query` query param
-                            ['AuditLog.event', 'AuditLog.body', 'User.username', 'User.name'],
-                            // These map additional query params to specific columns to allow filtering
-                            {
-                                event: 'AuditLog.event',
-                                username: 'User.username'
-                            }
-                        ),
+                        where: whereFinal,
                         order: [['createdAt', 'DESC']],
                         include: {
                             model: M.User,
@@ -202,6 +194,172 @@ module.exports = {
                     result.count = rows.length
                     result.timeline = rows
                     return result
+                },
+                buildScopeClause: async (entityType, entityId, pagination) => {
+                    /*
+                    The AuditLogs table has entityType [platform|team|application|project|device] and an associated entityId which is dependent on the entityType.
+                    To get entries for the team, we need to get:
+                        * all entries where AuditLog.entityType = 'team' and AuditLog.entityId = teamId
+                        * all entries where AuditLog.entityType = 'project' and Project.TeamId = teamId where Project.id = AuditLog.entityId
+                        * all entries where AuditLog.entityType = 'device' and Device.TeamId = teamId where Device.id = AuditLog.entityId
+                    To get entries for the application we need to get:
+                        * all entries where AuditLog.entityType = 'application' and Application.TeamId = teamId where Application.id = AuditLog.entityId
+                    To get entries for the project, we need to get:
+                        * all entries where AuditLog.entityType = 'project' and Project.ApplicationId = applicationId where Project.id = AuditLog.entityId
+                    To get entries for devices, we need to get:
+                        * all entries where AuditLog.entityType = 'device' and Device.ProjectId = projectId where Device.id = AuditLog.entityId
+                        OR
+                        * all entries where AuditLog.entityType = 'device' and Device.ApplicationId = applicationId where Device.id = AuditLog.entityId
+                    Then there are filtering considerations like includeChildren, and the top level scope. e.g. In getting entries for a team, the user
+                    may only want application or project scoped entries. In getting entries for an application, the user may only want project or device scoped entries.
+                    The below code handles all these considerations.
+                    */
+
+                    const permittedEntityScopes = {
+                        team: ['team', 'application', 'project', 'device'],
+                        application: ['application', 'project', 'device'],
+                        project: ['project', 'device']
+                    }
+                    if (permittedEntityScopes[entityType] === undefined) {
+                        throw new Error(`Invalid audit entity: ${entityType}`)
+                    }
+                    const permittedScopes = permittedEntityScopes[entityType]
+                    const scope = (pagination.scope || entityType)
+                    const includeChildren = [true, 'true', '1'].includes(pagination.includeChildren)
+                    delete pagination.includeChildren
+                    delete pagination.scope
+                    if (!permittedScopes.includes(scope)) {
+                        throw new Error(`Invalid audit scope: ${scope}`)
+                    }
+
+                    const whereClauses = []
+                    const addTeamScope = async (teamId, includeChildren = false) => {
+                        whereClauses.push({
+                            entityType: 'team',
+                            entityId: teamId
+                        })
+                        if (includeChildren) {
+                            await addApplicationScope(teamId, null, false, false) // only the applications (instances and devices will be included by addInstanceScope and addDeviceScope)
+                            await addInstanceScope(teamId, null, null, false) // only the instances (devices will be included by addDeviceScope)
+                            await addDeviceScope(teamId, null)
+                        }
+                    }
+                    const addApplicationScope = async (teamId, applicationId = null, includeInstances = false, includeApplicationDevices = false, includeInstanceDevices = false) => {
+                        let applicationIds = []
+                        if (applicationId) {
+                            applicationIds = [applicationId]
+                            whereClauses.push({
+                                entityType: 'application',
+                                entityId: applicationId
+                            })
+                        } else {
+                            const clause = { TeamId: teamId }
+                            applicationIds = (await M.Application.findAll({ where: clause, attributes: ['id'] })).map(a => a.id)
+                            if (applicationIds.length) {
+                                whereClauses.push({
+                                    entityType: 'application',
+                                    entityId: { [Op.in]: applicationIds }
+                                })
+                            }
+                        }
+                        if (applicationId === null && includeInstances && includeApplicationDevices && includeInstanceDevices) {
+                            // Since applicationId is null, we are doing this for the team.
+                            // And since all of the flags are all true, we can just add all instances and all devices
+                            // belonging to the team (regardless of who owns them) instead of adding them iteratively
+                            await addInstanceScope(teamId, null, null, false) // only the instances (devices will be included by addDeviceScope)
+                            await addDeviceScope(teamId, null) // all devices belonging to the team
+                        } else {
+                            if (includeInstances) {
+                                for (const appId of applicationIds) {
+                                    await addInstanceScope(teamId, appId, null, includeInstanceDevices)
+                                }
+                            }
+                            if (includeApplicationDevices) {
+                                for (const appId of applicationIds) {
+                                    await addDeviceScope(teamId, appId, null)
+                                }
+                            }
+                        }
+                    }
+                    const addInstanceScope = async (teamId, applicationId = null, instanceId = null, includeInstanceDevices = false) => {
+                        let instanceIds = []
+                        if (instanceId) {
+                            instanceIds = [instanceId]
+                            whereClauses.push({
+                                entityType: 'project',
+                                entityId: instanceId
+                            })
+                        } else {
+                            const clause = { TeamId: teamId }
+                            if (applicationId) {
+                                clause.ApplicationId = applicationId
+                            }
+                            instanceIds = (await M.Project.findAll({ where: clause, attributes: ['id'] })).map(p => p.id)
+                            if (instanceIds.length) {
+                                whereClauses.push({
+                                    entityType: 'project',
+                                    entityId: { [Op.in]: instanceIds }
+                                })
+                            }
+                        }
+                        if (includeInstanceDevices) {
+                            for (const instanceId of instanceIds) {
+                                await addDeviceScope(teamId, null, instanceId)
+                            }
+                        }
+                    }
+                    const addDeviceScope = async (teamId, applicationId = null, instanceId = null) => {
+                        const clause = { TeamId: teamId }
+                        if (instanceId) {
+                            clause.ProjectId = instanceId
+                        }
+                        if (applicationId) {
+                            clause.ApplicationId = applicationId
+                        }
+                        const ids = (await M.Device.findAll({ where: clause, attributes: ['id'] })).map(d => d.id)
+                        if (ids.length) {
+                            whereClauses.push({
+                                entityType: 'device',
+                                entityId: { [Op.in]: ids }
+                            })
+                        }
+                    }
+
+                    if (entityType === 'team') {
+                        const teamId = entityId
+                        if (scope === 'team') {
+                            await addTeamScope(teamId, includeChildren)
+                        } else if (scope === 'application') {
+                            await addApplicationScope(teamId, null, includeChildren, includeChildren, includeChildren)
+                        } else if (scope === 'project') {
+                            await addInstanceScope(teamId, null, null, includeChildren)
+                        } else if (scope === 'device') {
+                            await addDeviceScope(teamId) // all devices belonging to the team
+                        }
+                    } else if (entityType === 'application') {
+                        const applicationId = entityId
+                        const application = await M.Application.byId(applicationId)
+                        const teamId = application.TeamId
+                        if (scope === 'application') {
+                            await addApplicationScope(teamId, application.id, includeChildren, includeChildren, includeChildren)
+                        } else if (scope === 'project') {
+                            await addInstanceScope(teamId, application.id, null, includeChildren)
+                        } else if (scope === 'device') {
+                            await addDeviceScope(teamId, application.id) // all devices belonging to the application
+                        }
+                    } else if (entityType === 'project') {
+                        const projectId = entityId
+                        const project = await M.Project.byId(projectId)
+                        const teamId = project.TeamId
+                        await addInstanceScope(teamId, null, projectId, includeChildren)
+                    }
+
+                    if (whereClauses.length === 1) {
+                        return whereClauses[0]
+                    } else if (whereClauses.length > 1) {
+                        return { [Op.or]: whereClauses }
+                    }
+                    return null
                 }
             }
         }
