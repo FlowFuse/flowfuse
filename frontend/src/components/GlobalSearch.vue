dd548406d4c63f7b45eba88b592736bcf0451276
cstns <cstn.serban@gmail.com>
2024-11-19 16:05:18 +0200

    adding a global search api client and scaffolding for displaying results

--- frontend/src/components/GlobalSearch.vue
+++ frontend/src/components/GlobalSearch.vue
@@ -1,21 +1,48 @@
 <template>
     <div id="global-search" :class="{focused: isFocused}">
-        <div class="global-search-wrapper">
+        <transition name="fade" mode="out-in">
             <div v-if="isFocused" class="overlay" @click="deFocus" />
-            <div class="input-wrapper">
-                <SearchIcon class="ff-icon-sm search" />
+        </transition>
+        <div class="content-wrapper">
+            <SearchIcon class="ff-icon-sm search" />
 
-                <input
-                    v-model="search"
-                    type="text"
-                    placeholder="Search FlowFuse"
-                    @focusin="focus"
-                    @click="focus"
-                >
+            <input
+                v-model="query"
+                type="text"
+                placeholder="Search FlowFuse"
+                @focusin="focus"
+                @click="focus"
+            >
 
-                <transition name="fade" mode="out-in">
-                    <XIcon v-if="search.length" class="ff-icon-sm close cursor-pointer" @click="resetSearch" />
-                </transition>
+            <transition name="fade" mode="out-in">
+                <XIcon v-if="query.length" class="ff-icon-sm close cursor-pointer" @click="resetSearch" />
+            </transition>
+
+            <div class="results-wrapper">
+                <div v-if="resApplication.length > 0" class="section applications">
+                    <p class="title">Applications</p>
+                    <ul class="results">
+                        <li v-for="application in resApplication" :key="application.id">
+                            {{ application.name }}
+                        </li>
+                    </ul>
+                </div>
+                <div v-if="resDevices.length > 0" class="section devices">
+                    <p class="title">Devices</p>
+                    <ul class="results">
+                        <li v-for="device in resDevices" :key="device.id">
+                            {{ device.name }}
+                        </li>
+                    </ul>
+                </div>
+                <div v-if="resInstances.length > 0" class="section instances">
+                    <p class="title">Instances</p>
+                    <ul class="results">
+                        <li v-for="instance in resInstances" :key="instance.id">
+                            {{ instance.name }}
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -23,19 +50,48 @@
 
 <script>
 import { SearchIcon, XIcon } from '@heroicons/vue/outline'
+import { mapState } from 'vuex'
+
+import globalApi from '../api/global.js'
 
 export default {
     name: 'GlobalSearch',
-    components: { SearchIcon, XIcon },
+    components: {
+        SearchIcon,
+        XIcon
+    },
     data () {
         return {
-            search: '',
-            isFocused: false
+            query: '',
+            isFocused: false,
+            results: []
+        }
+    },
+    computed: {
+        ...mapState('account', ['team']),
+        resApplication () {
+            return this.results.filter(res => res.object === 'application')
+        },
+        resDevices () {
+            return this.results.filter(res => res.object === 'device')
+        },
+        resInstances () {
+            return this.results.filter(res => res.object === 'instance')
+        },
+        hasResults () {
+            return this.results.length > 0
+        }
+    },
+    watch: {
+        query (newVal) {
+            if (newVal) {
+                this.getData()
+            }
         }
     },
     methods: {
         resetSearch () {
-            this.search = ''
+            this.query = ''
         },
         onEscKeyPress () {
             this.deFocus()
@@ -53,6 +109,12 @@ export default {
             if (event.key === 'Escape') {
                 this.onEscKeyPress()
             }
+        },
+        async getData () {
+            return globalApi.search(this.team.id, this.query)
+                .then(res => {
+                    this.results = res.results
+                })
         }
     }
 }
@@ -65,72 +127,81 @@ export default {
     flex: 1;
     justify-content: flex-end;
 
-    .global-search-wrapper {
-        .input-wrapper {
-            padding: 5px 10px;
-            position: relative;
-            transition: ease-in-out width .3s;
-            display: flex;
-            justify-content: flex-end;
+    .content-wrapper {
+        padding: 5px 10px;
+        position: relative;
+        transition: ease-in-out width .3s;
+        justify-content: flex-end;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
 
-            .ff-icon-sm {
-                color: white;
-                position: absolute;
-                z-index: 1;
-                top: 10px;
+        .ff-icon-sm {
+            color: white;
+            position: absolute;
+            z-index: 1;
+            top: 10px;
 
-                &.search {
-                    left: 15px;
-                }
-
-                &.close {
-                    right: 15px;
-                }
+            &.search {
+                left: 15px;
             }
 
-            input {
-                color: $ff-grey-500;
-                padding: 3px 25px;
-                background: $ff-grey-700;
-                border-color: $ff-grey-500;
-                min-width: 25vw;
+            &.close {
+                right: 15px;
             }
+        }
+
+        input {
+            color: $ff-grey-500;
+            padding: 3px 25px;
+            background: $ff-grey-700;
+            border-color: $ff-grey-500;
+            min-width: 25vw;
+        }
+
+        .results-wrapper {
+            background: white;
+            display: none; // todo remove
+            min-height: 300px;
+            min-width: 100%;
+            z-index: 120;
         }
     }
 
     &.focused {
-        .global-search-wrapper {
-            .input-wrapper {
-                position: fixed;
-                width: 60vw;
-                background: white;
-                top: 10px;
-                left: 25%;
-                z-index: 120;
-                border: 1px solid  $ff-grey-500;
-                border-radius: 5px;
+        .content-wrapper {
+            position: fixed;
+            width: 60vw;
+            background: white;
+            top: 10px;
+            left: 25%;
+            z-index: 120;
+            border: 1px solid $ff-grey-500;
+            border-radius: 5px;
 
-                .ff-icon-sm {
-                    color: $ff-grey-500;
-                }
-
-                input {
-                    flex: 1;
-                    background: white;
-                }
+            .ff-icon-sm {
+                color: $ff-grey-500;
             }
 
-            .overlay {
-                position: absolute;
-                width: 100vw;
-                height: 100vh;
-                background: rgba(0,0,0,.3);
-                left: 0;
-                top: 0;
-                z-index: 110;
+            input {
+                flex: 1;
+                background: white;
+            }
+
+            .results-wrapper {
+                display: block; //todo remove
             }
         }
-    }
 
+        .overlay {
+            position: absolute;
+            width: 100vw;
+            height: 100vh;
+            background: rgba(0, 0, 0, .3);
+            left: 0;
+            top: 0;
+            z-index: 110;
+        }
+    }
 }
 </style>
