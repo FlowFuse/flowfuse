2f87828b22e7b2cc16b95b2b66284d680fcac290
cstns <cstn.serban@gmail.com>
2024-11-19 15:26:23 +0200

    merge fixes

--- frontend/src/pages/team/UNS/index.vue
+++ frontend/src/pages/team/UNS/index.vue
@@ -1,7 +1,7 @@
 <template>
     <ff-page>
         <template #header>
-            <ff-page-header title="Unified Namespace" :tabs="tabs">
+            <ff-page-header title="Broker" :tabs="tabs">
                 <template #context>
                     A list of the recent Topicss used by your team on the FlowFuse MQTT Broker.
                 </template>
@@ -9,9 +9,10 @@
                     <img alt="info" src="../../../images/pictograms/mqtt_broker_red.png">
                 </template>
                 <template #helptext>
-                    <p>The <b>Unified Namespace (UNS)</b> feature provides a centralized framework for managing and visualizing your entire data ecosystem, consolidating MQTT broker instances and topic structures within a single interface.</p>
-                    <p>Within the UNS, <b>Client</b> page enables seamless client management with customizable Access Control List (ACL) rules for secure and controlled data flow. The topic <b>Hierarchy</b> offers a clear, organized visualization of topic structures, providing fine-grained control over publishing and subscribing permissions.</p>
+                    <p>The <b>Broker</b> feature provides a centralized framework for managing and visualizing your entire data ecosystem, consolidating MQTT broker instances and topic structures within a single interface.</p>
+                    <p>Within the Broker, <b>Client</b> page enables seamless client management with customizable Access Control List (ACL) rules for secure and controlled data flow. The topic <b>Hierarchy</b> offers a clear, organized visualization of topic structures, providing fine-grained control over publishing and subscribing permissions.</p>
                     <p>Together, these components deliver an integrated approach to managing client connections, security settings, and message flow, supporting efficient and secure data communication across your system.</p>
+                    <p>Documentation <a href="https://flowfuse.com/docs/user/teambroker" target="_blank">here</a></p>
                 </template>
             </ff-page-header>
         </template>
