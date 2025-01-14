d9eb0d13b37bf8016af8e6ad9149fd4b53155036
cstns <cstn.serban@gmail.com>
2025-01-14 16:53:50 +0200

    touch up the team device group endpoint

--- forge/ee/routes/teamDeviceGroups/index.js
+++ forge/ee/routes/teamDeviceGroups/index.js
@@ -7,16 +7,6 @@
  * @memberof forge.routes.api
  */
 
-// const { ValidationError } = require('sequelize')
-//
-// const { UpdatesCollection } = require('../../../auditLog/formatters.js')
-// const { Roles } = require('../../../lib/roles.js')
-// const { DeviceGroupMembershipValidationError } = require('../../db/controllers/DeviceGroup.js')
-//
-// // Declare getLogger function to provide type hints / quick code nav / code completion
-// /** @type {import('../../../../forge/auditLog/application').getLoggers} */
-// const getApplicationLogger = (app) => { return app.auditLog.Application }
-
 /**
  * @param {import('../../../forge.js').ForgeApplication} app The application instance
  */
@@ -24,51 +14,43 @@ module.exports = async function (app) {
     // pre-handler for all routes in this file
     app.addHook('preHandler', async (request, reply) => {
         // todo validation preHandler
-        // // Get the application
-        // const applicationId = request.params.applicationId
-        // if (!applicationId) {
-        //     return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
-        // }
-        //
-        // try {
-        //     request.application = await app.db.models.Application.byId(applicationId)
-        //     if (!request.application) {
-        //         return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
-        //     }
-        //
-        //     if (request.session.User) {
-        //         request.teamMembership = await request.session.User.getTeamMembership(request.application.Team.id)
-        //         if (!request.teamMembership && !request.session.User.admin) {
-        //             return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
-        //         }
-        //     }
-        //
-        //     const teamType = await request.application.Team.getTeamType()
-        //     if (!teamType.getFeatureProperty('deviceGroups', false)) {
-        //         return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
-        //     }
-        // } catch (err) {
-        //     return reply.code(500).send({ code: 'unexpected_error', error: err.toString() })
-        // }
-        //
-        // // Get the device group
-        // const groupId = request.params.groupId
-        // if (groupId) {
-        //     request.deviceGroup = await app.db.models.DeviceGroup.byId(groupId)
-        //     if (!request.deviceGroup || request.deviceGroup.ApplicationId !== request.application.id) {
-        //         reply.code(404).send({ code: 'not_found', error: 'Not Found' })
-        //     }
-        // }
+
+        // Get the team
+        const teamId = request.params.teamId
+        if (!teamId) {
+            return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
+        }
+
+        try {
+            request.team = await app.db.models.Team.bySlug(request.params.teamId)
+            if (!request.team) {
+                return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
+            }
+
+            if (request.session.User) {
+                request.teamMembership = await request.session.User.getTeamMembership(request.params.teamId)
+                if (!request.teamMembership && !request.session.User.admin) {
+                    return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
+                }
+            }
+
+            const teamType = await request.team.getTeamType()
+            if (!teamType.getFeatureProperty('deviceGroups', false)) {
+                return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
+            }
+        } catch (err) {
+            return reply.code(500).send({ code: 'unexpected_error', error: err.toString() })
+        }
     })
 
     /**
      * Get a list of team device groups
      * @method GET
      * @name /api/v1/teams/:teamId/device-groups
-     * @memberof forge.routes.api.application
+     * @memberof forge.routes.api.team
      */
     app.get('/', {
-        preHandler: app.needsPermission('application:device-group:list'),
+        preHandler: app.needsPermission('team:device-group:list'),
         schema: {
             summary: 'Get a list of device groups in an application',
             tags: ['Application Device Groups'],
@@ -94,23 +76,21 @@ module.exports = async function (app) {
             }
         }
     }, async (request, reply) => {
-        reply.send({
-            meta: {},
-            count: 0,
-            groups: []
-        })
-        // const paginationOptions = app.db.controllers.Device.getDevicePaginationOptions(request)
-        //
-        // const where = {
-        //     ApplicationId: request.application.hashid
-        // }
-        //
-        // const groupData = await app.db.models.DeviceGroup.getAll(paginationOptions, where)
-        // const result = {
-        //     count: groupData.count,
-        //     meta: groupData.meta,
-        //     groups: (groupData.groups || []).map(d => app.db.views.DeviceGroup.deviceGroupSummary(d, { includeApplication: false }))
-        // }
-        // reply.send(result)
+        const paginationOptions = app.db.controllers.Device.getDevicePaginationOptions(request)
+
+        const applications = await app.db.models.Application.byTeam(request.team.id)
+
+        const where = {
+            ApplicationId: applications.map(app => app.dataValues.id)
+        }
+
+        const groupData = await app.db.models.DeviceGroup.getAll(paginationOptions, where, { includeApplication: true })
+        const result = {
+            count: groupData.count,
+            meta: groupData.meta,
+            groups: (groupData.groups || []).map(d => app.db.views.DeviceGroup.deviceGroupSummary(d, { includeApplication: true }))
+        }
+
+        reply.send(result)
     })
 }
