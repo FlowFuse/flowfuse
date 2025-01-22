a05a547170809fc2b110a2a4ed2e926a32aeda3e
Ben Hardill <ben@flowforge.com>
2025-01-22 09:52:54 +0000

    remove only

--- test/unit/forge/ee/routes/teamBroker/index_spec.js
+++ test/unit/forge/ee/routes/teamBroker/index_spec.js
@@ -6,7 +6,7 @@ const { Roles } = FF_UTIL.require('forge/lib/roles')
 
 const MAX_BROKER_USERS_PER_TEAM = 5
 
-describe.only('Team Broker API', function () {
+describe('Team Broker API', function () {
     describe('unlicensed', function () {
         let app
         it('client limit set to 0 for unlicensed', async function () {
@@ -751,8 +751,6 @@ describe.only('Team Broker API', function () {
                     url: `/api/v1/teams/${app.team.hashid}/broker/${brokerCredentialId}`,
                     cookies: { sid: TestObjects.tokens.alice }
                 })
-                console.error(response.statusCode)
-                console.error(response.body)
                 response.statusCode.should.equal(200)
                 response = await app.inject({
                     method: 'GET',
