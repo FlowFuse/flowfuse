e92352365e367dd6f86137cb37ea3db11a69563e
Ben Hardill <ben@flowforge.com>
2025-02-24 16:03:58 +0000

    fix lint

--- forge/ee/routes/catalogues/index.js
+++ forge/ee/routes/catalogues/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (app) {
-    app.get('/catalogue',{
-        config:{
+    app.get('/catalogue', {
+        config: {
             allowAnonymous: true
         }
     }, async (request, reply) => {
@@ -9,19 +9,18 @@ module.exports = async function (app) {
             reply.header('Access-Control-Allow-Origin', '*')
             reply.send({
                 name: `FlowFuse Team ${team.name} catalogue`,
-                updated_at: "2025-02-23T11:00:00.000Z",
+                updated_at: '2025-02-23T11:00:00.000Z',
                 modules: [
                     {
                         id: '@foobar/hello-world',
-                        version: "0.0.1",
-                        updated_at: "2025-02-23T11:00:00.000Z",
+                        version: '0.0.1',
+                        updated_at: '2025-02-23T11:00:00.000Z',
                         description: 'HelloWorld'
                     }
                 ]
             })
         } else {
-            reply.status(404).send({error: 'not_found', message: "not found"})
+            reply.status(404).send({ error: 'not_found', message: 'not found' })
         }
-        
     })
 }
