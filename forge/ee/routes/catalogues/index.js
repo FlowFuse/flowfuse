a8db3768d018078c17d7b5fc10c1d308f58f0fe8
Ben Hardill <ben@flowforge.com>
2025-02-26 10:46:37 +0000

    Add API to get teams npm packages
    part of #5190

--- forge/ee/routes/catalogues/index.js
+++ forge/ee/routes/catalogues/index.js
@@ -1,6 +1,39 @@
 const axios = require('axios')
 
 module.exports = async function (app) {
+
+    /**
+     * 
+     */
+    app.get('/npm/packages', {
+        preHandler: app.needsPermission('team:packages:read') 
+    }, async (request, reply) => {
+        try {
+            const packageList = await axios.get(`${app.config.npmRegistry?.url}/-/all`, {
+                // If we can swap this for a teams creds or token then the
+                // filtering will all be done in the npm repo
+                auth: {
+                    username: app.config.npmRegistry.admin.username,
+                    password: app.config.npmRegistry.admin.password
+                }
+            })
+
+            const packages = {}
+            for (const package in packageList.data) {
+                if (package == '_updated') {
+                    continue
+                }
+                if (package.startsWith(`@${request.teamId}/`)) {
+                    packages[package] = packageList.data[package]
+                }
+            }
+
+            reply.send(packages)
+        } catch (err) {
+            reply.status(500).send({ error: 'unkown_error', message: err.toString() })
+        }
+    })
+
     app.get('/catalogue', {
         config: {
             allowAnonymous: true
@@ -11,6 +44,8 @@ module.exports = async function (app) {
 
             try {
                 const packageList = await axios.get(`${app.config.npmRegistry?.url}/-/all`, {
+                    // If we can swap this for a teams creds or token then the
+                    // filtering will all be done in the npm repo
                     auth: {
                         username: app.config.npmRegistry.admin.username,
                         password: app.config.npmRegistry.admin.password
