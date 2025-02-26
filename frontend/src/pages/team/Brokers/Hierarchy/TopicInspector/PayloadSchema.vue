99baeb980405e87941f8bdf920f8a76f8bffc438
cstns <cstn.serban@gmail.com>
2025-02-26 15:42:05 +0200

    add broker suggestions section

--- frontend/src/pages/team/Brokers/Hierarchy/TopicInspector/PayloadSchema.vue
+++ frontend/src/pages/team/Brokers/Hierarchy/TopicInspector/PayloadSchema.vue
@@ -2,12 +2,27 @@
     <main-title title="Payload Schema" class="mt-1" />
 
     <div class="ff-topic-inspecting">
-        <sub-title title="Schema" :icon="CodeBracketSquareIcon" />
-        <topic-schema :schema="segment.inferredSchema" />
+        <section>
+            <sub-title title="Schema" :icon="CodeBracketSquareIcon" />
+            <topic-schema :schema="segment.inferredSchema" />
+        </section>
+
+        <section>
+            <sub-title title="Suggestions" :icon="LightBulbIcon" />
+            <div class="p-4 border border-gray-200 bg-gray-50 rounded-md">
+                <ul>
+                    <li class="p-4 border border-gray-200 bg-white rounded-md">
+                        el 1
+                    </li>
+                </ul>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
+import { LightBulbIcon } from '@heroicons/vue/outline'
+
 import CodeBracketSquareIcon from '../../../../../components/icons/CodeBracketSquare.js'
 import MainTitle from '../components/MainTitle.vue'
 import SubTitle from '../components/SubTitle.vue'
@@ -27,13 +42,16 @@ export default {
         }
     },
     setup () {
-        return { CodeBracketSquareIcon }
+        return { CodeBracketSquareIcon, LightBulbIcon }
     }
 }
 </script>
 
 <style scoped lang="scss">
 .ff-topic-inspecting {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
     background: $ff-white;
     padding: 10px;
     border-radius: 6px;
