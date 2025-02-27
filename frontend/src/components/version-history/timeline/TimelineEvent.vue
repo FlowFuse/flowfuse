9df00a31d8bd56bd61784dc44eb2d56677e004c9
cstns <cstn.serban@gmail.com>
2025-02-27 11:24:46 +0200

    update event title

--- frontend/src/components/version-history/timeline/TimelineEvent.vue
+++ frontend/src/components/version-history/timeline/TimelineEvent.vue
@@ -219,7 +219,7 @@ export default {
             case 'device.snapshot.deployed':
                 // eslint-disable-next-line vue/one-component-per-file
                 return defineComponent({
-                    template: `<span><i>${data.user.name}</i> manually deployed the <i>${data.snapshot.name}</i> snapshot</span>`
+                    template: `<span><i>${data.snapshot.name}</i> snapshot was manually deployed</span>`
                 })
             default:
                 // eslint-disable-next-line vue/one-component-per-file
