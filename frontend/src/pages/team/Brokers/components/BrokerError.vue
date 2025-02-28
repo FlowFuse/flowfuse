3ddc70b916cea188ccf360237e694b871ef3dc78
Joe Pavitt <joepavitt@flowforge.com>
2025-02-28 14:26:38 +0000

    Add relevant error messages

--- frontend/src/pages/team/Brokers/components/BrokerError.vue
+++ frontend/src/pages/team/Brokers/components/BrokerError.vue
@@ -16,8 +16,20 @@ export default {
     computed: {
         errorMessage () {
             switch (this.errorCode) {
+            case 'ECONNREFUSED':
+                return 'Your broker refused the connection, this is most likely either an incorrectly configured port or hostname.'
             case 'ENOTFOUND':
-                return 'The broker could not be found. Please check your connection settings and try again.'
+                return 'Your broker could not be found. Please check your connection settings and try again.'
+            case 'WRONGMQTTVERSION':
+                return 'Your broker does not support the configured MQTT protocol version. Please check your connection settings and try again.'
+            case 'CLIENTIDNOTALLOWED':
+                return 'Your broker has rejected the connection because the ClientID is either too long or contains invalid characters'
+            case 'MALFORMEDCREDS':
+                return 'Your broker has rejected the connection due to bad credentials. Please check your configured username/password.'
+            case 'NOTAUTHORIZED':
+                return 'Your broker has rejected the connection due to incorrect credentials'
+            case 'BANNED':
+                return 'Your MQTT 5 broker has banned this client connection.'
             default:
                 return ''
             }
