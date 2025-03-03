3517125bc06b55f1ead985e4ae78fc30a8a3c5d0
Costin Serban <cstn.serban@gmail.com>
2025-03-03 17:40:44 +0200

    Update frontend/src/pages/team/Brokers/Hierarchy/components/suggestions/TopicSuggestions.vue
    Co-authored-by: Joe Pavitt <99246719+joepavitt@users.noreply.github.com>

--- frontend/src/pages/team/Brokers/Hierarchy/components/suggestions/TopicSuggestions.vue
+++ frontend/src/pages/team/Brokers/Hierarchy/components/suggestions/TopicSuggestions.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="topic-suggestions p-2 border border-gray-200 bg-gray-50 rounded-md">
+    <div class="p-2 border border-gray-200 bg-gray-50 rounded-md">
         <ul>
             <slot />
         </ul>
