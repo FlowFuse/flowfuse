f28a4a9c971aeb8c5ffff996e0da67eaa2639a53
Joe Pavitt <joepavitt@flowforge.com>
2025-03-05 16:35:18 +0000

    Add framing for the "Publish" dialog

--- frontend/src/pages/team/Library/Registry/Index.vue
+++ frontend/src/pages/team/Library/Registry/Index.vue
@@ -1,19 +1,54 @@
 <template>
     <SectionTopMenu hero="Custom Node Catalog" info="Your Team's private Node catalog. Here you can publish private npm repositories for your team to use within your Node-RED Instances.">
         <template #tools>
-            <ff-button @click="publish">Publish</ff-button>
+            <ff-button v-if="canPublish" @click="publish">
+                <template #icon-left>
+                    <ArrowCircleUpIcon />
+                </template>
+                Publish
+            </ff-button>
         </template>
     </SectionTopMenu>
-    {{ registry }}
+    <div>
+        <EmptyState v-if="!registry.length" data-el="team-no-devices">
+            <template #img>
+                <img src="../../../../images/empty-states/team-library.png" alt="placeholder-image">
+            </template>
+            <template #header>Publish Your First Custom Nodes</template>
+            <template #message>
+                <p>
+                    Details about publishing your first Node package
+                </p>
+            </template>
+            <template #actions>
+                <ff-button
+                    v-if="canPublish"
+                    kind="primary"
+                    data-action="publish-package"
+                    @click="publish"
+                >
+                    <template #icon-left>
+                        <ArrowCircleUpIcon />
+                    </template>
+                    Publish
+                </ff-button>
+            </template>
+        </EmptyState>
+        <div v-else>
+            List Registry
+        </div>
+    </div>
 
     <PublishNodeDialog ref="publishNodeDialog" />
 </template>
 
 <script>
+import { ArrowCircleUpIcon } from '@heroicons/vue/outline'
 import { mapState } from 'vuex'
 
 import TeamAPI from '../../../../api/team.js'
 
+import EmptyState from '../../../../components/EmptyState.vue'
 import SectionTopMenu from '../../../../components/SectionTopMenu.vue'
 
 import PublishNodeDialog from './dialogs/PublishNode.vue'
@@ -21,16 +56,25 @@ import PublishNodeDialog from './dialogs/PublishNode.vue'
 export default {
     name: 'NodeRegistry',
     components: {
+        ArrowCircleUpIcon,
+        EmptyState,
         SectionTopMenu,
         PublishNodeDialog
     },
     data () {
         return {
-            registry: []
+            registryByPackage: []
         }
     },
     computed: {
-        ...mapState('account', ['team'])
+        ...mapState('account', ['settings', 'team']),
+        registry () {
+            return Object.values(this.registryByPackage)
+        },
+        canPublish () {
+            // return this.hasPermission('team:publish')
+            return true
+        }
     },
     mounted () {
         this.loadRegistry()
@@ -38,7 +82,7 @@ export default {
     methods: {
         async loadRegistry () {
             const registry = await TeamAPI.getTeamRegistry(this.team.id)
-            this.registry = registry
+            this.registryByPackage = registry.data
         },
         publish () {
             this.$refs.publishNodeDialog.show()
--- frontend/src/pages/team/Library/Registry/dialogs/PublishNode.vue
+++ frontend/src/pages/team/Library/Registry/dialogs/PublishNode.vue
@@ -5,7 +5,25 @@
         @confirm="confirm()"
     >
         <template #default>
-            Hello World
+            <div>
+                <p>
+                    These commands should be run wherever you store your codebase for your custom node package.
+                </p>
+                <p class="mt-2">
+                    Publishing to this registry, will make your package available to all of your Node-RED instances in FlowFuse.
+                </p>
+                <label class="block font-bold mt-4 mb-2">Login to Registry</label>
+                <CodeSnippet>{{ commands.login }}</CodeSnippet>
+                <CopySnippet :snippet="commands.login" />
+                <label class="block font-bold mt-2 mb-2">Credentials</label>
+                <p class="mb-3">
+                    You will be prompted to insert a <b>username</b> and <b>password</b>.
+                    You can re-use the credentials you have previously used, or regenerate credentials now.
+                </p>
+                <label class="block font-bold mt-2 mb-2">Publish Package</label>
+                <CodeSnippet>{{ commands.publish }}</CodeSnippet>
+                <CopySnippet :snippet="commands.publish" />
+            </div>
             <!-- <label class="block font-bold mb-2">Install Device Agent</label>
             <p>Run this command on the hardware where you want your Remote Instance to run:</p>
             <pre class="overflow-auto text-xs font-light p-4 my-2 border rounded bg-gray-800 text-gray-200">npm install -g @flowfuse/device-agent</pre>
@@ -43,10 +61,17 @@
 <script>
 import { mapState } from 'vuex'
 
+import CodeSnippet from '../../../../../components/CodeSnippet.vue'
+import CopySnippet from '../../../../../components/CopySnippet.vue'
+
 // import TeamAPI from '../../../../api/team.js'
 
 export default {
     name: 'PublishNodeDialog',
+    components: {
+        CopySnippet,
+        CodeSnippet
+    },
     setup () {
         return {
             show () {
@@ -59,7 +84,16 @@ export default {
         }
     },
     computed: {
-        ...mapState('account', ['team'])
+        ...mapState('account', ['settings', 'team']),
+        registryHost () {
+            return this.settings ? this.settings['team:npm:registry'] : ''
+        },
+        commands () {
+            return {
+                login: `npm login --registry ${this.registryHost}`,
+                publish: 'npm publish'
+            }
+        }
     },
     async mounted () {
 
