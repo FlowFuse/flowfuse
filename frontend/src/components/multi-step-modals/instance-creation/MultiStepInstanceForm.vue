ca5069974d4012353ef1e886a436b0881ba9a34d
cstns <cstn.serban@gmail.com>
2025-03-17 18:10:02 +0200

    add an instance multi-step form

--- frontend/src/components/multi-step-modals/instance-creation/MultiStepInstanceForm.vue
+++ frontend/src/components/multi-step-modals/instance-creation/MultiStepInstanceForm.vue
@@ -1,10 +1,57 @@
 <template>
-    <h1>Hello Light</h1>
+    <MultiStepForm :steps="formSteps" :starting-step="1" @step-updated="updateForm" />
 </template>
 
 <script>
+import MultiStepForm from '../MultiStepForm.vue'
+
+import BlueprintStep from './steps/BlueprintStep.vue'
+import InstanceStep from './steps/InstanceStep.vue'
+
+const INSTANCE_SLUG = 'instance'
+const BLUEPRINT_SLUG = 'instance'
+
 export default {
-    name: 'MultiStepInstanceForm'
+    name: 'MultiStepInstanceForm',
+    components: { MultiStepForm },
+    data () {
+        return {
+            form: {
+                [INSTANCE_SLUG]: { },
+                [BLUEPRINT_SLUG]: { }
+            }
+        }
+    },
+    computed: {
+        formSteps () {
+            return [
+                {
+                    sliderTitle: 'Application',
+                    disabled: true
+                },
+                {
+                    sliderTitle: 'Instance',
+                    bindings: {
+                        slug: INSTANCE_SLUG,
+                        state: this.form[INSTANCE_SLUG].input
+                    },
+                    component: InstanceStep
+                },
+                {
+                    sliderTitle: 'Blueprint',
+                    bindings: {
+                        slug: BLUEPRINT_SLUG
+                    },
+                    component: BlueprintStep
+                }
+            ]
+        }
+    },
+    methods: {
+        updateForm (payload) {
+            this.form = { ...this.form, ...payload }
+        }
+    }
 }
 </script>
 
