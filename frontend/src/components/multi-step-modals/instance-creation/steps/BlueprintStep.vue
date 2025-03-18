a1b1bfad0ff1766ad5730f04c43c02b58791d599
cstns <cstn.serban@gmail.com>
2025-03-18 16:15:36 +0200

    add the blueprint step and modify the blueprint tile to match the new format

--- frontend/src/components/multi-step-modals/instance-creation/steps/BlueprintStep.vue
+++ frontend/src/components/multi-step-modals/instance-creation/steps/BlueprintStep.vue
@@ -1,13 +1,153 @@
 <template>
-    <h1>Blueprint step here</h1>
+    <section class="blueprint-step text-center flex flex-col gap-7">
+        <h2>Select Your Blueprint</h2>
+
+        <p>We have a collection of pre-build flow templates that you can use as a starting point for your Node-RED Instance.</p>
+
+        <div class="flex gap-16 text-left flex-wrap-reverse">
+            <div class="blueprints flex-1">
+                <ul class="categories flex flex-col gap-8">
+                    <li v-for="(category, $categoryName) in categories" :key="$categoryName" :ref="$categoryName">
+                        <h3>{{ $categoryName }}</h3>
+                        <hr class="my-3">
+                        <ul class="tiles flex gap-5 flex-wrap">
+                            <li v-for="(blueprint, $key) in category" :key="$key" class="tile">
+                                <BlueprintTile
+                                    :blueprint="blueprint"
+                                    :display-preview-button="false"
+                                    :display-external-url-button="false"
+                                    :display-select-button="false"
+                                    :tile-behavior="true"
+                                    :active="selectedBlueprint && selectedBlueprint.id === blueprint.id"
+                                    :alt-preview-button="true"
+                                    :class="{'no-icon': !blueprint.icon}"
+                                    default-icon="plus"
+                                    @selected="onTileSelect"
+                                />
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="categories">
+                <h3>Categories</h3>
+                <hr class="my-3">
+                <ul>
+                    <li
+                        v-for="(category, $categoryName) in categories"
+                        :key="$categoryName"
+                        class="mb-3 flex gap-3 font-bold"
+                    >
+                        <div class="flex-1 flex gap-2 cursor-pointer" @click="onCategoryClick($categoryName)">
+                            <span>
+                                {{ $categoryName }}
+                            </span>
+                        </div>
+                        <div>{{ category.length }}</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
+import { PlusIcon } from '@heroicons/vue/solid'
+import { mapState } from 'vuex'
+
+import flowBlueprintsApi from '../../../../api/flowBlueprints.js'
+import { scrollIntoView } from '../../../../composables/Ux.js'
+import BlueprintTile from '../../../blueprints/BlueprintTile.vue'
+
 export default {
-    name: 'BlueprintStep'
+    name: 'BlueprintStep',
+    components: { BlueprintTile },
+    setup () {
+        return { PlusIcon }
+    },
+    data () {
+        return {
+            blueprints: [],
+            selectedBlueprint: null
+        }
+    },
+    computed: {
+        ...mapState('account', ['team']),
+        categories () {
+            return [...this.blueprints].sort((a, b) => {
+                return a.order - b.order
+            }).reduce((acc, blueprint) => {
+                const category = blueprint.category || 'Other';
+                (acc[category] = acc[category] || []).push(blueprint)
+                return acc
+            }, {})
+        }
+    },
+    async mounted () {
+        await this.getBlueprints()
+    },
+    methods: {
+        async getBlueprints () {
+            const response = await flowBlueprintsApi.getFlowBlueprintsForTeam(this.team.id)
+            this.blueprints = response.blueprints
+        },
+        onCategoryClick (category) {
+            const el = this.$refs[category]
+
+            if (el.length > 0) {
+                scrollIntoView(el[0])
+            }
+        },
+        onTileSelect (blueprint) {
+            if (this.selectedBlueprint && this.selectedBlueprint.id === blueprint.id) {
+                this.selectedBlueprint = null
+            } else {
+                this.selectedBlueprint = blueprint
+            }
+        }
+    }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+.blueprint-step {
 
+    .blueprints {
+        overflow: auto;
+        min-width: 400px;
+        max-height: 65vh;
+        padding-right: 15px;
+
+        .tiles {
+            .tile {
+                .ff-blueprint-tile {
+                    width: 280px;
+
+                    .ff-blueprint-tile--header {
+                        height: 115px;
+
+                        .ff-icon {
+                            transform: scale(8);
+                            position: absolute;
+                            top: 70px;
+                        }
+                    }
+
+                    &.no-icon {
+                        .ff-blueprint-tile--header {
+                            .ff-icon:not(.alt-preview) {
+                                transform: scale(4);
+                                position: initial;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    .categories {
+        min-width: 300px;
+    }
+}
 </style>
