0418f850d7c80268c3bdf3acc4680f66a12ff5ae
cstns <cstn.serban@gmail.com>
2025-04-17 13:43:36 +0300

    Alter the kebab menu and fix clippings

--- frontend/src/ui-components/components/KebabMenu.vue
+++ frontend/src/ui-components/components/KebabMenu.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="ff-kebab-menu" :class="{'active': open}" data-el="kebab-menu">
-        <!-- using this v-if hack in order to enable both
-        the button and click-outside to work when closing the menu -->
+    <div ref="trigger" class="ff-kebab-menu" :class="{'active': open}" data-el="kebab-menu">
         <DotsVerticalIcon v-if="!open" @click.stop="openOptions" />
         <DotsVerticalIcon v-if="open" @click.stop="closeOptions" />
-        <template v-if="open">
-            <ul :ref="menuOpening" v-click-outside="closeOptions" class="ff-kebab-options"
-                :class="'ff-kebab-options--' + menuAlign"
+        <teleport to="body">
+            <ul v-if="open && visible"
+                ref="menu"
+                v-click-outside="closeOptions"
+                class="ff-kebab-options"
+                :style="{
+                    position: 'fixed',
+                    top: position.top + 'px',
+                    left: position.left + 'px'
+                }"
             >
                 <slot></slot>
             </ul>
-        </template>
+        </teleport>
     </div>
 </template>
 
 <script>
-
 import { DotsVerticalIcon } from '@heroicons/vue/solid'
 
 export default {
@@ -23,65 +27,65 @@ export default {
     components: {
         DotsVerticalIcon
     },
-    props: {
-        // eslint-disable-next-line vue/prop-name-casing
-        'menu-align': {
-            type: String,
-            default: 'right'
-        }
-    },
     data () {
         return {
             open: false,
-            openEvent: null,
-            viewport: {
-                width: window.innerWidth,
-                height: window.innerHeight
-            },
-            mouse: {
-                x: 0,
-                y: 0
+            visible: true,
+            position: {
+                top: 0,
+                left: 0
             }
         }
     },
+    mounted () {
+        window.addEventListener('resize', this.updatePosition)
+        window.addEventListener('scroll', this.updatePosition, true)
+    },
+    beforeUnmount () {
+        window.removeEventListener('resize', this.updatePosition)
+        window.removeEventListener('scroll', this.updatePosition, true)
+    },
     methods: {
-        openOptions (event) {
-            // update the viewport variable - BEFORE opening the menu
-            this.viewport.width = window.innerWidth
-            this.viewport.height = window.innerHeight
-
-            // update the mouse position - BEFORE opening the menu
-            this.mouse.x = event.clientX
-            this.mouse.y = event.clientY
-
-            // open the menu - this will cause the menu to render and the ref to be come available
-            // which will in-turn trigger the menuOpening method
-            this.open = !this.open
+        openOptions () {
+            this.open = true
+            this.$nextTick(this.updatePosition)
         },
         closeOptions () {
-            this.openEvent = null
             this.open = false
         },
-        menuOpening (el) {
-            // get the height of the menu el
-            const menu = {
-                width: el.offsetWidth,
-                height: el.offsetHeight
+        updatePosition () {
+            if (!this.$refs.trigger || !this.$refs.menu) return
+            const rect = this.$refs.trigger.getBoundingClientRect()
+            const menu = this.$refs.menu
+            const { width, height } = menu.getBoundingClientRect()
+
+            const vw = window.innerWidth
+            const vh = window.innerHeight
+
+            // Default positions
+            let top = rect.bottom + window.scrollY
+            let left = rect.left + window.scrollX
+
+            // Try flipping vertically
+            if (top + height > window.scrollY + vh && rect.top - height >= 0) {
+                top = rect.top + window.scrollY - height
             }
 
-            // check if the menu would be out of the viewport
-            const left = this.mouse.x + menu.width + 20 > this.viewport.width // + 20 for padding
-            const top = this.mouse.y + menu.height + 20 > this.viewport.height // + 20 for padding
-
-            // if the menu would be out of the viewport, nudge it in a negative direction
-            // (menu is positioned absolute, so we only need to change the top/left values as a negative offset)
-            if (left && this.menuAlign !== 'right') {
-                el.style.left = '-' + menu.width + 'px'
+            // Try flipping horizontally
+            if (left + width > window.scrollX + vw && rect.right - width >= 0) {
+                left = rect.right + window.scrollX - width
             }
 
-            if (top) {
-                el.style.top = '-' + menu.height + 'px'
-            }
+            this.position = { top, left }
+
+            // Determine visibility of trigger
+            const triggerFullyVisible = (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= vh &&
+                rect.right <= vw
+            )
+            this.visible = triggerFullyVisible
         }
     }
 }
