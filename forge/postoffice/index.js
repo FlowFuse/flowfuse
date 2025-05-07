da350f5c389fbe353eb470d71c631ae325bd9716
Steve-Mcl <sdmclaughlin@gmail.com>
2025-05-07 14:56:19 +0100

    Refactor email sanitization to utils module for later reuse

--- forge/postoffice/index.js
+++ forge/postoffice/index.js
@@ -3,6 +3,7 @@ const handlebars = require('handlebars')
 const nodemailer = require('nodemailer')
 
 const defaultLayout = require('./layouts/default.js')
+const { sanitizeText } = require('./utils.js')
 
 const templates = {}
 
@@ -128,18 +129,6 @@ module.exports = fp(async function (app, _opts) {
         }
     }
 
-    /**
-     * Generates email-safe versions (both text and html) of a piece of text.
-     * This is intended to make user-provided strings (eg username) that may look
-     * like a URL to not looks like a URL to an email client
-     * @param {String} value
-     */
-    function sanitizeText (value) {
-        return {
-            text: value.replace(/\./g, ' '),
-            html: value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\./g, '<br style="display: none;"/>.')
-        }
-    }
     /**
      * Generates email-safe versions (both text and html) of a log array
      * This is intended to make iso time strings and and sanitized log messages
