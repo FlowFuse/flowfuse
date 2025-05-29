6e3a8be5139826e1202bc390b95a31952109d7e4
Nick O'Leary <nick.oleary@gmail.com>
2025-05-29 13:41:15 +0100

    Add pull options to git repo model

--- forge/ee/db/models/PipelineStageGitRepo.js
+++ forge/ee/db/models/PipelineStageGitRepo.js
@@ -7,22 +7,55 @@ module.exports = {
             type: DataTypes.STRING,
             allowNull: false
         },
+        // The branch to push updates to
         branch: {
             type: DataTypes.STRING,
             allowNull: false,
             default: 'main'
         },
+        // The branch to pull updates from. Defaults to '', meaning
+        // use the same as 'branch'.
+        pullBranch: {
+            type: DataTypes.STRING,
+            default: ''
+        },
+        // The path to push to in the repo. If blank, derives the name
+        // from the source of the snapshot.
+        pushPath: {
+            type: DataTypes.STRING,
+            default: ''
+        },
+        // The path that was actually pushed to the last time the stage ran.
+        // Allows the pull operation to retrieve the file if it was derived
+        lastPushPath: {
+            type: DataTypes.STRING,
+            default: ''
+        },
+        // When the last push ran
         lastPushAt: {
             type: DataTypes.DATE
         },
+        // The path to pull from. If blank, uses 'lastPushPath'. If that is also
+        // blank, it will fail.
+        pullPath: {
+            type: DataTypes.STRING,
+            default: ''
+        },
+        // When the last pull ran
+        lastPullAt: {
+            type: DataTypes.DATE
+        },
+        // The last status of the stage
         status: {
             type: DataTypes.STRING,
             default: ''
         },
+        // Any message associated with the last status
         statusMessage: {
             type: DataTypes.STRING,
             default: ''
         },
+        // The credential secret to use for export/import of the flows
         credentialSecret: {
             type: DataTypes.STRING,
             default: ''
@@ -60,9 +93,12 @@ module.exports = {
                     setImmediate(async () => {
                         try {
                             const gitToken = await this.getGitToken()
-                            let targetFilename = 'snapshot.json'
+                            let targetFilename = this.pushPath
                             if (options.sourceObject?.name) {
                                 targetFilename = 'snapshot-' + options.sourceObject.name + '.json'
+                            }
+                            if (!targetFilename) {
+                                targetFilename = 'snapshot.json'
                             }
                             await app.gitops.pushToRepository({
                                 token: gitToken.token,
@@ -72,6 +108,7 @@ module.exports = {
                                 path: targetFilename
                             }, snapshot, options)
 
+                            this.lastPushPath = targetFilename
                             this.status = 'success'
                             this.statusMessage = ''
                             await this.save()
