c1ccbd7e5e37da69b7b1369b37a36c595a5ed13f
cstns <cstn.serban@gmail.com>
2025-06-19 14:23:50 +0300

    removed pointer class from glance stats

--- frontend/src/pages/team/Home/components/AtAGlanceInstanceStats.vue
+++ frontend/src/pages/team/Home/components/AtAGlanceInstanceStats.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex gap-2">
-        <div class="rounded-md flex-1 p-3 bg-green-50 text-green-600 cursor-pointer" @click="clicked('running')">
+        <div class="rounded-md flex-1 p-3 bg-green-50 text-green-600" @click="clicked('running')">
             <p class="title">Running</p>
             <span class="counter font-extrabold text-4xl">{{ stats.running ?? 0 }}</span>
         </div>
-        <div class="rounded-md flex-1 p-3 bg-red-50 text-red-500 cursor-pointer" @click="clicked('error')">
+        <div class="rounded-md flex-1 p-3 bg-red-50 text-red-500" @click="clicked('error')">
             <p class="title">Error</p>
             <span class="counter font-extrabold text-4xl">{{ stats.error ?? 0 }}</span>
         </div>
-        <div class="rounded-md flex-1 p-3 bg-gray-50 text-gray-500 cursor-pointer" @click="clicked('not-running')">
+        <div class="rounded-md flex-1 p-3 bg-gray-50 text-gray-500" @click="clicked('not-running')">
             <p class="title">Not Running</p>
             <span class="counter font-extrabold text-4xl">{{ stats.error ?? 0 }}</span>
         </div>
