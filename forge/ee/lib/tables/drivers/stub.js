0c6af6de2ed7019e544ef9a046228ed6f21d93a0
Ben Hardill <ben@flowforge.com>
2025-07-16 11:50:47 +0100

    Only add routes if enabled
    Also fix stub driver

--- forge/ee/lib/tables/drivers/stub.js
+++ forge/ee/lib/tables/drivers/stub.js
@@ -7,18 +7,23 @@ module.exports = {
     },
     shutdown: async function (app) {},
     getDatabases: async function (team) {
-        if (!databases[team.hashid]) {
-            throw new Error("Database not found")
+        const tables = await this._app.db.models.Table.byTeamId(team.id)
+        if (tables && tables.length > 0) {
+            return tables
+        } else {
+            throw new Error(`Database for team ${team.hashid} does not exist`)
         }
-        return [databases[team.hashid]]
     },
     getDatabase: async function (team, databaseId) {
-        if (!databases[team.hashid]) {
-            throw new Error("Database not found")
+        const table = await this._app.db.models.Table.byId(teamId, databaseId)
+        if (table) {
+            return table
+        } else {
+            throw new Error(`Database ${databaseId} for team ${teamId} does not exist`)
         }
-        return databases[team.hashid]
     },
     createDatabase: async function (team, name) {
+        console.log("Creating database for team", team.hashid, "with name", name)
         if (databases[team.hashid]) {
             throw new Error("Database already exists")
         }
@@ -32,14 +37,18 @@ module.exports = {
                 user: "postgres",
                 password: generatePassword(16),
                 ssl: false
-            }
+            },
+            meta: {}
         };
-        return databases[team.hashid]
+        const table = await this._app.db.models.Table.create(databases[team.hashid] )
+        return table
     },
     destroyDatabase: async function (team, databaseId) {
-        if (!databases[team.hashid]) {
-            throw new Error("Database not found")
+        const db = await this._app.db.models.Table.byId(team.id, databaseId)
+        if (!db) {
+            throw new Error(`Database ${databaseId} for team ${team.hashid} does not exist`)
         }
+        await db.destroy()
         delete databases[team.hashid]
     },
     getTables: async function (team, databaseId) {
