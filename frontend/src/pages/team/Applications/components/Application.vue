4a2782adedc60d66184131a072bfd08e185d1903
cstns <cstn.serban@gmail.com>
2025-07-23 11:16:11 +0300

    remove clutter

--- frontend/src/pages/team/Applications/components/Application.vue
+++ frontend/src/pages/team/Applications/components/Application.vue
@@ -6,8 +6,6 @@
 
         <DevicesWrapper :application="localApplication" :search-query="searchQuery" @delete-device="$emit('device-deleted')" />
 
-        <ConfirmInstanceDeleteDialog ref="confirmInstanceDeleteDialog" @confirm="onInstanceDeleted" />
-
         <transition name="fade">
             <div v-if="isLoading" class="overlay flex">
                 <ff-spinner v-if="isLoading" class="flex-1 self-center" />
@@ -17,8 +15,6 @@
 </template>
 
 <script>
-import ConfirmInstanceDeleteDialog from '../../../instance/Settings/dialogs/ConfirmInstanceDeleteDialog.vue'
-
 import ApplicationHeader from './ApplicationHeader.vue'
 
 import DevicesWrapper from './compact/DevicesWrapper.vue'
@@ -27,7 +23,6 @@ import InstancesWrapper from './compact/InstancesWrapper.vue'
 export default {
     name: 'ApplicationListItem',
     components: {
-        ConfirmInstanceDeleteDialog,
         ApplicationHeader,
         InstancesWrapper,
         DevicesWrapper
@@ -68,16 +63,6 @@ export default {
         this.setLocalApplication()
     },
     methods: {
-        onInstanceDeleted (instance) {
-            if (this.localApplication.instances.find((el) => el.id === instance.id)) {
-                this.localApplication.instances = this.localApplication.instances.filter((el) => el.id !== instance.id)
-                this.localApplication.instanceCount--
-                this.$emit('instance-deleted')
-            }
-        },
-        onInstanceDelete (instance) {
-            this.$refs.confirmInstanceDeleteDialog.show(instance)
-        },
         setLocalApplication () {
             this.localApplication = this.application
         }
