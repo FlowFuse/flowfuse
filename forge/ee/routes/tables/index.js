751dc0e961770db6e3c704e6ab46431809abfc1d
Ben Hardill <ben@flowforge.com>
2025-07-28 14:36:57 +0100

    Log why DB create failed

--- forge/ee/routes/tables/index.js
+++ forge/ee/routes/tables/index.js
@@ -91,6 +91,7 @@ module.exports = async function (app) {
                 return reply.status(409).send({ code: 'already_exists', error: 'Database already exists' })
             } else {
                 // console.log(err)
+                app.log.error(`Failed to create database\n${JSON.stringify(response, null, 2)}`)
                 reply.status(500).send({ code: 'unexpected_error', error: 'Failed to create database' })
             }
         }
