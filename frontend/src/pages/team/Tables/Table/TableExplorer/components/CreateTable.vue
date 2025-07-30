66551808ae4e908c7c71df09697e70e16695c925
cstns <cstn.serban@gmail.com>
2025-07-30 14:52:57 +0300

    improve the table validation

--- frontend/src/pages/team/Tables/Table/TableExplorer/components/CreateTable.vue
+++ frontend/src/pages/team/Tables/Table/TableExplorer/components/CreateTable.vue
@@ -6,24 +6,43 @@
             </div>
         </div>
         <div class="content-wrapper">
-            <div class="header grid grid-cols-12 gap-1 mb-1">
-                <span class="col-span-3 title">Name</span>
-                <span class="col-span-2 title">Type</span>
-                <span class="col-span-2 title">Default</span>
-                <span class="col-span-2 title">Options</span>
-                <span class="col-span-1 title -ml-1">Nullable</span>
-                <span class="col-span-1 title -ml-2">Unsigned</span>
+            <div class="section table-name">
+                <h3>Define name</h3>
+                <ff-text-input
+                    v-model="tableName"
+                    placeholder="Your table's new name"
+                    type="string"
+                    :error="errors.name"
+                    @change="validateForm"
+                />
+                <div v-if="errors.name" data-el="form-row-error" class="ml-4 text-red-400 text-xs">
+                    {{ errors.name }}
+                </div>
             </div>
-            <ul class="columns">
-                <li v-for="(column, $key) in columns" :key="$key">
-                    <table-column :column="column" @remove="removeColumn($key)" />
-                </li>
-            </ul>
-            <ff-button type="button" kind="secondary" class="w-full" @click="onNewColumn">Add a new column</ff-button>
+            <div class="section table-columns">
+                <h3>Define Columns</h3>
+                <div class="header grid grid-cols-12 gap-1 mb-1">
+                    <span class="col-span-3 title">Name</span>
+                    <span class="col-span-2 title">Type</span>
+                    <span class="col-span-2 title">Default</span>
+                    <span class="col-span-2 title">Options</span>
+                    <span class="col-span-1 title -ml-1">Nullable</span>
+                    <span class="col-span-1 title -ml-2">Unsigned</span>
+                </div>
+                <ul class="columns">
+                    <li v-for="(column, $key) in columns" :key="$key">
+                        <table-column :column="column" @remove="removeColumn($key)" />
+                    </li>
+                </ul>
+                <div v-if="errors.columns" data-el="form-row-error" class="ml-4 text-red-400 text-xs text-center p-5">
+                    {{ errors.columns }}
+                </div>
+                <ff-button type="button" kind="secondary" class="w-full" @click="onNewColumn">Add a new column</ff-button>
+            </div>
         </div>
         <div class="footer flex gap-3">
             <ff-button type="button" kind="secondary" class="w-full" @click="closeRightDrawer">Cancel</ff-button>
-            <ff-button type="button" kind="primary" class="w-full" :disabled="!isFormValid">Save</ff-button>
+            <ff-button type="button" kind="primary" class="w-full" :disabled="!areColumnsValid || hasErrors">Save</ff-button>
         </div>
     </div>
 </template>
@@ -47,15 +66,27 @@ export default defineComponent({
     components: { TableColumn },
     data () {
         return {
-            columns: [{ ...emptyColumn }]
+            columns: [{ ...emptyColumn }],
+            tableName: '',
+            errors: { }
         }
     },
     computed: {
-        isFormValid () {
+        areColumnsValid () {
             return !this.columns.map(col => {
                 if (col._errors_ === true) return true
                 return !col.name || !col.type
             }).includes(true)
+        },
+
+        hasErrors () {
+            return Object.keys(this.errors).length > 0
+        }
+    },
+    watch: {
+        columns: {
+            deep: true,
+            handler: 'validateForm'
         }
     },
     methods: {
@@ -65,6 +96,31 @@ export default defineComponent({
         },
         removeColumn (key) {
             this.columns.splice(key, 1)
+        },
+        validateForm () {
+            // PostgreSQL identifiers:
+            // - max 63 bytes (can be less than 63 characters if multibyte)
+            // - must begin with a letter or underscore
+            // - can contain letters, digits, and underscores
+            switch (true) {
+            case typeof this.tableName !== 'string':
+                this.errors.name = 'The table name must be a string.'
+                break
+            case this.tableName.length === 0:
+                this.errors.name = 'A table name is mandatory.'
+                break
+            case this.tableName.length > 63:
+                this.errors.name = 'The table name must not exceed 63 characters.'
+                break
+            case !/^[a-zA-Z_][a-zA-Z0-9_]*$/.test(this.tableName):
+                this.errors.name = 'The table name must have a valid format.'
+                break
+            case this.columns.length === 0:
+                this.errors.columns = 'The table must have at least one column.'
+                break
+            default:
+                this.errors = { }
+            }
         }
     }
 })
@@ -106,16 +162,22 @@ export default defineComponent({
         overflow: auto;
         padding: 12px;
 
-        .header {
-            .title {
-                color: $ff-grey-600;
-                font-size: 10px;
-            }
-        }
+       .section {
+           padding-bottom: 15px;
+           margin-bottom: 15px;
+           border-bottom: 1px solid $ff-grey-200;
 
-        .columns {
-            margin-bottom: 20px;
-        }
+           .header {
+               .title {
+                   color: $ff-grey-600;
+                   font-size: 10px;
+               }
+           }
+
+           .columns {
+               margin-bottom: 20px;
+           }
+       }
     }
 
     .footer {
