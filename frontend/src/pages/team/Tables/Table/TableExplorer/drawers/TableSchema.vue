76584e901a329685c4b4b733b94d4d310777a2e1
Joe Pavitt <joepavitt@flowforge.com>
2025-07-30 19:14:49 +0100

    Shift delete/cancel buttons on the "Edit Schema" drawer

--- frontend/src/pages/team/Tables/Table/TableExplorer/drawers/TableSchema.vue
+++ frontend/src/pages/team/Tables/Table/TableExplorer/drawers/TableSchema.vue
@@ -3,40 +3,40 @@
         <div class="header">
             <div class="flex content">
                 <h2 class="title flex-grow"><i>{{ table.name }}</i> schema</h2>
+                <div class="flex gap-2">
+                    <ff-button type="button" kind="secondary" class="w-full" @click="closeRightDrawer">Close</ff-button>
+                    <ff-button
+                        type="button"
+                        kind="danger"
+                        class="w-full"
+                        @click="submit"
+                    >
+                        Delete
+                    </ff-button>
+                </div>
             </div>
         </div>
-        <div class="content-wrapper">
-            <h3>Columns</h3>
-            <div class="header grid grid-cols-10 gap-1 mb-1">
-                <span class="col-span-2 title">Name</span>
-                <span class="col-span-2 title">Type</span>
-                <span class="col-span-2 title">Nullable</span>
-                <span class="col-span-2 title">Default</span>
-                <span class="col-span-2 title">Generated</span>
-            </div>
-            <ul>
-                <li v-for="(col, $key) in table.schema" :key="$key">
-                    <div class="grid grid-cols-10 gap-1 mb-1">
-                        <span class="col-span-2 value truncate cursor-default" :title="col.name">{{ col.name }}</span>
-                        <span class="col-span-2 value truncate cursor-default" :title="col.type">{{ col.type }}</span>
-                        <span class="col-span-2 value truncate cursor-default" :title="col.nullable">{{ col.nullable }}</span>
-                        <span class="col-span-2 value truncate cursor-default" :title="col.default">{{ col.default }}</span>
-                        <span class="col-span-2 value truncate cursor-default" :title="col.generated">{{ col.generated }}</span>
-                    </div>
-                </li>
-            </ul>
+    </div>
+    <div class="content-wrapper">
+        <h3>Columns</h3>
+        <div class="header grid grid-cols-10 gap-1 mb-1">
+            <span class="col-span-2 title">Name</span>
+            <span class="col-span-2 title">Type</span>
+            <span class="col-span-2 title">Nullable</span>
+            <span class="col-span-2 title">Default</span>
+            <span class="col-span-2 title">Generated</span>
         </div>
-        <div class="footer flex gap-3">
-            <ff-button type="button" kind="secondary" class="w-full" @click="closeRightDrawer">Close</ff-button>
-            <ff-button
-                type="button"
-                kind="danger"
-                class="w-full"
-                @click="submit"
-            >
-                Delete Table
-            </ff-button>
-        </div>
+        <ul>
+            <li v-for="(col, $key) in table.schema" :key="$key">
+                <div class="grid grid-cols-10 gap-1 mb-1">
+                    <span class="col-span-2 value truncate cursor-default" :title="col.name">{{ col.name }}</span>
+                    <span class="col-span-2 value truncate cursor-default" :title="col.type">{{ col.type }}</span>
+                    <span class="col-span-2 value truncate cursor-default" :title="col.nullable">{{ col.nullable }}</span>
+                    <span class="col-span-2 value truncate cursor-default" :title="col.default">{{ col.default }}</span>
+                    <span class="col-span-2 value truncate cursor-default" :title="col.generated">{{ col.generated }}</span>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
