3304e9767f5af20615dacbed71c00a83b8bcf52f
Ben Hardill <ben@flowforge.com>
2025-08-06 11:38:53 +0100

    enable by default for enterprise

--- forge/ee/lib/index.js
+++ forge/ee/lib/index.js
@@ -30,9 +30,7 @@ module.exports = fp(async function (app, opts) {
         if (app.config.tables?.enabled) {
             app.decorate('tables', await require('./tables').init(app))
         }
-        if (app.config.certifiedNodes?.enabled) {
-            app.config.features.register('certifiedNodes', true, true)
-        }
+        app.config.features.register('certifiedNodes', true, true)
     }
 
     // Set the Team Library Feature Flag
