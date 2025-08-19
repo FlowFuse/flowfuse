cfc9ac7d8c7c85c66972c4dcfdcf8c8e62783e0f
Ben Hardill <ben@flowforge.com>
2025-08-19 16:49:06 +0100

    lint

--- forge/ee/routes/catalogues/index.js
+++ forge/ee/routes/catalogues/index.js
@@ -129,9 +129,7 @@ module.exports = async function (app) {
             reply.status(422).send({ error: 'bad_version', message: 'Invalid semver' })
             return
         }
-        console.log('passed tests')
         const upload = await subflow.buildUpdate(package, subflowJSON, app.config.npmRegistry.url)
-        console.log(upload)
 
         try {
             await axios.put(`${app.config.npmRegistry?.url}/${package.name}`, upload, {
