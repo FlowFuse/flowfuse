1b74cdc6963cc3a688bdbf82234e2cf294776c83
cstns <cstn.serban@gmail.com>
2025-09-03 15:42:28 +0300

    add FFPopover component and enhance button hover with active state support

---
+++ frontend/src/ui-components/components/Popover.vue
@@ -0,0 +1,75 @@
+<template>
+    <Popover v-slot="{ open }" class="relative">
+        <PopoverButton ref="trigger"
+                       class="ff-btn ff-btn-icon transition-fade--color"
+                       :class="{...buttonClass, active: open}"
+                       @click="() => { $nextTick(() => { updatePosition(); open = true }) }"
+        >
+            <span v-if="hasIconLeft" class="ff-btn--icon ff-btn--icon-left">
+                <slot name="icon-left"></slot>
+            </span>
+            <span>{{ buttonText }}</span>
+            <span class="ml-2">
+                <ChevronDownIcon class="ff-icon transition-ease" :class="{ 'rotate-180 transform': open }" />
+            </span>
+        </PopoverButton>
+        <teleport to="body">
+            <PopoverPanel
+                v-if="open"
+                class="absolute w-full overflow-auto bg-white  border border-gray-200 rounded-md shadow-md z-[200]"
+                :style="{
+                    top: position.top + 'px',
+                    left: position.left + 'px',
+                    width: position.width + 'px'
+                }"
+            >
+                <slot name="panel" />
+            </PopoverPanel>
+        </teleport>
+    </Popover>
+</template>
+
+<script>
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/solid'
+import { defineComponent } from 'vue'
+
+import BoxOptionsMixin from '../../mixins/BoxOptionsMixin.js'
+
+export default defineComponent({
+    name: 'ff-popover',
+    components: {
+        ChevronDownIcon,
+        Popover,
+        PopoverButton,
+        PopoverPanel
+    },
+    mixins: [BoxOptionsMixin],
+    props: {
+        buttonText: {
+            type: String,
+            required: true
+        },
+        buttonKind: {
+            default: 'primary',
+            type: String // "primary", "secondary", "tertiary"
+        }
+    },
+    computed: {
+        hasIconLeft: function () {
+            return this.$slots['icon-left']
+        },
+        buttonClass () {
+            return {
+                ['ff-btn--' + this.buttonKind]: true,
+                'ff-btn-small': this.buttonKind === 'small',
+                'ff-btn-fwidth': this.buttonKind === 'full-width'
+            }
+        }
+    }
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
