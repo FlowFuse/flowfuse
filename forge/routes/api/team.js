aac54162521b250cbd7ee90582035058fa9a0f2d
Nick O'Leary <nick.oleary@gmail.com>
2025-09-09 13:42:11 +0100

    Filter collection apis based on application rbac

--- forge/routes/api/team.js
+++ forge/routes/api/team.js
@@ -254,12 +254,19 @@ module.exports = async function (app) {
         const associationsLimit = request.query.associationsLimit
         const includeApplicationSummary = !!associationsLimit || request.query.includeApplicationSummary
 
-        const applications = await app.db.models.Application.byTeam(request.params.teamId, {
+        let applications = await app.db.models.Application.byTeam(request.params.teamId, {
             includeInstances,
             includeApplicationDevices,
             associationsLimit,
             includeApplicationSummary
         })
+
+        // Apply Application level RBAC
+        if (!request.session?.User?.admin && request.teamMembership && request.teamMembership.permissions?.applications) {
+            applications = applications.filter(application => {
+                return app.hasPermission(request.teamMembership, 'project:read', { application })
+            })
+        }
 
         reply.send({
             count: applications.length,
@@ -305,9 +312,15 @@ module.exports = async function (app) {
         const includeApplicationDevices = true
         const associationsLimit = request.query.associationsLimit
 
-        const applications = await app.db.models.Application.byTeam(request.params.teamId, { includeInstances, includeApplicationDevices, includeInstanceStorageFlow: true, associationsLimit })
+        let applications = await app.db.models.Application.byTeam(request.params.teamId, { includeInstances, includeApplicationDevices, includeInstanceStorageFlow: true, associationsLimit })
         if (!applications) {
             return reply.code(404).send({ code: 'not_found', error: 'Not Found' })
+        }
+
+        if (!request.session?.User?.admin && request.teamMembership && request.teamMembership.permissions?.applications) {
+            applications = applications.filter(application => {
+                return app.hasPermission(request.teamMembership, 'project:read', { application })
+            })
         }
         const applicationsWithAssociationsStatuses = await app.db.views.Application.applicationAssociationsStatusList(applications)
         reply.send({
@@ -348,7 +361,7 @@ module.exports = async function (app) {
         const limit = request.query.limit
         const orderByMostRecentFlows = request.query.orderByMostRecentFlows
 
-        const projects = await app.db.models.Project.byTeam(request.params.teamId, {
+        let projects = await app.db.models.Project.byTeam(request.params.teamId, {
             includeSettings: true,
             limit,
             includeMeta,
@@ -356,6 +369,11 @@ module.exports = async function (app) {
         })
 
         if (projects) {
+            if (!request.session?.User?.admin && request.teamMembership && request.teamMembership.permissions?.applications) {
+                projects = projects.filter(projects => {
+                    return app.hasPermission(request.teamMembership, 'project:read', { applicationId: app.db.models.Application.encodeHashid(projects.ApplicationId) })
+                })
+            }
             let result = await app.db.views.Project.instancesList(projects, {
                 includeSettings: true,
                 includeMeta
