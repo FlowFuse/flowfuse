77ec25af512d8687943b41d850c21c0a80a55d9e
cstns <cstn.serban@gmail.com>
2025-09-11 18:06:59 +0300

    improve right drawer layout by adding flexbox structure and overflow handling

--- frontend/src/components/drawers/RightDrawer.vue
+++ frontend/src/components/drawers/RightDrawer.vue
@@ -61,6 +61,9 @@ export default {
     min-width: 0;
     transition: ease-in-out .3s;
     box-shadow: -5px 0px 8px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
 
     & > * {
         padding: 1rem;
