df37864974888d4811fee8520f7646fc0e88eaad
cstns <cstn.serban@gmail.com>
2025-09-15 15:18:10 +0300

    Refactor: split `ApplicationSettings` into modular views (`General`, `UserAccess`), update routing with nested structure and add a blank user access page tab

---
+++ frontend/src/pages/application/Settings/UserAccess.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>User Access</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'UserAccess'
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/pages/application/Settings/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <SectionTopMenu hero="Application Settings" />
+    <div class="flex flex-col sm:flex-row mt-9 ml-6" data-el="application-settings">
+        <SectionSideMenu :options="sideNavigation" />
+        <router-view :application="application" />
+    </div>
+</template>
+
+<script>
+
+import SectionSideMenu from '../../../components/SectionSideMenu.vue'
+import SectionTopMenu from '../../../components/SectionTopMenu.vue'
+import usePermissions from '../../../composables/Permissions.js'
+
+export default {
+    name: 'ApplicationSettings',
+    components: {
+        SectionTopMenu,
+        SectionSideMenu
+    },
+    inheritAttrs: false,
+    props: {
+        application: {
+            type: Object,
+            required: true
+        }
+    },
+    emits: ['application-delete', 'application-updated'],
+    setup () {
+        const { hasPermission } = usePermissions()
+
+        return { hasPermission }
+    },
+    data () {
+        return {
+            sideNavigation: [
+                {
+                    name: 'General',
+                    path: {
+                        name: 'application-settings-general',
+                        props: {
+                            applicationId: this.application.id
+                        }
+                    }
+                },
+                {
+                    name: 'User Access',
+                    path: {
+                        name: 'application-settings-user-access'
+                    }
+                }
+            ]
+        }
+    }
+}
+</script>
