389a7b3445fd5245440a847ec0259e28ed3014d1
cstns <cstn.serban@gmail.com>
2025-09-17 11:48:21 +0300

    add `RoleRow` component and integrate into `UserAccess` to display user roles with contextual role handling

--- frontend/src/pages/application/Settings/UserAccess.vue
+++ frontend/src/pages/application/Settings/UserAccess.vue
@@ -1,12 +1,83 @@
 <template>
-    <h1>User Access</h1>
+    <ff-data-table :columns="columns" :rows="members" :search="searchTerm" :show-search="true">
+        <template #context-menu="{row}">
+            <ff-list-item data-action="edit-token" label="Edit Permissions" @click="editUserPermissions(row)" />
+        </template>
+    </ff-data-table>
+
+    <EditApplicationPermissionsDialog ref="editApplicationPermissionsDialog" @user-updated="onUserUpdated" />
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, markRaw } from 'vue'
+import { mapGetters } from 'vuex'
+
+import teamClient from '../../../api/team.js'
+
+import RoleRow from './components/RoleRow.vue'
+import EditApplicationPermissionsDialog from './dialogs/EditApplicationPermissionsDialog.vue'
 
 export default defineComponent({
-    name: 'UserAccess'
+    name: 'UserAccess',
+    components: { EditApplicationPermissionsDialog },
+    props: {
+        application: {
+            type: Object,
+            required: true
+        }
+    },
+    data () {
+        return {
+            searchTerm: '',
+            members: []
+        }
+    },
+    computed: {
+        ...mapGetters('account', ['team']),
+        columns () {
+            return [
+                {
+                    key: 'name',
+                    label: 'User',
+                    sortable: true
+                },
+                {
+                    key: 'role',
+                    label: 'Role',
+                    sortable: true,
+                    component: {
+                        is: markRaw(RoleRow),
+                        extraProps: {
+                            applicationId: this.application.id
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    mounted () {
+        this.getUsers()
+            .catch(error => error)
+    },
+    methods: {
+        getUsers () {
+            return teamClient.getTeamMembers(this.team.id)
+                .then(response => {
+                    this.members = response.members
+                })
+        },
+        editUserPermissions (user) {
+            this.$refs.editApplicationPermissionsDialog.show(user, this.application)
+        },
+        onUserUpdated (user) {
+            this.members = this.members.map(member => {
+                if (member.id === user.id) {
+                    return user
+                }
+                return member
+            })
+        }
+    }
 })
 </script>
 
