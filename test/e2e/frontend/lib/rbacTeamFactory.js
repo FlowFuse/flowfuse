dc7c2ab6b5cc05c8265a9776d18a60801a866840
cstns <cstn.serban@gmail.com>
2025-09-22 17:58:36 +0300

    assign descending app permissions to the rbac team users

--- test/e2e/frontend/lib/rbacTeamFactory.js
+++ test/e2e/frontend/lib/rbacTeamFactory.js
@@ -9,10 +9,29 @@ const FF_UTIL = require('flowforge-test-utils')
 const { Roles } = FF_UTIL.require('forge/lib/roles')
 
 module.exports = async function (forge) {
+    async function assignCustomRoleToApplication (ownerOwen, application, role) {
+        const teamMembership = await forge.db.models.TeamMember.findOne({
+            where: {
+                TeamId: rbacTeam.id,
+                UserId: ownerOwen.id
+            }
+        })
+        teamMembership.permissions = {
+            applications: {
+                ...(teamMembership.permissions.applications || {}),
+                ...{ [application.hashid]: role }
+            }
+        }
+        await teamMembership.save()
+    }
+
     const factory = new TestModelFactory(forge)
     const { stack1: stack } = await projectStackFactory.get()
     const { template1: template } = await projectTemplateFactory.get()
-    const { type1: projectType, type2: spareProjectType } = await projectTypeFactory.get()
+    const {
+        type1: projectType,
+        type2: spareProjectType
+    } = await projectTypeFactory.get()
 
     const rbacTeamType = await factory.createTeamType({
         name: 'Rbac Team',
@@ -95,7 +114,10 @@ module.exports = async function (forge) {
     await rbacTeam.addUser(noRoleBarry, { through: { role: Roles.None } })
 
     // create application1 with one instance, one device owned by the application, and one device owned by the instance
-    const application1 = await factory.createApplication({ name: 'application-1' }, rbacTeam)
+    const application1 = await factory.createApplication({
+        name: 'application-1',
+        description: 'Every user will have the Owner role'
+    }, rbacTeam)
     const application1Instance1 = await factory.createInstance(
         { name: 'application-1-instance-1' },
         application1,
@@ -126,7 +148,10 @@ module.exports = async function (forge) {
     }, rbacTeam, application1Instance1, null)
 
     // create application2 with one instance, one device owned by the application, and one device owned by the instance
-    const application2 = await factory.createApplication({ name: 'application-2' }, rbacTeam)
+    const application2 = await factory.createApplication({
+        name: 'application-2',
+        description: 'Every user will have the Member role'
+    }, rbacTeam)
     const application2Instance1 = await factory.createInstance(
         { name: 'application-2-instance-1' },
         application2,
@@ -157,7 +182,10 @@ module.exports = async function (forge) {
     }, rbacTeam, application2Instance1, null)
 
     // create application3 with one instance, one device owned by the application, and one device owned by the instance
-    const application3 = await factory.createApplication({ name: 'application-3' }, rbacTeam)
+    const application3 = await factory.createApplication({
+        name: 'application-3',
+        description: 'Every user will have the Viewer role'
+    }, rbacTeam)
     const application3Instance1 = await factory.createInstance(
         { name: 'application-3-instance-1' },
         application3,
@@ -188,7 +216,10 @@ module.exports = async function (forge) {
     }, rbacTeam, application3Instance1, null)
 
     // create application4 with one instance, one device owned by the application, and one device owned by the instance
-    const application4 = await factory.createApplication({ name: 'application-4' }, rbacTeam)
+    const application4 = await factory.createApplication({
+        name: 'application-4',
+        description: 'Every user will have the Dashboard role'
+    }, rbacTeam)
     const application4Instance1 = await factory.createInstance(
         { name: 'application-4-instance-1' },
         application4,
@@ -219,7 +250,10 @@ module.exports = async function (forge) {
     }, rbacTeam, application4Instance1, null)
 
     // create application6 with one instance, one device owned by the application, and one device owned by the instance
-    const application5 = await factory.createApplication({ name: 'application-5' }, rbacTeam)
+    const application5 = await factory.createApplication({
+        name: 'application-5',
+        description: 'Every user will have the None role'
+    }, rbacTeam)
     const application5Instance1 = await factory.createInstance(
         { name: 'application-5-instance-1' },
         application5,
@@ -250,7 +284,10 @@ module.exports = async function (forge) {
     }, rbacTeam, application5Instance1, null)
 
     // create application6 with one instance, one device owned by the application, and one device owned by the instance
-    const application6 = await factory.createApplication({ name: 'application-6' }, rbacTeam)
+    const application6 = await factory.createApplication({
+        name: 'application-6',
+        description: 'Every user will have their original team role'
+    }, rbacTeam)
     const application6Instance1 = await factory.createInstance(
         { name: 'application-6-instance-1' },
         application6,
@@ -279,4 +316,34 @@ module.exports = async function (forge) {
         name: 'application-6-instance-1-device',
         type: 'type2'
     }, rbacTeam, application6Instance1, null)
+
+    await assignCustomRoleToApplication(ownerOwen, application1, Roles.Owner)
+    await assignCustomRoleToApplication(ownerOwen, application2, Roles.Member)
+    await assignCustomRoleToApplication(ownerOwen, application3, Roles.Viewer)
+    await assignCustomRoleToApplication(ownerOwen, application4, Roles.dashboard)
+    await assignCustomRoleToApplication(ownerOwen, application5, Roles.None)
+
+    await assignCustomRoleToApplication(memberMike, application1, Roles.Owner)
+    await assignCustomRoleToApplication(memberMike, application2, Roles.Member)
+    await assignCustomRoleToApplication(memberMike, application3, Roles.Viewer)
+    await assignCustomRoleToApplication(memberMike, application4, Roles.dashboard)
+    await assignCustomRoleToApplication(memberMike, application5, Roles.None)
+
+    await assignCustomRoleToApplication(viewerVictor, application1, Roles.Owner)
+    await assignCustomRoleToApplication(viewerVictor, application2, Roles.Member)
+    await assignCustomRoleToApplication(viewerVictor, application3, Roles.Viewer)
+    await assignCustomRoleToApplication(viewerVictor, application4, Roles.dashboard)
+    await assignCustomRoleToApplication(viewerVictor, application5, Roles.None)
+
+    await assignCustomRoleToApplication(dashboardDan, application1, Roles.Owner)
+    await assignCustomRoleToApplication(dashboardDan, application2, Roles.Member)
+    await assignCustomRoleToApplication(dashboardDan, application3, Roles.Viewer)
+    await assignCustomRoleToApplication(dashboardDan, application4, Roles.dashboard)
+    await assignCustomRoleToApplication(dashboardDan, application5, Roles.None)
+
+    await assignCustomRoleToApplication(noRoleBarry, application1, Roles.Owner)
+    await assignCustomRoleToApplication(noRoleBarry, application2, Roles.Member)
+    await assignCustomRoleToApplication(noRoleBarry, application3, Roles.Viewer)
+    await assignCustomRoleToApplication(noRoleBarry, application4, Roles.dashboard)
+    await assignCustomRoleToApplication(noRoleBarry, application5, Roles.None)
 }
