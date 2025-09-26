2c620116d6031aa0c9116df01e4fa1d5149776fe
Joe Pavitt <joepavitt@flowforge.com>
2025-09-26 10:53:48 +0100

    Remote Instance Logs: Add placeholder & connected status for NR logs

--- frontend/src/pages/device/Logs.vue
+++ frontend/src/pages/device/Logs.vue
@@ -1,9 +1,32 @@
 <template>
-    <SectionTopMenu hero="Node-RED Logs" help-header="FlowFuse - Node-RED Logs" info="Live logs from your Remote Node-RED Instance" />
-    <LogsShared :device="device" />
+    <SectionTopMenu hero="Node-RED Logs" help-header="FlowFuse - Node-RED Logs" info="Live logs from your Remote Node-RED Instance">
+        <template #tools>
+            <template v-if="connected">
+                <div class="flex items-center gap-2 text-green-500">
+                    <WifiIcon class="ff-icon" />
+                    Connected to Live Logs
+                </div>
+            </template>
+            <template v-else-if="offline">
+                <div class="flex items-center gap-2 text-red-500">
+                    <WifiIcon class="ff-icon" />
+                    Offline
+                </div>
+            </template>
+            <template v-else>
+                <div class="flex items-center gap-2 text-gray-500">
+                    <WifiIcon class="ff-icon" />
+                    Connecting to Live Logs
+                </div>
+            </template>
+        </template>
+    </SectionTopMenu>
+    <LogsShared :device="device" @connected="connected = true" @disconnected="offline = true" />
 </template>
 
 <script>
+import { WifiIcon } from '@heroicons/vue/outline'
+
 import SectionTopMenu from '../../components/SectionTopMenu.vue'
 
 import LogsShared from './components/DeviceLog.vue'
@@ -11,12 +34,19 @@ import LogsShared from './components/DeviceLog.vue'
 export default {
     name: 'DeviceLogs',
     components: {
+        WifiIcon,
         SectionTopMenu,
         LogsShared
     },
     inheritAttrs: false,
     props: {
         device: { type: Object, required: true }
+    },
+    data () {
+        return {
+            connected: false,
+            offline: false
+        }
     }
 }
 </script>
