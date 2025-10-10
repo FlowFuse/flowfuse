2b394dd74bdb9fa5d0ba5b4e9e5882808c1ee9c5
Ben Hardill <ben@flowforge.com>
2025-10-10 11:33:51 +0100

    Add settings tests

---
+++ test/unit/forge/settings/settings_spec.js
@@ -0,0 +1,31 @@
+const should = require('should') // eslint-disable-line
+
+const setup = require('./setup') 
+
+describe.only('Platform Settings', function () {
+    let app
+
+    before(async function () {
+        app = await setup()
+    })
+
+    after(async function () {
+        await app.close()
+    })
+
+    describe('read settings', function () {
+        it('read default value', async function () {
+            const value = app.settings.get('telemetry:enabled')
+            value.should.equal(true)
+        })
+    })
+    describe('update settings', function () {
+        it('value should change', async function () {
+             const origValue = app.settings.get('telemetry:enabled')
+             app.settings.set('telemetry:enabled', false)
+             const newValue = app.settings.get('telemetry:enabled')
+             newValue.should.equal(false)
+             newValue.should.not.equal(origValue)
+        })
+    })
+})
