864fb416120d81551d67ab5a9acb7e6940f0746d
cstns <cstn.serban@gmail.com>
2025-10-10 15:47:30 +0300

    Add device group management to Device settings and overview pages
    - Introduced UI for assigning and unassigning devices to/from groups.
    - Integrated dialog for selecting a device group.
    - Added navigation enhancements for group-related actions.
    - Updated components and composables to support new functionality.

---
+++ frontend/src/components/dialogs/_addDeviceToGroupDialog.vue
@@ -0,0 +1,92 @@
+<template>
+    <section data-el="add-device-to-group-dialog">
+        <transition name="fade" mode="out-in">
+            <ff-loading v-if="loading" />
+            <div v-else class="flex flex-col gap-4">
+                <h3>Select a group from {{ device.application.name }}</h3>
+                <ff-combobox v-model="selected" :options="deviceGroups" label-key="name" value-key="id">
+                    <template #option="{ option, selected, active }">
+                        <li :class="{selected, active}">
+                            <div class="ff-option-content flex flex-col !gap-2 !items-start">
+                                <div class="title flex justify-between w-full items-center">
+                                    <span class="truncate bold flex-1">{{ option.name }}</span>
+                                    <span class="truncate text-gray-500 flex gap-1 items-center">
+                                        <ChipIcon class="ff-icon ff-icon-sm" />
+                                        {{ option.deviceCount }}
+                                    </span>
+                                </div>
+                                <div
+                                    v-if="option.description"
+                                    class="description italic text-gray-400 clipped-overflow--two-lines"
+                                >
+                                    <p :title="option.description">
+                                        {{ option.description }}
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                    </template>
+                </ff-combobox>
+            </div>
+        </transition>
+    </section>
+</template>
+
+<script>
+import { ChipIcon } from '@heroicons/vue/outline'
+
+import ApplicationAPI from '../../api/application.js'
+import FfLoading from '../Loading.vue'
+
+export default {
+    name: 'AddDeviceToGroupDialog',
+    components: {
+        FfLoading,
+        ChipIcon
+    },
+    props: {
+        device: {
+            type: Object,
+            required: true
+        }
+    },
+    emits: ['selected'],
+    data () {
+        return {
+            loading: true,
+            deviceGroups: [],
+            selected: null
+        }
+    },
+    watch: {
+        selected () {
+            this.$emit('selected', this.selected)
+        }
+    },
+    mounted () {
+        this.getDeviceGroups()
+    },
+    methods: {
+        async getDeviceGroups () {
+            return ApplicationAPI.getDeviceGroups(this.device.application.id)
+                .then((groups) => {
+                    this.deviceGroups = groups.groups
+                    if (this.deviceGroups?.length > 0) {
+                        this.deviceGroups.forEach((group) => {
+                            group.targetSnapshot = group.targetSnapshot || {}
+                        })
+                    }
+                })
+                .catch((err) => {
+                    console.error(err)
+                }).finally(() => {
+                    this.loading = false
+                })
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
