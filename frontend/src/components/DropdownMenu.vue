0446bbe76d93cb5aa65909245ee962e28522b78f
cstns <cstn.serban@gmail.com>
2025-11-11 13:23:58 +0200

    chore(deps): update `tailwindcss` to v3.4.0 and other dependencies for enhanced compatibility

--- frontend/src/components/DropdownMenu.vue
+++ frontend/src/components/DropdownMenu.vue
@@ -15,7 +15,7 @@
             leave-from-class="transform scale-100 opacity-100"
             leave-to-class="transform scale-95 opacity-0"
         >
-            <MenuItems :class="[edge === 'left' ? 'left-0 origin-top-left' : 'right-0 origin-top-right','z-50 absolute w-56 mt-1 bg-white divide-y divide-gray-100 rounded overflow-hidden shadow-lg ring-1 ring-black ring-opacity-10 focus:outline-none']">
+            <MenuItems :class="[edge === 'left' ? 'left-0 origin-top-left' : 'right-0 origin-top-right','z-50 absolute w-56 mt-1 bg-white divide-y divide-gray-100 rounded overflow-hidden shadow-lg ring-1 ring-black/10 focus:outline-none']">
                 <div class="apx-1 apy-1">
                     <MenuItem v-for="(item, $index) in options" v-slot="{ active }" :key="$index" :disabled="!item || item.disabled == true ? true : false">
                         <template v-if="item == null">
