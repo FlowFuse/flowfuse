973f7d697d0fc3653e6a45627b1610bafe2de66f
Ben Hardill <ben@flowforge.com>
2025-11-18 16:48:26 +0000

    fix test lint

--- test/unit/forge/ee/routes/mcp/index_spec.js
+++ test/unit/forge/ee/routes/mcp/index_spec.js
@@ -1,11 +1,8 @@
 const should = require('should') // eslint-disable-line
 const setup = require('../../setup')
 
-const FF_UTIL = require('flowforge-test-utils')
-
-describe.only('MCP Server Registration', function () {
+describe('MCP Server Registration', function () {
     let app
-    const TestObjects = { tokens: {} }
 
     before(async function () {
         const license = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZkNDFmNmRjLTBmM2QtNGFmNy1hNzk0LWIyNWFhNGJmYTliZCIsInZlciI6IjIwMjQtMDMtMDQiLCJpc3MiOiJGbG93Rm9yZ2UgSW5jLiIsInN1YiI6IkZsb3dGdXNlIERldmVsb3BtZW50IiwibmJmIjoxNzMwNjc4NDAwLCJleHAiOjIwNzc3NDcyMDAsIm5vdGUiOiJEZXZlbG9wbWVudC1tb2RlIE9ubHkuIE5vdCBmb3IgcHJvZHVjdGlvbiIsInVzZXJzIjoxMCwidGVhbXMiOjEwLCJpbnN0YW5jZXMiOjEwLCJtcXR0Q2xpZW50cyI6NiwidGllciI6ImVudGVycHJpc2UiLCJkZXYiOnRydWUsImlhdCI6MTczMDcyMTEyNH0.02KMRf5kogkpH3HXHVSGprUm0QQFLn21-3QIORhxFgRE9N5DIE8YnTH_f8W_21T6TlYbDUmf4PtWyj120HTM2w'
