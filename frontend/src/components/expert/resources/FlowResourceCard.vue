603505e1afaeea466f033dfaefec44cb4c67ee03
cstns <cstn.serban@gmail.com>
2025-11-19 18:57:33 +0200

    refactor(expert): replace custom copy button with `TextCopier` in `FlowResourceCard` component

--- frontend/src/components/expert/resources/FlowResourceCard.vue
+++ frontend/src/components/expert/resources/FlowResourceCard.vue
@@ -25,14 +25,7 @@
                             <chevron-down-icon v-if="flowsExpanded" class="h-4 w-4" />
                             <chevron-up-icon v-if="!flowsExpanded" class="h-4 w-4" />
                         </button>
-                        <button class="text-gray-600 copy plain">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="h-6 w-6 copy-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="h-6 w-6 hidden check-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                            </svg>
-                        </button>
+                        <text-copier :text="flowsJson" :showText="false" />
                     </div>
                 </div>
                 <p class="text-xs" style="margin-bottom: 0;">
@@ -50,11 +43,12 @@
 <script>
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/vue/solid'
 
+import TextCopier from '../../TextCopier.vue'
 import FlowViewer from '../../flow-viewer/FlowViewer.vue'
 
 export default {
     name: 'StandardResourceCard',
-    components: { FlowViewer, ChevronUpIcon, ChevronDownIcon },
+    components: { TextCopier, FlowViewer, ChevronUpIcon, ChevronDownIcon },
     props: {
         flow: {
             type: Object,
@@ -64,6 +58,11 @@ export default {
     data () {
         return {
             flowsExpanded: true
+        }
+    },
+    computed: {
+        flowsJson () {
+            return JSON.stringify(this.flow.metadata.flows, null, 2)
         }
     }
 }
