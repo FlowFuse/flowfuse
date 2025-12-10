14778fa0c5fa164c31ff06d4af5f0a105aa4feb8
cstns <cstn.serban@gmail.com>
2025-12-10 12:05:40 +0200

    feat(ui): add collapsible menu for table explorer layout
    - Introduced `RowsHeader` component with toggle functionality.
    - Added expand/collapse icons, animations, and updated styles.

---
+++ frontend/src/pages/team/Tables/Table/TableExplorer/components/RowsHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="rows-header flex gap-2">
+        <button class="toggle-collapse" type="button" @click="$emit('toggle-collapse')">
+            <MenuExpand v-if="menuCollapsed" />
+            <MenuCollapse v-else />
+        </button>
+    </section>
+</template>
+
+<script>
+import MenuCollapse from '../../.././../../../components/icons/menu-collapse.js'
+import MenuExpand from '../../.././../../../components/icons/menu-expand.js'
+export default {
+    name: 'RowsHeader',
+    components: { MenuCollapse, MenuExpand },
+    props: {
+        menuCollapsed: {
+            type: Boolean,
+            required: true
+        }
+    },
+    emits: ['toggle-collapse']
+}
+</script>
+
+<style scoped lang="scss">
+.rows-header {
+    margin-bottom: 15px;
+    padding-bottom: 15px;
+    border-bottom: 1px solid $ff-color--border;
+
+    .toggle-collapse {
+        border: 1px solid transparent;
+    }
+}
+</style>
