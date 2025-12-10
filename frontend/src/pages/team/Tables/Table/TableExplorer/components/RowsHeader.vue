372833818bcd3c736b26a73e2942e6d84b84b5ec
cstns <cstn.serban@gmail.com>
2025-12-10 14:33:12 +0200

    feat(ui): add refresh table action to RowsHeader component
    - Introduced a refresh button in `RowsHeader` to reload table data.
    - Integrated Vuex actions for `getTableData` and `setTableLoadingState`.
    - Updated related styles and components to support loading state.

--- frontend/src/pages/team/Tables/Table/TableExplorer/components/RowsHeader.vue
+++ frontend/src/pages/team/Tables/Table/TableExplorer/components/RowsHeader.vue
@@ -4,22 +4,49 @@
             <MenuExpand v-if="menuCollapsed" />
             <MenuCollapse v-else />
         </button>
+        <button
+            class="refresh-table" type="button" title="Refresh table data"
+            :disabled="!tableSelection"
+            @click="refreshTable"
+        >
+            <RefreshIcon class="ff-icon ff-icon-md" aria-hidden="true" />
+        </button>
     </section>
 </template>
 
 <script>
+import { RefreshIcon } from '@heroicons/vue/outline'
+import { mapActions, mapState } from 'vuex'
+
 import MenuCollapse from '../../.././../../../components/icons/menu-collapse.js'
 import MenuExpand from '../../.././../../../components/icons/menu-expand.js'
+
 export default {
     name: 'RowsHeader',
-    components: { MenuCollapse, MenuExpand },
+    components: { MenuCollapse, MenuExpand, RefreshIcon },
     props: {
         menuCollapsed: {
             type: Boolean,
             required: true
         }
     },
-    emits: ['toggle-collapse']
+    emits: ['toggle-collapse'],
+    computed: {
+        ...mapState('account', ['team']),
+        ...mapState('product/tables', ['tableSelection'])
+    },
+    methods: {
+        ...mapActions('product/tables', ['getTableData', 'setTableLoadingState']),
+        refreshTable () {
+            return this.setTableLoadingState(true)
+                .then(() => this.getTableData({
+                    teamId: this.team.id,
+                    databaseId: this.$route.params.id,
+                    tableName: this.tableSelection
+                }))
+                .finally(() => this.setTableLoadingState(false))
+        }
+    }
 }
 </script>
 
@@ -29,7 +56,7 @@ export default {
     padding-bottom: 15px;
     border-bottom: 1px solid $ff-color--border;
 
-    .toggle-collapse {
+    .toggle-collapse, .refresh-table {
         border: 1px solid transparent;
     }
 }
