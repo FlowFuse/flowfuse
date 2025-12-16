c5352e3721df53db4558cf315e0197f367fa53ef
Steve-Mcl <sdmclaughlin@gmail.com>
2025-12-16 12:40:12 +0000

    wrap MCPRegistration deletions in try catch

--- forge/db/models/Device.js
+++ forge/db/models/Device.js
@@ -169,12 +169,18 @@ module.exports = {
                 })
                 // if MCPRegistration model is available (EE mode), remove any registrations for this device
                 if (app.db.models.MCPRegistration) {
-                    await app.db.models.MCPRegistration.destroy({
-                        where: {
-                            targetType: 'device',
-                            targetId: '' + device.id
-                        }
-                    })
+                    try {
+                        await app.db.models.MCPRegistration.destroy({
+                            where: {
+                                targetType: 'device',
+                                targetId: '' + device.id
+                            }
+                        })
+                    } catch (err) {
+                        // Ignore errors that occur while attempting to clean up MCPRegistration
+                        // to avoid test flakiness where destroy hooks run after shutdown.
+                        app.log && app.log.debug && app.log.debug('Ignoring MCPRegistration cleanup error during device destroy:', err && err.message)
+                    }
                 }
             }
         }
