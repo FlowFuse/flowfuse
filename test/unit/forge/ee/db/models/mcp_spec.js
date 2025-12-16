ea0f4808e199299b740dfece778ebd575d24c13d
Steve-Mcl <sdmclaughlin@gmail.com>
2025-12-16 10:06:22 +0000

    Add tests to ensure MCP entries are removed on instance and device deletion

---
+++ test/unit/forge/ee/db/models/mcp_spec.js
@@ -0,0 +1,77 @@
+const should = require('should') // eslint-disable-line
+const setup = require('../../setup')
+
+const FF_UTIL = require('flowforge-test-utils')
+const { Roles } = FF_UTIL.require('forge/lib/roles')
+
+describe('MCPRegistration Model', function () {
+    let app
+    const TestObjects = { tokens: {} }
+
+    before(async function () {
+        const license = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImZkNDFmNmRjLTBmM2QtNGFmNy1hNzk0LWIyNWFhNGJmYTliZCIsInZlciI6IjIwMjQtMDMtMDQiLCJpc3MiOiJGbG93Rm9yZ2UgSW5jLiIsInN1YiI6IkZsb3dGdXNlIERldmVsb3BtZW50IiwibmJmIjoxNzMwNjc4NDAwLCJleHAiOjIwNzc3NDcyMDAsIm5vdGUiOiJEZXZlbG9wbWVudC1tb2RlIE9ubHkuIE5vdCBmb3IgcHJvZHVjdGlvbiIsInVzZXJzIjoxMCwidGVhbXMiOjEwLCJpbnN0YW5jZXMiOjEwLCJtcXR0Q2xpZW50cyI6NiwidGllciI6ImVudGVycHJpc2UiLCJkZXYiOnRydWUsImlhdCI6MTczMDcyMTEyNH0.02KMRf5kogkpH3HXHVSGprUm0QQFLn21-3QIORhxFgRE9N5DIE8YnTH_f8W_21T6TlYbDUmf4PtWyj120HTM2w'
+        app = await setup({ license })
+
+        await login('alice', 'aaPassword')
+
+        const userBob = await app.db.models.User.create({ username: 'bob', name: 'Bob Solo', email: 'bob@example.com', email_verified: true, password: 'bbPassword' })
+        await app.team.addUser(userBob, { through: { role: Roles.Owner } })
+        // Run all the tests with bob - non-admin Team Owner
+        await login('bob', 'bbPassword')
+    })
+
+    after(async function () {
+        await app.close()
+    })
+
+    async function login (username, password) {
+        const response = await app.inject({
+            method: 'POST',
+            url: '/account/login',
+            payload: { username, password, remember: false }
+        })
+        response.cookies.should.have.length(1)
+        response.cookies[0].should.have.property('name', 'sid')
+        TestObjects.tokens[username] = response.cookies[0].value
+    }
+
+    it('deleting an instance should remove associated MCP entry', async function () {
+        // First, create a sacrificial instance and MCP entry
+        const sacrificialInstance = await app.factory.createInstance({ name: 'instance-to-be-deleted' }, app.application, app.stack, app.template, app.projectType, { start: false })
+
+        await app.db.models.MCPRegistration.create({
+            name: 'to-be-deleted',
+            protocol: 'http',
+            endpointRoute: '/mcp',
+            targetType: 'instance',
+            targetId: '' + sacrificialInstance.id,
+            nodeId: 'xxxxx',
+            TeamId: app.team.id
+        })
+        // Now delete the instance
+        await sacrificialInstance.destroy()
+        // Now check the MCP entry is gone
+        const mcpServer = await app.db.models.MCPRegistration.byTypeAndIDs('instance', sacrificialInstance.hashid, '')
+        should.not.exist(mcpServer)
+    })
+
+    it('deleting a device should remove associated MCP entry', async function () {
+        // First, create a sacrificial device and MCP entry
+        const sacrificialDevice = await app.factory.createInstance({ name: 'device-to-be-deleted' }, app.application, app.stack, app.template, app.projectType, { start: false })
+
+        await app.db.models.MCPRegistration.create({
+            name: 'to-be-deleted-device',
+            protocol: 'http',
+            endpointRoute: '/mcp',
+            targetType: 'device',
+            targetId: '' + sacrificialDevice.id,
+            nodeId: 'yyyyy',
+            TeamId: app.team.id
+        })
+        // Now delete the device
+        await sacrificialDevice.destroy()
+        // Now check the MCP entry is gone
+        const mcpServer = await app.db.models.MCPRegistration.byTypeAndIDs('device', sacrificialDevice.hashid, '')
+        should.not.exist(mcpServer)
+    })
+})
