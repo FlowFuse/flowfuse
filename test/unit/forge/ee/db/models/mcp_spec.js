c89f5c8f4c1aea35b5acae2365dc17eed3017f31
Steve-Mcl <sdmclaughlin@gmail.com>
2026-01-06 12:08:16 +0000

    fix test fail due to no-prototype on fastify v5 cookie

--- test/unit/forge/ee/db/models/mcp_spec.js
+++ test/unit/forge/ee/db/models/mcp_spec.js
@@ -31,8 +31,9 @@ describe('MCPRegistration Model', function () {
             payload: { username, password, remember: false }
         })
         response.cookies.should.have.length(1)
-        response.cookies[0].should.have.property('name', 'sid')
-        TestObjects.tokens[username] = response.cookies[0].value
+        const cookie1 = { ...response.cookies[0] }
+        cookie1.should.have.property('name', 'sid')
+        TestObjects.tokens[username] = cookie1.value
     }
 
     it('deleting an instance should remove associated MCP entry', async function () {
