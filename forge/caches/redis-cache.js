d2c6c29e9e4620b151c9ffade45ed48385bc146b
Ben Hardill <ben@flowforge.com>
2026-03-02 11:36:09 +0000

    Ensure Valkey/Redis cache reconnects cleanly
    fixes #6777
    
    Adds error handlers and a reconnect stategy as well as upgrading
    @redis/client package

--- forge/caches/redis-cache.js
+++ forge/caches/redis-cache.js
@@ -5,8 +5,29 @@ const caches = {}
 let client
 
 async function initCache (options) {
-    client = createClient(options)
-    await client.connect()
+    const newOptions = {
+        ...options,
+        socket: {
+            reconnectStrategy: (retries, cause) => {
+                // Generate a random jitter between 0 – 200 ms:
+                const jitter = Math.floor(Math.random() * 200);
+                // Delay is an exponential back off, (times^2) * 50 ms, with a maximum value of 2000 ms:
+                const delay = Math.min(Math.pow(2, retries) * 50, 2000);
+
+                return delay + jitter;
+            }
+        }
+    }
+    client = createClient(newOptions)
+    client.on('error', (err) => {})
+    client.on('end', () => {})
+    client.on('reconnecting', () => {})
+    client.on('connected', () => {})
+    client.on('ready', () => {})
+    try {
+        await client.connect()
+    } catch (err) {
+    }
 }
 
 function getCache (name, options) {
