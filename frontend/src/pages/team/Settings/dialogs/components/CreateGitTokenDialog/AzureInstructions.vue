18d60e8da892654d5d9aefabb88b1a7dd180bc76
Noley Holland <hollandnoley@gmail.com>
2026-03-20 10:16:01 -0700

    Add in option tabs for azure and github, add svgs, ensure type is updated in confirm

---
+++ frontend/src/pages/team/Settings/dialogs/components/CreateGitTokenDialog/AzureInstructions.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <p>Azure DevOps instructions coming soon.</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'AzureInstructions',
+    inheritAttrs: false
+}
+</script>
