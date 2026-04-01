ff6ae4a3d9c8b3e8ab054938da914576d425a920
Ben Hardill <ben@flowforge.com>
2026-04-01 20:52:46 +0100

    fix lint

--- forge/routes/api/teamMembers.js
+++ forge/routes/api/teamMembers.js
@@ -73,13 +73,10 @@ module.exports = async function (app) {
         let result = app.db.views.User.teamMemberList(members)
         if (app.config.features.enabled('sso')) {
             const sso = await app.db.views.User.ssoManaged(members, request.team)
-            console.log(result)
-            console.log(sso)
             result = result.map(r => {
                 r.ssoManaged = sso[r.id]
                 return r
             })
-            console.log(result)
         }
         reply.send({
             meta: {}, // For future pagination
