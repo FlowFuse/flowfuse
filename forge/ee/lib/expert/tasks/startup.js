1e609c30b4306c27e125b899e62ba655d9cd0713
Stephen McLaughlin <44235289+Steve-Mcl@users.noreply.github.com>
2026-05-06 12:06:16 +0100

    Apply suggestion from @Steve-Mcl

--- forge/ee/lib/expert/tasks/startup.js
+++ forge/ee/lib/expert/tasks/startup.js
@@ -1,8 +1,8 @@
 const { syncBridge } = require('../emxq-bridge/setup.js')
 module.exports = {
     name: 'EmqxExpertBridgeSetup',
-    startup: true,
-    schedule: '',
+    startup: 90 * 1000, // Run 1.5min after startup
+    schedule: '', // no schedule
     run: async function (app) {
         // On startup, the web app is not running anyway so a full resync
         // of the bridge is not disruptive and ensures that any changes to the
