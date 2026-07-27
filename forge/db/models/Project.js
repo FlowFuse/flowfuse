28fd7fba04297dd946a24d588537f4957dfece42
Ben Hardill <ben@flowforge.com>
2026-07-27 17:12:20 +0100

    Installed node settings hook (#7880)

--- forge/db/models/Project.js
+++ forge/db/models/Project.js
@@ -260,6 +260,13 @@ module.exports = {
                         app.log.error(`Error removing MCPRegistrations for deleted instance ${project.id}: ${err.message}`)
                     }
                 }
+                // Remove version info when Project removed
+                await app.db.models.NodeREDNodeVersions.destory({
+                    where: {
+                        ownerType: 'instance',
+                        ownerId: project.id
+                    }
+                })
             }
         }
     },
