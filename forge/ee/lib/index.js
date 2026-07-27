06ac8606f113202c2dab7c3fd79c276ece74751d
Ben Hardill <ben@flowforge.com>
2026-07-27 17:15:18 +0100

    fix lint

--- forge/ee/lib/index.js
+++ forge/ee/lib/index.js
@@ -61,7 +61,7 @@ module.exports = fp(async function (app, opts) {
             (Object.prototype.hasOwnProperty.call(app.config?.expert ?? {}, 'insights') ? !!app.config?.expert?.insights?.enabled : true)
 
         app.config.features.register('expertInsights', isInsightsEnabled ?? false, true)
-        
+
         require('./bom').init(app)
     }
 
