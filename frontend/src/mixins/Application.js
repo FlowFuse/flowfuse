bdedb684106a448cf80e739a6985a1c3bfc3534d
Noley Holland <63617269+n-lark@users.noreply.github.com>
2026-08-03 09:06:52 -0700

    [7651] Data-farm > Applications 2 - store + first consumer (#8021)
    Co-authored-by: Ben Hardill <ben@flowforge.com>

--- frontend/src/mixins/Application.js
+++ frontend/src/mixins/Application.js
@@ -4,6 +4,7 @@ import ApplicationApi from '../api/application.js'
 import alerts from '../services/alerts.js'
 
 import { useContextStore } from '@/stores/context.js'
+import { useDataFarmApplicationsStore } from '@/stores/data-farm-applications'
 
 export default {
     data () {
@@ -36,6 +37,7 @@ export default {
     },
     methods: {
         ...mapActions(useContextStore, { setContextualApplication: 'setApplication' }),
+        ...mapActions(useDataFarmApplicationsStore, { deleteApplicationEntity: 'deleteApplication' }),
         async updateApplication () {
             const applicationId = this.$route.params.id
 
@@ -87,7 +89,7 @@ export default {
             this.loading.deleting = true
 
             try {
-                await ApplicationApi.deleteApplication(this.application.id, this.team.id)
+                await this.deleteApplicationEntity(this.application.id, this.team.id)
                 await useContextStore().refreshTeam()
                 this.$router.push({ name: 'Applications' })
                 alerts.emit('Application successfully deleted.', 'confirmation')
