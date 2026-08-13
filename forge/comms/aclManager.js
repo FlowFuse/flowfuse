12c3b20dd72a4cb5650309f74733a78451e56490
Noley Holland <63617269+n-lark@users.noreply.github.com>
2026-08-13 12:28:05 -0700

    [8079] Data Farm > Hosted Instances 1 - backend: lifecycle events   (#8112)

--- forge/comms/aclManager.js
+++ forge/comms/aclManager.js
@@ -518,6 +518,8 @@ module.exports = function (app) {
                 { topic: /^ff\/v1\/[^/]+\/d\/[^/]+\/state$/ },
                 // - ff/v1/<team>/a/<application>/created|updated|deleted
                 { topic: /^ff\/v1\/[^/]+\/a\/[^/]+\/(created|updated|deleted)$/ },
+                // - ff/v1/<team>/p/<instance>/created|updated|deleted
+                { topic: /^ff\/v1\/[^/]+\/p\/[^/]+\/(created|updated|deleted)$/ },
                 // ff/v1/platform/sync
                 { topic: /^ff\/v1\/platform\/sync$/ },
                 // ff/v1/platform/leader
@@ -589,7 +591,9 @@ module.exports = function (app) {
                 // - ff/v1/<team>/d/+/state
                 { topic: /^ff\/v1\/([^/]+)\/d\/([^/]+)\/state$/, verify: 'checkTeamStateSub' },
                 // - ff/v1/<team>/a/+/created|updated|deleted
-                { topic: /^ff\/v1\/([^/]+)\/a\/([^/]+)\/(created|updated|deleted)$/, verify: 'checkTeamStateSub' }
+                { topic: /^ff\/v1\/([^/]+)\/a\/([^/]+)\/(created|updated|deleted)$/, verify: 'checkTeamStateSub' },
+                // - ff/v1/<team>/p/+/created|updated|deleted
+                { topic: /^ff\/v1\/([^/]+)\/p\/([^/]+)\/(created|updated|deleted)$/, verify: 'checkTeamStateSub' }
             ],
             pub: [
                 // ff/v1/browser/tab-presence/<userId>/<sessionId>/<heartbeat|context>
