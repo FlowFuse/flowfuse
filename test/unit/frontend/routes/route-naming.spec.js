016ffde8c7acac9fb4f8bf5a321a97de946ec379
cstns <cstn.serban@gmail.com>
2026-08-14 19:36:57 +0300

    Update imports to use `node:` prefix for consistency with newer node versions

--- test/unit/frontend/routes/route-naming.spec.js
+++ test/unit/frontend/routes/route-naming.spec.js
@@ -1,5 +1,5 @@
-import { readFileSync, readdirSync, statSync } from 'fs'
-import { join } from 'path'
+import { readFileSync, readdirSync, statSync } from 'node:fs'
+import { join } from 'node:path'
 
 import { describe, expect, test } from 'vitest'
 
