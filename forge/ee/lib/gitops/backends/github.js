88feeeb77b7e007110c1b42fd4a835e302d57b2b
Ben Hardill <ben@flowforge.com>
2026-09-15 10:37:23 +0100

    Sanitize Git command line arguments
    Ensure that no Shell expansions are passed as `git` arguments

--- forge/ee/lib/gitops/backends/github.js
+++ forge/ee/lib/gitops/backends/github.js
@@ -1,10 +1,10 @@
-const { exec } = require('node:child_process')
+const { execFile } = require('node:child_process')
 const { existsSync } = require('node:fs')
 const fs = require('node:fs/promises')
 const os = require('node:os')
 const path = require('node:path')
 const { promisify } = require('node:util')
-const execPromised = promisify(exec)
+const execFilePromised = promisify(execFile)
 
 const axios = require('axios')
 
@@ -63,10 +63,10 @@ module.exports.init = async function (app) {
             await cloneRepository(url, branch, workingDir)
 
             // 4. set username/email
-            await execPromised('git config user.email "no-reply@flowfuse.com"', { cwd: workingDir })
-            await execPromised('git config user.name "FlowFuse"', { cwd: workingDir })
+            await execFilePromised('git', ['config', 'user.email', 'no-reply@flowfuse.com'], { cwd: workingDir })
+            await execFilePromised('git', ['config', 'user.name', 'FlowFuse'], { cwd: workingDir })
             // For local dev - disable gpg signing in case its set in global config
-            await execPromised('git config commit.gpgsign false', { cwd: workingDir })
+            await execFilePromised('git', ['config', 'commit.gpgsign', 'false'], { cwd: workingDir })
 
             // 5. export snapshot
             const exportOptions = {
@@ -86,14 +86,15 @@ module.exports.init = async function (app) {
             await fs.writeFile(snapshotFile, JSON.stringify(snapshotExport, null, 4))
 
             // 6. stage file
-            await execPromised(`git add "${snapshotFile}"`, { cwd: workingDir })
+            await execFilePromised('git', ['add', snapshotFile], { cwd: workingDir })
 
             // 7. commit
-            await execPromised(`git commit -m "Update snapshot\n\nSnapshot updated by FlowFuse Pipeline '${options.pipeline.name.replace(/"/g, '')}', triggered by ${options.user.username.replace(/"/g, '')}" --author="${author}"`, { cwd: workingDir })
+            const commitMessage = `"Update snapshot\n\nSnapshot updated by FlowFuse Pipeline '${options.pipeline.name}', triggered by ${options.user.username}"`
+            await execFilePromised('git', ['commit', '-m', commitMessage, '--author=', 'author'], { cwd: workingDir })
 
             try {
                 // 8. push
-                await execPromised('git push', { cwd: workingDir })
+                await execFilePromised('git', ['push'], { cwd: workingDir })
             } catch (err) {
                 const output = err.stdout + err.stderr
                 if (/unable to access/.test(output)) {
